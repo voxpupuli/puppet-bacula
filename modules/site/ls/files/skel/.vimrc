f5a5467432b00d40faedcdb48fbad8bee4617cfd
Teyo Tyree <teyo@lappy-2.local>
2009-11-14 15:53:39 -0800

    cleanup modules in prep for infra refactoring

--- modules/site/ls/files/skel/.vimrc
+++
@@ -1,1 +0,0 @@
-set nu et ts=4 sw=4 shiftround ignorecase smartcase
--- modules/site/puppetmaster/manifests/init.pp
+++
@@ -1,4 +0,0 @@
-class puppetmaster {
-  include puppet::server::storedconfigs
-  puppet::server::storedconfigs::puppetstoredb {"reductive": }
-}

2d765e3f03e8ab64acc81e25468969eb22de135e
creating sysadmin user <dan@localhost.localdomain>
2009-11-22 05:04:47 -0600

    Added network code (nsswitch, host.conf, hosts, resolv.conf)

---
+++ infrastructure/modules/network/manifests/tests/network.pp
@@ -0,0 +1,3 @@
+$my_searchpath=localdomain
+$my_nameservers=['68.87.85.102', '68.87.69.150']
+include network
--- infrastructure/modules/test/manifests/test.pp
+++ infrastructure/modules/test/manifests/test.pp
@@ -1,10 +1,16 @@
+#
+# This tests rather or not dependencies effect the order of 
+# function calls
+#
+
+
+
 #file{"/tmp/test":
 #  content => template('test/test.erb'),
 #}
 include test
 class test {
   three{'three':
-
   } 
   one{'one':
     require => Two['two']

875511616dceab754fb72304e4b6d80805a59c0f
root <root@localhost.localdomain>
2010-02-09 07:45:45 -0500

    fixes for installing passenger on centos

--- dist/passenger/manifests/init.pp
+++ dist/passenger/manifests/init.pp
@@ -1,16 +1,17 @@
 class passenger {
-  include apache
-  include ruby-dev
-  # include gcc
-  # include gplusplus
-  # apache-dev
-  # apr, apu
+  include ruby::dev
+  include gcc
+  include apache::dev
   package{'passenger':
     name   => 'passenger',
     ensure => installed,
-    provider => 'passenger',
+    provider => 'gem',
   }
-#  exec{'compile-passenger':
-#    path => 
-#  }
+  exec{'compile-passenger':
+    path => ['/usr/bin', '/bin'],
+    command => 'passenger-install-apache2-module -a',
+    logoutput => true,
+    # this creates path doesnt seem very general
+    creates => '/usr/lib/ruby/gems/1.8/gems/passenger-2.2.11/ext/apache2/mod_passenger.so',
+  }
 }

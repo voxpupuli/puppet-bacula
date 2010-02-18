25fff59d1d67cd4d9d45ab939c4241776335b7c9
root <root@ubuntu.(none)>
2010-02-17 23:15:27 -0800

    test files for passenger and webbrick redmine installations

---
+++ infrastructure/modules/redmine/tests/passenger.pp
@@ -0,0 +1,8 @@
+#$mysql_old_pw='redmine'
+$mysql_root_pw='password'
+$redmine_db='redmine'
+$redmine_db_user='redmine'
+$redmine_db_pw='redmine'
+$redmine_db_socket='/var/run/mysqld/mysqld.sock'
+$rails_version='2.1.2'
+include redmine::passenger

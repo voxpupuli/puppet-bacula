64dde8fd18fd6ee920cf4c218ea02a4c7fe00e73
root <root@ubuntu.(none)>
2010-02-24 15:54:10 -0800

    added all of my infrastructure modules to dist, removed the modules that were there

---
+++ manifests/vhost.pp
@@ -0,0 +1,21 @@
+define apache::vhost(
+    $port, 
+    $docroot,
+    $webdir,
+    $ssl=true, 
+    $template='apache/vhost-default.conf.erb'
+  ) {
+  include apache
+  $apachedir = $operatingsystem? {
+    'ubuntu' => '/etc/apache2/sites-enabled/',
+    default  => '/etc/httpd/conf.d',
+  }
+  file{"${apachedir}/${name}.conf":
+    content => template($template),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '777',
+    require => Package['httpd'],
+    notify  => Service['httpd'],
+  }
+}

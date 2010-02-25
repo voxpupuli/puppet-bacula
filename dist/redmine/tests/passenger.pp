feb56da3d04d8846f2a6b61c161d4b3d2621aa96
root <root@ubuntu.(none)>
2010-02-24 21:20:27 -0800

    changes related to fixing redmine as a defined resource type

--- dist/redmine/tests/passenger.pp
+++ dist/redmine/tests/passenger.pp
@@ -1,8 +1,28 @@
+# you may have to specify an old mysql pw if you borked /root/.my.cnf
 #$mysql_old_pw='redmine'
+# this needs to be moved somewhere else, but I am a little confused by the scoping??
 $mysql_root_pw='password'
-$redmine_db='redmine'
-$redmine_db_user='redmine'
-$redmine_db_pw='redmine'
-$redmine_db_socket='/var/run/mysqld/mysqld.sock'
-$rails_version='2.1.2'
-include redmine::passenger
+file{'/opt/redmine':
+  ensure => directory,
+}
+redmine::passenger{'puppet-ubuntu':
+  db      => 'redmine1',
+  db_user => 'redmine',
+  db_pw => 'password',
+  dir => '/opt/redmine', 
+  port => '80',
+}
+redmine::passenger{'puppet-ubuntu2':
+  db      => 'redmine2',
+  db_user => 'redmine2',
+  db_pw => 'password2',
+  dir => '/opt/redmine', 
+  port => '81',
+}
+redmine::passenger{'puppet-ubuntu5':
+  db      => 'redmine5',
+  db_user => 'redmine3',
+  db_pw => 'password3',
+  dir => '/opt/redmine', 
+  port => '82',
+}

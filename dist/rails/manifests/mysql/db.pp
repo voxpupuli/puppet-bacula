b9fd87b12f9386d3b63a0090bf8a7fb165631f9b
root <root@ubuntu.(none)>
2010-02-25 00:29:53 -0800

    abstracted rails management stuff here so it can be reused

---
+++ dist/rails/manifests/mysql/db.pp
@@ -0,0 +1,18 @@
+define rails::mysql::db($db_user, $db_pw, $db_charset = 'utf8', $dir, $host = 'localhost', $grant='all') {
+  require mysql::ruby
+  include mysql::myparams
+  mysql::db{$name:
+    db_user => $db_user,
+    db_pw => $db_pw,
+    db_charset => $db_charset,
+    host => $host,
+    grant => $grant, 
+  }
+  rails::db_config{"$dir":
+    adapter => 'mysql',
+    username => $db_user,
+    password => $db_pw,
+    database => $name,
+    socket => $mysql::params::socket,
+  }
+}

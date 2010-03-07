c6c3fe3969c40ef1e338a84296549460f4c13b72
dan <dan@reductivelabs.com>
2010-03-06 20:20:00 -0800

    added ruby-dev to ruby::dev

--- dist/ruby/manifests/dev.pp
+++ dist/ruby/manifests/dev.pp
@@ -3,18 +3,20 @@ class ruby::dev {
   require ruby
   case $operatingsystem {
     'centos': {
+      $ruby_dev='ruby-devel'
       $ruby_rdoc='ruby-rdoc'
       $ruby_irb='ruby-irb'
       $ruby_rake='rubygem-rake'
     }
     'ubuntu': {
+      $ruby_dev='ruby-dev'
       $ruby_rdoc='rdoc'
       $ruby_irb='irb'
       $ruby_rake='rake'
     }
   
   }
-  package {[$ruby_rdoc, $ruby_irb, $ruby_rake]:
+  package {[$ruby_rdoc, $ruby_irb, $ruby_rake, $ruby_dev]:
     ensure => installed,
   }
 }

2eeb16978510391178c6bbe72ebdb6b927d83970
root <root@baal.reductivelabs.com>
2010-03-20 11:50:32 +0000

    Updates to get add documentation and fix a number of errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,15 @@
 # ensure apache is installed
 class apache {
-  $apache_name = $operatingsystem? {
-    'ubuntu'  => 'apache2',
-    default => 'httpd',
-  }
+  include apache::params
+
   package{'httpd': 
-    name   => $apache_name,
+    name   => $apache::params::apache_name,
     ensure => present,
   }
-# I dont know where the default arguments go for Ubuntu
-#  file{ "/etc/sysconfig/httpd":
-#    ensure  => present,
-#    owner   => "root",
-#    group   => "root",
-#    mode    => "644",
-#    source  => "puppet:///modules/apache/httpd",
-#    require => Package["httpd"],
-#  }
   service { 'httpd':
-    name      => $apache_name,
+    name      => $apache::params::apache_name,
     ensure    => running,
     enable    => true,
-#    subscribe => File["/etc/sysconfig/httpd"],
     subscribe => Package['httpd'],
   }
 }
--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -1,6 +1,20 @@
 class apache::ssl {
   include apache
-  package{'mod_ssl':
-    require => Package['httpd'],
+
+  
+  case $operatingsystem {
+     "centos": {
+        package { $apache::params::ssl_package:
+           require => Package['httpd'],
+        }
+     }
+     "ubuntu": {
+        exec { "a2enmod ssl": 
+           path => "/usr/bin:/usr/sbin:/bin",
+        }
+        exec { "a2enmod headers":
+           path => "/usr/bin:/usr/sbin:/bin",
+        }
+     }
   }
 }

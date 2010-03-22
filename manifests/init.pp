9cfaee326bf80e34ef4263dc3ab9c676a12ed46b
teyo <teyo@puppetlabs.com>
2010-03-22 21:14:32 +0000

    fix php-curl dependencies for puppetlabs::website

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # ensure apache is installed
 class apache {
   include apache::params
-
   package{'httpd': 
     name   => $apache::params::apache_name,
     ensure => present,
@@ -12,4 +11,16 @@ class apache {
     enable    => true,
     subscribe => Package['httpd'],
   }
+  #
+  # May want to purge all none realize modules using the resources resource type.
+  # A2mod resource type is broken.  Look into fixing it and moving it into apache.
+  #
+  A2mod { require => Package['httpd'], notify => Service['httpd']}
+  a2mod { 'mod_rewrite' : ensure => present }
+  file { '/etc/apache2/sites-enabled':
+    ensure => directory,
+    recurse => true,
+    purge => true,
+    notify => Service['httpd'],
+  } 
 }

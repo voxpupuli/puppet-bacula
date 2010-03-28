f7c3af50ae2df5ec7e2adf101899b69ef913a2b3
root <root@baal.puppetlabs.com>
2010-03-28 05:29:49 +0000

    Added storedconfig support for Puppet module

--- site/puppetlabs/manifests/dxul.pp
+++ site/puppetlabs/manifests/dxul.pp
@@ -14,11 +14,11 @@ class puppetlabs::dxul {
   $mysql_root_pw = 'c@11-m3-m1st3r-p1t4ul'
   $bacula_director = 'baal.puppetlabs.com'
   $bacula_password = '9haB2+SxaNXF2C1LFdptETvihkk/zKro2Hxf+cQFEbIQ'
+  $puppet_storedconfig_password = 'password'
 
   include puppetlabs
   include puppet
-  include puppet::server
-  include puppet::dashboard
+  include 'puppet-demo'
   include account::master
   include bacula::client
 }

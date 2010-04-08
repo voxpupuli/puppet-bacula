2b41aec6cc375ff26f46e067ecbb4b62286a855f
root <root@baal.puppetlabs.com>
2010-04-08 11:56:22 +0000

    Restructured Bacula, Nagios, Puppet modules
    Added NRPE support

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,6 @@
 #   Installs the bacula-common package
 #
 # Requires:
-#   - On CentOS the epel module
 #
 # Sample Usage:
 #
@@ -17,7 +16,28 @@ class bacula {
 
   include bacula::params
 
-  package{'bacula-common':
-    ensure   => present,
+  package { 'bacula-common':
+    ensure => present,
+  }
+
+  package { $bacula::params::bacula_client_packages:
+    ensure => present,
+  }
+
+  service { $bacula::params::bacula_client_services:
+    ensure => running,
+    enable => true,
+    require => Package[$bacula::params::bacula_client_packages],
+  }
+
+  file { '/etc/bacula/bacula-fd.conf':
+    require => Package[$bacula::params::bacula_client_packages],
+    content => template('bacula/bacula-fd.conf.erb'),
+    notify => Service[$bacula::params::bacula_client_services],
+  }
+
+  file { '/var/lib/bacula/mysql':
+    ensure => directory,
+    require => Package[$bacula::params::bacula_client_packages],
   }
 }

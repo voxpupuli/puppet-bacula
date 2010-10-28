ea4ced5e6fbfa070d9d62342f7ca0cb7335f8d83
Zach <zach@puppetlabs.com>
2010-10-28 15:44:51 -0400

    improvements to pxe module, breaking out into defines

--- dist/pxe/manifests/init.pp
+++ dist/pxe/manifests/init.pp
@@ -2,6 +2,9 @@ class pxe {
 	include pxe::params
 	include apache
 
+	$tftp_root = $pxe::params::tftp_root
+	$ks_root   = $pxe::params::ks_root
+
 	package {
 		"xinetd":				ensure => installed;
 		"syslinux":			ensure => installed;
@@ -15,25 +18,21 @@ class pxe {
 			mode		=> 644,
 			notify	=> Service["xinetd"],
 			source	=> "puppet:///modules/pxe/tftp_xinetd";
-		"/tftpboot": ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/pxelinux.0": ensure => directory, owner => root, group => root, mode => 755,
+		"${tftp_root}": ensure => directory, owner => root, group => root, mode => 755;
+		"${tftp_root}/pxelinux.0": ensure => directory, owner => root, group => root, mode => 755,
 			source => "/usr/lib/syslinux/pxelinux.0";
-		"/tftpboot/menu.c32": ensure => directory, owner => root, group => root, mode => 755,
+		"${tftp_root}/menu.c32": ensure => directory, owner => root, group => root, mode => 755,
 			source => "/usr/lib/syslinux/menu.c32";
-		"/tftpboot/memdisk": ensure => directory, owner => root, group => root, mode => 755,
+		"${tftp_root}/memdisk": ensure => directory, owner => root, group => root, mode => 755,
 			source => "/usr/lib/syslinux/memdisk";
-		"/tftpboot/mboot.c32": ensure => directory, owner => root, group => root, mode => 755,
+		"${tftp_root}/mboot.c32": ensure => directory, owner => root, group => root, mode => 755,
 			source => "/usr/lib/syslinux/mboot.c32";
-		"/tftpboot/chain.c32": ensure => directory, owner => root, group => root, mode => 755,
+		"${tftp_root}/chain.c32": ensure => directory, owner => root, group => root, mode => 755,
 			source => "/usr/lib/syslinux/chain.c32";
-		"/tftpboot/pxelinux.cfg": ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/images":										ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/images/centos":						ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/images/centos/x86":				ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/images/centos/x86/5.5":		ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/images/centos/x86_64":			ensure => directory, owner => root, group => root, mode => 755;
-		"/tftpboot/images/centos/x86_64/5.5":	ensure => directory, owner => root, group => root, mode => 755;
-		"/var/www/html/ks": ensure => directory, owner => root, group => root, mode => 755;
+		#"${tftp_root}/pxelinux.cfg": ensure => directory, owner => root, group => root, mode => 755;
+		"${tftp_root}/images":										ensure => directory, owner => root, group => root, mode => 755;
+		"${tftp_root}/images/centos":						ensure => directory, owner => root, group => root, mode => 755;
+		"${ks_root}": ensure => directory, owner => root, group => root, mode => 755;
 	}
 
 	service {
@@ -41,5 +40,74 @@ class pxe {
 			ensure => running,
 			enable => true;
 	}
+	
+	$dirs = [
+		"${tftp_root}/images/centos/i386",
+		"${tftp_root}/images/centos/x86_64",
+	]
+	
+	file { $dirs: ensure => directory, owner => root, group => root, mode => 755; }
+	
+	#fragment { "cent-header":
+  #  order => '00',
+  #  path => "${tftp_root}/pxelinux.cfg",
+  #  target => 'cent',
+  #  source => 'puppet:///modules/pxe/centosPXE-header',
+  #}
+
+  #fragment::concat { 'cent':
+  #  owner => 'root',
+  #  group => 'root',
+  #  mode => '0644',
+  #  path => "${tftp_root}/pxelinux.cfg",
+  #}
+
+	centosimages { 
+		"centos_i386_4.8":
+			arch => "i386",
+			ver => "4.8";
+		"centos_x86_64_4.8":
+			arch => "x86_64",
+			ver => "4.8";
+		"centos_i386_5.5":
+			arch => "i386",
+			ver => "5.5";
+		"centos_x86_64_5.5":
+			arch => "x86_64",
+			ver => "5.5";
+	}
+	
+}
+
+define centosimages($arch,$ver) {
+	
+	$dirs = [
+		"${tftp_root}/images/centos/${arch}/${ver}",
+	]
+
+	file { $dirs: ensure => directory, owner => root, group => root, mode => 755; }
+
+	exec {
+		"pull centos pxe vmlinuz $arch $ver":
+			cwd => "${tftp_root}/images/centos/${arch}/${ver}",
+			command => "/usr/bin/wget http://mirrors.kernel.org/centos/${ver}/os/${arch}/images/pxeboot/vmlinuz",
+			creates => "${tftp_root}/images/centos/${arch}/${ver}/vmlinuz";
+		"pull centos pxe initrd.img $arch $ver":
+			cwd => "${tftp_root}/images/centos/${arch}/${ver}",
+			command => "/usr/bin/wget http://mirrors.kernel.org/centos/${ver}/os/${arch}/images/pxeboot/initrd.img",
+			creates => "${tftp_root}/images/centos/${arch}/${ver}/initrd.img";
+	}
+
+	file {
+		"${ks_root}/centos_${arch}_${ver}.cfg":
+			owner => root, group => root, mode => 644, content => template("pxe/centos_ks.cfg.erb");
+	}
+
+  #fragment{ "cent-${title}":
+  #  filename   => "${tftp_root}/pxelinux.cfg/cent",
+  #  content    => template("pxe/centosPXE-entry"),
+  #}
+
 
 }
+
--- dist/pxe/manifests/params.pp
+++ dist/pxe/manifests/params.pp
@@ -1,6 +1,7 @@
 class pxe::params {
 
 	$tftp_root = "/tftpboot"
+	$ks_root = "/var/www/html/ks"
 
 
 }

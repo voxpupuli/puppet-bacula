657f81aad33ae4546921dfbee01a07072220e5c2
Zach <zach@puppetlabs.com>
2010-11-21 16:24:22 -0500

    add concat module

---
+++ dist/concat/manifests/setup.pp
@@ -0,0 +1,29 @@
+# Sets up the concat system, you should set $concatdir to a place
+# you wish the fragments to live, this should not be somewhere like
+# /tmp since ideally these files should not be deleted ever, puppet
+# should always manage them
+#
+# It also copies out the concatfragments.sh file to /usr/local/bin
+class concat::setup {
+    $concatdir = "/var/lib/puppet/concat"
+    $sortpath = $operatingsystem ? {
+        darwin => "/usr/bin/sort",
+        default => "/bin/sort",
+    }
+
+    file{"/usr/local/bin/concatfragments.sh": 
+            owner  => root,
+            group  => 0,
+            mode   => 755,
+            #source => "puppet:///modules/concat/concatfragments.sh";
+            content => template("concat/concatfragments.sh");
+
+         $concatdir: 
+            ensure => directory,
+            owner  => root,
+            group  => 0,
+            mode   => 755;
+    }
+}
+
+# vi:tabstop=4:expandtab:ai

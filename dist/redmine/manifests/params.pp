7e8bb133dc2a69d45725738487e705311657a1ea
Zach <zach@puppetlabs.com>
2010-12-21 15:34:09 -0500

    modify version

--- dist/redmine/manifests/params.pp
+++ dist/redmine/manifests/params.pp
@@ -1,5 +1,5 @@
 class redmine::params {
-  $version = 'bc951a9d4f849099f907c5a01b066b75f9d7bf4c'
+  $version = '84dd413f22b9a3900cea'
   $source  = 'git://github.com/edavis10/redmine'
   $rails_version = '2.3.5'
 }

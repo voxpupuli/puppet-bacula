8aefcc2001af3955afc0a41c82eb738648111519
Zach <zach@puppetlabs.com>
2010-12-21 15:47:12 -0500

    add vhosts for prosvcs and pm on legba

--- site/puppetlabs/manifests/legba.pp
+++ site/puppetlabs/manifests/legba.pp
@@ -13,8 +13,55 @@
 class puppetlabs::legba {
 
   include puppetlabs
+  include puppetlabs_ssl
   include account::master
   include collectd::client
+
+	$ssl_path = $puppetlabs_ssl::params::ssl_path
+
+  apache::vhost {'ps.puppetlabs.com':
+	  serveraliases => "ps.puppetlabs.com",
+    port => 80,
+    docroot => '/opt/prosvc',
+    ssl => false,
+    priority => 10,
+    template => 'puppetlabs/legba.conf.erb',
+  }
+
+  apache::vhost {'ps.puppetlabs.com_ssl':
+	  serveraliases => "ps.puppetlabs.com",
+    port => 443,
+    docroot => '/opt/prosvc',
+    ssl => true,
+		auth => true,
+    priority => 11,
+    template => 'puppetlabs/legba.conf.erb',
+  }
+
+	file { "/opt/prosvc": ensure => directory, owner => root, group => prosvc, mode => 664, recurse => true, checksum => none; }
+	file { "/opt/prosvc/.htaccess": owner => root, group => root, mode => 644, source => "puppet:///modules/puppetlabs/legba_htaccess"; }
+  
+	apache::vhost {'pm.puppetlabs.com':
+	  serveraliases => "pm.puppetlabs.com",
+    port => 80,
+    docroot => '/opt/pm',
+    ssl => false,
+    priority => 15,
+    template => 'puppetlabs/legba.conf.erb',
+  }
+
+  apache::vhost {'pm.puppetlabs.com_ssl':
+	  serveraliases => "pm.puppetlabs.com",
+    port => 443,
+    docroot => '/opt/pm',
+    ssl => true,
+		auth => true,
+    priority => 16,
+    template => 'puppetlabs/legba.conf.erb',
+	}
+
+	file { "/opt/pm": ensure => directory, owner => root, group => prosvc, mode => 664, recurse => true, checksum => none; }
+	file { "/opt/pm/.htaccess": owner => root, group => root, mode => 644, source => "puppet:///modules/puppetlabs/legba_htaccess"; }
 
 	Account::User <| tag == 'prosvc' |>
 	Group <| tag == 'prosvc' |>

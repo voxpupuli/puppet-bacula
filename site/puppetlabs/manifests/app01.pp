89fa05d198720cea9d7f1e6ff0db4ff68edf5d14
Zach <zach@puppetlabs.com>
2011-01-07 15:30:04 -0500

    remove old comments

--- site/puppetlabs/manifests/app01.pp
+++ site/puppetlabs/manifests/app01.pp
@@ -1,40 +1,6 @@
 class puppetlabs::app01 {
-
+	include puppetlabs::lan
+	include puppetlabs::dev
 	include patchwork
-#	include pkgs::admin
-#	include django
-#  
-#	$mysql_root_pw = 'aksjdhaskjhasdKJHASkjhasd'
-#	include mysql::server
-#
-#  apache::vhost {'app01.puppetlabs.lan':
-#    port => 80,
-#    docroot => '/var/www',
-#    ssl => false,
-#    priority => 10,
-#    template => 'puppetlabs/patchwork.conf.erb',
-#  }
-#	$www_data_passwd = 'aslkdjasdGSDMbmsbdak'
-#	$nobody_passwd = 'alskdjalskdjaloqiueoqiwmnzbxcvmnxzcbv'
-#
-#	database_user{'www-data@localhost':
-#		ensure => present,
-#		password_hash => mysql_password($www_data_passwd)
-#	}
-#
-#  mysql::db{"patchwork":
-#    db_user => 'patchwork',
-#    db_pw => 'kjasdjajsdb',
-#  }
-#
-#	package { "python-mysqldb": ensure => installed; }
-#	package { "python-django-registration": ensure => installed; }
-#	
-#	file { "/var/www/apps/local_settings.py":
-#		owner => root,
-#		group => www-data,
-#		mode => 640,
-#		source => "puppet:///modules/patchwork/local_settings.py";
-#	}
 
 }

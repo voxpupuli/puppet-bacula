53e3ec8bf293dc454b57bd171702a70a849f231a
Zach <zach@puppetlabs.com>
2011-01-12 18:49:54 -0500

    align and improve

--- site/virtual/manifests/groups.pp
+++ site/virtual/manifests/groups.pp
@@ -1,26 +1,43 @@
 class virtual::groups {
+	@group {'allstaff':
+    ensure => present,
+    gid    => 600,
+	}
   @group {'sysadmin':
     ensure => present,
-    gid => 666,
+    gid    => 666,
   }
   @group {'developers':
     ensure => present,
-    gid => 667,
+    gid    => 667,
   }
   @group {'prosvc':
     ensure => present,
-    gid => 668,
+    gid    => 668,
   }
+	@group {'operations':
+		ensure => present,
+		gid    => 673,
+  }
+	@group {'git':
+		ensure => present,
+		gid    => 674,
+	}
+
+#
+# Service Groups
+#
   @group {'hudson':
     ensure => present,
-    gid => 669,
+    gid    => 620,
   }
   @group {'osqa':
     ensure => present,
-    gid => 670,
+    gid    => 621,
   }
   @group {'patchwork':
     ensure => present,
-    gid => 671,
+    gid    => 622,
   }
 }
+

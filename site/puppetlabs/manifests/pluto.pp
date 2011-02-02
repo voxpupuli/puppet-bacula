28c7c3deea99d295f4a9aefd74b09d308a988ab2
Zach <zach@puppetlabs.com>
2011-02-01 19:35:38 -0500

    add luks encryption support

--- site/puppetlabs/manifests/pluto.pp
+++ site/puppetlabs/manifests/pluto.pp
@@ -4,6 +4,16 @@ class puppetlabs::pluto {
 	ssh::allowgroup { "developers": }
 	ssh::allowgroup { "prosvc": }
 
+	package { "cryptsetup": ensure => installed; }
+
+	exec { "/bin/dd if=/dev/urandom of=/var/chroot.key bs=512 count=4":
+		creates => '/var/chroot.key';
+	}
+	
+	file { 
+		"/var/chroot.key": mode => 0400, require => Exec["/bin/dd if=/dev/urandom of=/var/chroot.key bs=512 count=4"];
+	}
+
 	file {
 		"/opt/enterprise": 
 		  owner => root,

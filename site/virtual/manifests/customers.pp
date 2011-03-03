1ba28f3d39a66991512a9748e377e4df053b855e
Zach <zach@puppetlabs.com>
2011-03-02 17:29:43 -0800

    add motorola account for davepark

--- site/virtual/manifests/customers.pp
+++ site/virtual/manifests/customers.pp
@@ -2,10 +2,19 @@ class virtual::customers {
   include virtual::groups
   @account::user {'menglund':
     comment => 'Martin Englund',
-		usekey  => false,
+    usekey  => false,
     group   => vmware,
     tag     => customer,
   }
+
+  @account::user {'davepark':
+    comment => 'Dave Park',
+    group   => motorola,
+    tag     => customer,
+    key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAo/r6KjkRGGR98bTfaNAOExKFJkPTvq2RyTBPEahcm25Lj15KWsT1f4BKUJirlwqe6RerM5DU8h/Nj8WbFJioG09P79oXfJjos801tlSnyEWBxZnHk6ijwKs/R9FVRPp70JMI0BhyC6+3pJfPs71zQdpWQBFaUxTVvZaaazJm8mO0kFBOqWxoWDegf1NxYDmsBL78VAsOYwlI28FrV46LMi/gO8Y0Q1H2qFvE1iJQpeN7fx90/7GiSTsNCBhdUJZsZBSID/WzXRqCFTD5pF0e6Gm6cxz/A3HzsrT6aUgOlfI8nql3AYRG7p8HiCiXKFURCfPVfoIXay1VRo7pLEMxyw==",
+    keytype => "ssh-rsa",
+  }
+
 
 }
 

8c19e22776f07f8884e28cd10fe94e6030da81f8
Zach <zach@puppetlabs.com>
2011-03-24 11:09:14 -0700

    add blog.pupptetlabs.com redirect to www.puppetlabs.com/blog

--- site/puppetlabs/manifests/www.pp
+++ site/puppetlabs/manifests/www.pp
@@ -36,20 +36,28 @@ class puppetlabs::www {
 			ensure => link,
 			target => "centos-5.5-pe-1.0-ovf.tar.gz";
 	}
-		
+
 
   # Mysql
   $mysql_root_pw = 'afmesackjebhee'
   include mysql::server
   include postfix
-  wordpress::instance {'puppetlabs.com':
-    auth_key => 'PE{TEN%T).U~V6Cl;b_?0mcrvhoUVIP#+0R|e-LB>00:o*((b%[8pve/1Y+H}P(o',
-    secure_auth_key => 'PDqD)bN|B22D.hxk@Uvy;nkT0#9QVB8=~J^r3@9f:7gRn9PmNGBth(t2+hLt|=Ne',
-    logged_in_key => '#_Y3SS3oBj(<ja{dW+#fE!{=YhoiP<0@m~e)Gp[d0j5x1OxGAAFjl|3yHzmH{srZ',
-    nonce_key => 'P,>pH-J+OTw#z2qn`M[lt||`[Nf|w#I:J %z>-MRY@Yt_Egyj84znb2H*s;0J||3',
-    db_pw => 'gosh-if-no-hull',
-    template => 'puppetlabs/puppetlabs_vhost.conf.erb',
+
+  apache::vhost::redirect {
+    'blog.puppetlabs.com':
+      port => '80',
+      dest => 'http://www.puppetlabs.com/blog'
   }
+
+  wordpress::instance {'puppetlabs.com':
+    auth_key        => 'PE{TEN%T).U~V6Cl;b_?0mcrvhoUVIP#+0R|e-LB>00:o*((b%[8pve/1Y+H}P(o',
+    secure_auth_key => 'PDqD)bN|B22D.hxk@Uvy;nkT0#9QVB8=~J^r3@9f:7gRn9PmNGBth(t2+hLt|=Ne',
+    logged_in_key   => '#_Y3SS3oBj(<ja{dW+#fE!{=YhoiP<0@m~e)Gp[d0j5x1OxGAAFjl|3yHzmH{srZ',
+    nonce_key       => 'P,>pH-J+OTw#z2qn`M[lt||`[Nf|w#I:J %z>-MRY@Yt_Egyj84znb2H*s;0J||3',
+    db_pw           => 'gosh-if-no-hull',
+    template        => 'puppetlabs/puppetlabs_vhost.conf.erb',
+  }
+
   package {'php5-curl': ensure => present, notify => Service[httpd] }
   realize(A2mod['headers'])
   realize(A2mod['expires'])

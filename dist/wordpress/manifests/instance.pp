3990721bb5ca03c076aa44020acb71451dfd0fce
Zach <zach@puppetlabs.com>
2011-03-28 13:51:31 -0700

    add backup option to wordpress::instance for the ability to turn it off

--- dist/wordpress/manifests/instance.pp
+++ dist/wordpress/manifests/instance.pp
@@ -1,25 +1,42 @@
 #
 # installs an instance of wordpress
 #
-define wordpress::instance( $auth_key, $secure_auth_key, $logged_in_key, $nonce_key, $dir='/var/www/', $port='80', $db_pw, $template, $priority = '00') {
+define wordpress::instance( 
+    $auth_key, 
+    $secure_auth_key, 
+    $logged_in_key, 
+    $nonce_key, 
+    $dir='/var/www/', 
+    $port='80', 
+    $db_pw, 
+    $template, 
+    $priority = '00',
+    $backup = true,
+  ) {
+
   include wordpress
   $dbname = regsubst($name, '\.', '', 'G')
   $vhost_dir = "${dir}/${name}"
   file{[$dir, $vhost_dir]:
     ensure => directory,
   }
+
   mysql::db{$dbname:
     db_user => $dbname,
     db_pw => $db_pw,
   }
+
   apache::vhost{$name:
     port    => $port,
     docroot => $vhost_dir,
     template => $template,
     priority => $priority,
   }
-  bacula::mysql { $dbname: }
-  #
+
+  if $backup == true {
+    bacula::mysql { $dbname: }
+  }
+  
   # Develop a wordpress versioning/staging strategy.
   #
 }

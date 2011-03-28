1c1c67f90d6e8112056ca999087d6cd42b7c5b39
Zach <zach@puppetlabs.com>
2011-03-28 13:50:51 -0700

    remove hudson access to shell turn off bacula information for wordpress instance

--- site/puppetlabs/manifests/shell.pp
+++ site/puppetlabs/manifests/shell.pp
@@ -12,7 +12,7 @@
 #
 class puppetlabs::shell {
 	ssh::allowgroup { "allstaff": }
-	ssh::allowgroup { "hudson": }
+  #ssh::allowgroup { "hudson": }
 
   # Mysql
   $mysql_root_pw = 'c@11-m3-m1st3r-p1t4a1'
@@ -26,7 +26,7 @@ class puppetlabs::shell {
   #$dashboard_site = 'shell.puppetlabs.com'
   #include puppet::server
   #include puppet::dashboard
-	include hudson::slave
+  #include hudson::slave
 
 	include pkgs::admin
 	include apache
@@ -37,18 +37,20 @@ class puppetlabs::shell {
 	# Collectd
 	include collectd::client
 
-	$bacula_director = 'baal.puppetlabs.com'
-	$bacula_password = 'aNXF2C1LFdptETvihkk/z'
+  #$bacula_director = 'baal.puppetlabs.com'
+  #$bacula_password = 'aNXF2C1LFdptETvihkk/z'
   #include bacula
 
   #include postfix
-  wordpress::instance {'dev':
-    auth_key => 'PE{TEN%T).U~V6Cl;b_?0mcrvhoUVIP#+0R|e-LB>00:o*((b%[8pve/1Y+H}P(o',
-    secure_auth_key => 'PDqD)bN|B22D.hxk@Uvy;nkT0#9QVB8=~J^r3@9f:7gRn9PmNGBth(t2+hLt|=Ne',
-    logged_in_key => '#_Y3SS3oBj(<ja{dW+#fE!{=YhoiP<0@m~e)Gp[d0j5x1OxGAAFjl|3yHzmH{srZ',
-    nonce_key => 'P,>pH-J+OTw#z2qn`M[lt||`[Nf|w#I:J %z>-MRY@Yt_Egyj84znb2H*s;0J||3',
-    db_pw => 'gosh-if-no-hull',
-    template => 'puppetlabs/dev_vhost.conf.erb',
+  wordpress::instance {
+    'dev':
+      auth_key        => 'PE{TEN%T).U~V6Cl;b_?0mcrvhoUVIP#+0R|e-LB>00:o*((b%[8pve/1Y+H}P(o',
+      secure_auth_key => 'PDqD)bN|B22D.hxk@Uvy;nkT0#9QVB8=~J^r3@9f:7gRn9PmNGBth(t2+hLt|=Ne',
+      logged_in_key   => '#_Y3SS3oBj(<ja{dW+#fE!{=YhoiP<0@m~e)Gp[d0j5x1OxGAAFjl|3yHzmH{srZ',
+      nonce_key       => 'P,>pH-J+OTw#z2qn`M[lt||`[Nf|w#I:J %z>-MRY@Yt_Egyj84znb2H*s;0J||3',
+      db_pw           => 'gosh-if-no-hull',
+      template        => 'puppetlabs/dev_vhost.conf.erb',
+      backup          => false,
   }
   package {'php5-curl': ensure => present, notify => Service[httpd] }
   #realize(A2mod['headers'])

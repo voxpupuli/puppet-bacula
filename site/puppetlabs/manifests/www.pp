ec640cda80c19b0d791d7d132c0c9f6f64f0fa12
Zach <zach@puppetlabs.com>
2011-03-31 11:47:25 -0700

    change all the wordpress instance passwords in production

--- site/puppetlabs/manifests/www.pp
+++ site/puppetlabs/manifests/www.pp
@@ -30,13 +30,12 @@ class puppetlabs::www {
       recurse  => true,
       checksum => none;
     "/var/www/puppetlabs.com/downloads/training/current-pm-vm.tar.gz":
-      ensure => link,
-      target => "centos-5.5-pe-1.0.tar.gz";
+      ensure   => link,
+      target   => "centos-5.5-pe-1.0.tar.gz";
     "/var/www/puppetlabs.com/downloads/training/current-pm-vm-ovf.tar.gz":
-      ensure => link,
-      target => "centos-5.5-pe-1.0-ovf.tar.gz";
+      ensure   => link,
+      target   => "centos-5.5-pe-1.0-ovf.tar.gz";
   }
-
 
   # Mysql
   $mysql_root_pw = 'afmesackjebhee'
@@ -46,16 +45,24 @@ class puppetlabs::www {
   apache::vhost::redirect {
     'blog.puppetlabs.com':
       port => '80',
-      dest => 'http://www.puppetlabs.com/blog'
+      dest => 'http://www.puppetlabs.com/blog';
+    'puppetcamp.org':
+      port => '80',
+      dest => 'http://www.puppetlabs.com/community/puppet-camp';
   }
 
-  wordpress::instance {'puppetlabs.com':
-    auth_key        => 'PE{TEN%T).U~V6Cl;b_?0mcrvhoUVIP#+0R|e-LB>00:o*((b%[8pve/1Y+H}P(o',
-    secure_auth_key => 'PDqD)bN|B22D.hxk@Uvy;nkT0#9QVB8=~J^r3@9f:7gRn9PmNGBth(t2+hLt|=Ne',
-    logged_in_key   => '#_Y3SS3oBj(<ja{dW+#fE!{=YhoiP<0@m~e)Gp[d0j5x1OxGAAFjl|3yHzmH{srZ',
-    nonce_key       => 'P,>pH-J+OTw#z2qn`M[lt||`[Nf|w#I:J %z>-MRY@Yt_Egyj84znb2H*s;0J||3',
-    db_pw           => 'gosh-if-no-hull',
-    template        => 'puppetlabs/puppetlabs_vhost.conf.erb',
+  wordpress::instance {
+    'puppetlabs.com':
+#      auth_key        => 'PE{TEN%T).U~V6Cl;b_?0mcrvhoUVIP#+0R|e-LB>00:o*((b%[8pve/1Y+H}P(o',
+#      secure_auth_key => 'PDqD)bN|B22D.hxk@Uvy;nkT0#9QVB8=~J^r3@9f:7gRn9PmNGBth(t2+hLt|=Ne',
+#      logged_in_key   => '#_Y3SS3oBj(<ja{dW+#fE!{=YhoiP<0@m~e)Gp[d0j5x1OxGAAFjl|3yHzmH{srZ',
+#      nonce_key       => 'P,>pH-J+OTw#z2qn`M[lt||`[Nf|w#I:J %z>-MRY@Yt_Egyj84znb2H*s;0J||3',
+      auth_key        => 'rspk1YxmxcUDIqhjH1pUQq59WCTvISTXlflU7UZoUFw8faCzNy1VC8Uq9sl0gxg',
+      secure_auth_key => 'MJQKXGrbtSs4toOG2JsqjrYopYU9Ij9wQYX5kttycvTwEfc9uGFDxo6leeLCITL',
+      logged_in_key   => 't7O5y1MW41ZT4jETgJkdCUn35f8fzQgfnZ9sivR4k8M5kU6U17I6GhA73NtLVLd',
+      nonce_key       => 'ReRyvF7pScI6OAhGQaDaSgNnXsKBkutqshJxc2a4PRmXEDEVzNarEYJenhvDQIq',
+      db_pw           => 'X2i8nRTE',
+      template        => 'puppetlabs/puppetlabs_vhost.conf.erb',
   }
 
   package {'php5-curl': ensure => present, notify => Service[httpd] }

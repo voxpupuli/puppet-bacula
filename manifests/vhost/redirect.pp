6ad34e3e0e73719100ad830f806d368d5292021c
Zach <zach@puppetlabs.com>
2011-03-31 14:35:50 -0700

    fix to last

--- manifests/vhost/redirect.pp
+++ manifests/vhost/redirect.pp
@@ -22,7 +22,7 @@ define apache::vhost::redirect (
     $port,
     $dest,
     $priority = '10',
-    $serveraliases,
+    $serveraliases='',
     $template = "apache/vhost-redirect.conf.erb"
     ) {
 

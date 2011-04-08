140c89e6883cd07769d0101651d3e84524380990
Zach Leslie <zach@puppetlabs.com>
2011-04-08 16:09:21 -0700

    add www-dev group and include dansupinski

--- site/virtual/manifests/users/external.pp
+++ site/virtual/manifests/users/external.pp
@@ -4,7 +4,8 @@ class virtual::users::external {
     'dansupinski':
       comment => 'Dan Supinski',
       group   => contractors,
-      key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAsJ1AN1iOfjhQTUF6wIMwckmPXgqEFDkEwMEOXUVOX/zyFxdXlBqspTgiQTCGLHsW11CIRVsFYEAqtAubH4JSPbYaOwKcpao4TLH6SXWZxbmwYrhg2Qil2lqINVEfff2xAM11EhsCdzGpB8a3uhKMTT5EfJcXge19LQDNAXA7drxywJVWwgtCYzW1L6uYWaY+skMwSTl125QnC+A/XYim024m29eBvIQU+dp1D9+PJaE9Nl/sZYX2yryY+t97HyCL4LBDHhq2CXT1jaJK+lZxObyiLMA6rybA021akvBTkblNvYmdelq/MRtUxEG2XswytWmRlg6YetLzuZ8tcZElQQ==",
+      groups  => ["www-dev"],
+      key     => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAsJ1AN1iOfjhQTUF6wIMwckmPXgqEFDkEwMEOXUVOX/zyFxdXlBqspTgiQTCGLHsW11CIRVsFYEAqtAubH4JSPbYaOwKcpao4TLH6SXWZxbmwYrhg2Qil2lqINVEfff2xAM11EhsCdzGpB8a3uhKMTT5EfJcXge19LQDNAXA7drxywJVWwgtCYzW1L6uYWaY+skMwSTl125QnC+A/XYim024m29eBvIQU+dp1D9+PJaE9Nl/sZYX2yryY+t97HyCL4LBDHhq2CXT1jaJK+lZxObyiLMA6rybA021akvBTkblNvYmdelq/MRtUxEG2XswytWmRlg6YetLzuZ8tcZElQQ==',
       keytype => "ssh-rsa",
       expire  => "2011-10-01",
   }

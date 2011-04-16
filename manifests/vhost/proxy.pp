6061a62671ecbb7cbf1237f95cb3dc7b5484a978
Zach Leslie <zach@puppetlabs.com>
2011-04-16 14:16:06 -0700

    add ssl support to apache::vhost::proxy class fix enkal to support ssl

--- manifests/vhost/proxy.pp
+++ manifests/vhost/proxy.pp
@@ -2,11 +2,19 @@ define apache::vhost::proxy (
     $port,
     $dest,
     $priority = '10',
-    $template = "apache/vhost-proxy.conf.erb"
+    $template = "apache/vhost-proxy.conf.erb",
+    $servername    = '',
+    $serveraliases = '',
+    $ssl = false
     ) {
+
   include apache
 
   $srvname = $name
+
+  if $ssl == true {
+    include apache::ssl
+  }
 
   file {"${apache::params::vdir}/${priority}-${name}":
     content => template($template),

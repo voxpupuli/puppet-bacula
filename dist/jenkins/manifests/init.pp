02adffbb19fb071e620e012452ee1677c8b28784
Zach Leslie <zach@puppetlabs.com>
2011-04-26 16:16:21 -0700

    remove hudson module remove jenkins.war file, using packages now

--- dist/jenkins/manifests/init.pp
+++ dist/jenkins/manifests/init.pp
@@ -3,6 +3,8 @@
 # This class installs and configures jenkins
 #
 # Parameters:
+# * $site_alias
+#   The hostname that the site will be reachable by
 #
 # Actions:
 #
@@ -17,11 +19,11 @@
 # Apache modules should be moved to apache class and realized when needed
 # Test for distro, currently only apt based 
 
-
 class jenkins ($site_alias) {
   include jenkins::params
-  #include apache
   include git
+
+  nagios::website { "$site_alias": }
 
   exec { "Import jenkins apt-key":
     path        => "/bin:/usr/bin",
@@ -36,45 +38,7 @@ class jenkins ($site_alias) {
 
   package { "jenkins": ensure => installed, require => File["/etc/apt/sources.list.d/jenkins-ci.list"]; }
 
-#  package { $jenkins::params::jetty_packages: 
-#    ensure => installed,
-#  }
-#
-#  package { $jenkins::params::build_packages_gems:
-#    ensure => installed,
-#    provider => gem,
-#  }
-#
-#  # war retrieved from http://mirrors.jenkins-ci.org/war/latest/
-#  file { 'jenkins.war':
-#    path => '/usr/share/jetty/webapps/root.war',
-#    source => 'puppet:///modules/jenkins/jenkins.war',
-#    owner => 'jetty',
-#    group => 'adm',
-#    require => Package[$jenkins::params::jetty_packages],
-#    notify => Service['jetty'],
-#  }
-#
-#  file { '/etc/default/jetty':
-#    source => 'puppet:///modules/jenkins/jetty.default',
-#  }
-#
-#  service { 'jetty':
-#    ensure => running,
-#    enable => true, 
-#    hasstatus => true,
-#    require => [ File['/etc/default/jetty'], Package[$jenkins::params::jetty_packages] ],
-#  }
-#
   a2mod { [ 'proxy', 'proxy_http', 'proxy_balancer', 'proxy_ajp' ]: ensure => present, }
-#
-#  apache::vhost { 'ci.puppetlabs.com':
-#    port => '80',
-#    priority => '10',
-#    docroot => 'null',
-#    template => 'jenkins/jenkins-apache.conf.erb',
-#    require => [ Service['jetty'], A2mod['proxy'] ]
-#  }
 
   apache::vhost::redirect {
     "${site_alias}":

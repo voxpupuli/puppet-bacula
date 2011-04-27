3ef164f490b9bc2028507a656b15fb3569a85d0f
Zach Leslie <zach@puppetlabs.com>
2011-04-27 16:39:10 -0700

    imrpot apt key on debian and ubuntu

--- site/puppetlabs/manifests/os/linux/debian.pp
+++ site/puppetlabs/manifests/os/linux/debian.pp
@@ -5,4 +5,11 @@ class puppetlabs::os::linux::debian {
     "keychain":    ensure => installed; 
   }
 
+  exec { 
+    "import puppet labs apt key":
+      user    => root,
+      command => "/usr/bin/wget -q -O - http://apt.puppetlabs.com/ops/4BD6EC30.asc | apt-key add -",
+      unless  => "/usr/bin/apt-key list | grep -q 4BD6EC30",
+  }
+
 }
--- site/puppetlabs/manifests/os/linux/ubuntu.pp
+++ site/puppetlabs/manifests/os/linux/ubuntu.pp
@@ -5,5 +5,12 @@ class puppetlabs::os::linux::ubuntu {
     "keychain":    ensure => installed; 
   }
 
+  exec { 
+    "import puppet labs apt key":
+      user    => root,
+      command => "/usr/bin/wget -q -O - http://apt.puppetlabs.com/ops/4BD6EC30.asc | apt-key add -",
+      unless  => "/usr/bin/apt-key list | grep -q 4BD6EC30",
+  }
+
 }
 

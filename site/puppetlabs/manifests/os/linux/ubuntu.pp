61773075d4d75a70c37d1f4ab731a57fe0f960e8
Zach Leslie <zach@puppetlabs.com>
2011-04-27 17:00:14 -0700

    create the ops.list apt source for all ubuntu and debian systems temporary measure to get all systems up to the same version to make use of the lsbdistid facter value for future better apt source management which will require this functionality

--- site/puppetlabs/manifests/os/linux/ubuntu.pp
+++ site/puppetlabs/manifests/os/linux/ubuntu.pp
@@ -10,6 +10,17 @@ class puppetlabs::os::linux::ubuntu {
       user    => root,
       command => "/usr/bin/wget -q -O - http://apt.puppetlabs.com/ops/4BD6EC30.asc | apt-key add -",
       unless  => "/usr/bin/apt-key list | grep -q 4BD6EC30",
+      before  => Exec["apt-get update"];
+    "apt-get update":
+      user => root,
+      command => "/usr/bin/apt-get -qq update",
+      refreshonly => true;
+  }
+
+  file {
+    "/etc/apt/sources.list.d/ops.list": 
+      content => "deb http://apt.puppetlabs.com/ops sid main\n",
+      notify => Exec["apt-get update"];
   }
 
 }

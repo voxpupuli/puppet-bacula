28543af912c5c23fb0102528658a5e5f32f0f5be
Zach Leslie <zach@puppetlabs.com>
2011-05-03 18:22:20 -0700

    initial import and refactor of old ha code

---
+++ dist/heartbeat/manifests/key.pp
@@ -0,0 +1,14 @@
+define heartbeat::key (
+    $key_id = 0,
+    $key = "44d4379a0e273b7a09d81fc55fc57d17ecd4cd46",
+    $key_type = "sha1"
+  ){
+
+  concat::fragment { "authkey-$key_id":
+    target => '/etc/heartbeat/authkeys',
+    content => template("heartbeat/authkey.erb"),
+  }
+
+
+}
+

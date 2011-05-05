1dd4097946b4bbd87befda1da7242077f5a3d389
Zach Leslie <zach@puppetlabs.com>
2011-05-05 15:15:45 -0700

    add heartbeat to wyrd

--- site/puppetlabs/manifests/wyrd.pp
+++ site/puppetlabs/manifests/wyrd.pp
@@ -13,4 +13,17 @@ class puppetlabs::wyrd {
       docroot => '/opt/www'
   }
 
+  class { "heartbeat": 
+    nodes => "wyrd vanir",
+    network => "ucast eth1 192.168.50.6"
+  }
+
+  heartbeat::authkey { "1": key_id => "1"; }
+
+  heartbeat::resource {
+    "server01": 
+      resource => "wyrd IPaddr::192.168.100.15/24/eth0:0";
+  }
+
 }
+

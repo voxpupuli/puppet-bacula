57b606dd1636297d7bcc59b1ebf2f8d65a6b3286
Zach Leslie <zach@puppetlabs.com>
2011-05-05 15:56:11 -0700

    add the ability to ignore apt-cacher-ng service for ha reasons add ha to vanir install apt-cacher on wyrd for ha environment

--- site/puppetlabs/manifests/vanir.pp
+++ site/puppetlabs/manifests/vanir.pp
@@ -1,4 +1,18 @@
 class puppetlabs::vanir {
-  include apt-cacher
+
+  class { "apt-cacher": service => false; }
+
+  class { "heartbeat":
+    nodes => "wyrd vanir",
+    network => "ucast eth1 192.168.50.5"
+  }
+
+  heartbeat::authkey { "1": key_id => "1"; }
+
+  heartbeat::resource {
+    "server01":
+      resource => "wyrd IPaddr::192.168.100.15/24/eth0:0";
+  }
+
 
 }

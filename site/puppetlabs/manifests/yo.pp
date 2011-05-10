ca55cfea68a9ac1ee78225537e7d0dbd3af389e2
Zach Leslie <zach@puppetlabs.com>
2011-05-10 13:36:28 -0700

    add rhel 5.5 to mrepo

--- site/puppetlabs/manifests/yo.pp
+++ site/puppetlabs/manifests/yo.pp
@@ -27,4 +27,22 @@ class puppetlabs::yo {
     iso       => 'rhel-server-6.0-$arch-dvd.iso',
     updates   => 'rhns:///rhel-$arch-server-6',
   }
+  mrepo::repo { "5server-x86_64":
+    ensure    => present,
+    rhn       => true,
+    repotitle => "RHEL server 5 64 bit",
+    arch      => "x86_64",
+    release   => "5Server",
+    iso       => 'rhel-server-5.5-$arch-dvd.iso',
+    updates   => 'rhns:///rhel-$arch-server-5',
+  }
+  mrepo::repo { "5server-i386":
+    ensure    => present,
+    rhn       => true,
+    repotitle => "RHEL server 5 32 bit",
+    arch      => "i386",
+    release   => "5Server",
+    iso       => 'rhel-server-5.5-$arch-dvd.iso',
+    updates   => 'rhns:///rhel-$arch-server-5',
+  }
 }

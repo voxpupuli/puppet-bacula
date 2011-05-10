8017bbd98cebda9469a3d8ddaae8525c97ca619b
Zach Leslie <zach@puppetlabs.com>
2011-05-10 14:54:16 -0700

    simplify the mrepo stuff

--- site/puppetlabs/manifests/yo.pp
+++ site/puppetlabs/manifests/yo.pp
@@ -9,40 +9,22 @@ class puppetlabs::yo {
     isoroot       => '/var/mrepo/iso',
   }
 
-  mrepo::repo { "6server-x86_64":
+  mrepo::repo { "rhel6s":
     ensure    => present,
     rhn       => true,
-    repotitle => "RHEL server 6 64 bit",
-    arch      => "x86_64",
-    release   => "6Server",
+    repotitle => 'Red Hat Enterprise Linux Server $release ($arch)',
+    arch      => "i386 x86_64",
+    release   => "6",
     iso       => 'rhel-server-6.0-$arch-dvd.iso',
     updates   => 'rhns:///rhel-$arch-server-6',
   }
-  mrepo::repo { "6server-i386":
+  mrepo::repo { "rhel5.6s":
     ensure    => present,
     rhn       => true,
-    repotitle => "RHEL server 6 32 bit",
-    arch      => "i386",
-    release   => "6Server",
-    iso       => 'rhel-server-6.0-$arch-dvd.iso',
-    updates   => 'rhns:///rhel-$arch-server-6',
-  }
-  mrepo::repo { "5server-x86_64":
-    ensure    => present,
-    rhn       => true,
-    repotitle => "RHEL server 5 64 bit",
-    arch      => "x86_64",
-    release   => "5Server",
-    iso       => 'rhel-server-5.5-$arch-dvd.iso',
-    updates   => 'rhns:///rhel-$arch-server-5',
-  }
-  mrepo::repo { "5server-i386":
-    ensure    => present,
-    rhn       => true,
-    repotitle => "RHEL server 5 32 bit",
-    arch      => "i386",
-    release   => "5Server",
-    iso       => 'rhel-server-5.5-$arch-dvd.iso',
+    repotitle => 'Red Hat Enterprise Linux Server $release ($arch)',
+    arch      => "i386 x86_64",
+    release   => "5.6",
+    iso       => 'rhel-server-5.6-$arch-dvd.iso',
     updates   => 'rhns:///rhel-$arch-server-5',
   }
 }

a48062d0ad3a6484cf018aee59c49841a86d0551
Zach Leslie <zach@puppetlabs.com>
2011-05-11 14:54:16 -0700

    leave the version on the tar

--- dist/graphite/manifests/params.pp
+++ dist/graphite/manifests/params.pp
@@ -2,13 +2,13 @@ class graphite::params {
   $build_dir = "/tmp/graphite_build_root"
 
   $whisper_dl_url = "http://launchpad.net/graphite/1.0/0.9.8/+download/whisper-0.9.8.tar.gz"
-  $whisper_dl_loc = "whisper.tar.gz"
+  $whisper_dl_loc = "whisper.tar-0.9.8.gz"
 
   $webapp_dl_url = "http://launchpad.net/graphite/1.0/0.9.8/+download/graphite-web-0.9.8.tar.gz"
-  $webapp_dl_loc = "graphite-web.tar.gz"
+  $webapp_dl_loc = "graphite-web-0.9.8.tar.gz"
 
   $carbon_dl_url = "http://launchpad.net/graphite/1.0/0.9.8/+download/carbon-0.9.8.tar.gz"
-  $carbon_dl_loc = "carbon.tar.gz"
+  $carbon_dl_loc = "carbon-0.9.8.tar.gz"
 
   $install_prefix = "/opt/"
 
@@ -18,3 +18,4 @@ class graphite::params {
   }
 
 }
+

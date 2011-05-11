4058452e43eb086eb0df87be4238342f95dec26d
Zach Leslie <zach@puppetlabs.com>
2011-05-11 14:44:53 -0700

    add refactored https://github.com/spikelab/puppet-graphite

--- dist/graphite/manifests/init.pp
+++ dist/graphite/manifests/init.pp
@@ -1,23 +1,24 @@
-class graphite {
-
+class graphite (
+    $site_alias = $fqdn
+  ) {
 
 # TODO:
 #  /opt/graphite/conf/carbon.conf
 #  /opt/graphite/conf/storage-schemas.conf
-#  /etc/apache2/sites-available/graphite.conf
+#  /opt/graphite/conf/dashboard.conf
+
+  include graphite::install
 
   include apache::mod::wsgi
   include apache::mod::python
   include django
-  package { "python-cairo": ensure => installed; }
+
+  package { "python-cairo":    ensure => installed; }
   package { "python-memcache": ensure => installed; }
-  package { "python-sqlite": ensure => installed; }
-  package { "memcached": ensure => installed; }
+  package { "python-sqlite":   ensure => installed; }
+  package { "memcached":       ensure => installed; }
 
-  # gather project
-  package { "libxml-simple-perl": ensure => installed; }
-
-  apache::vhost {"$fqdn":
+  apache::vhost {"$site_alias":
     port     => '80',
     docroot  => '/var/www',
     ssl      => false,
@@ -25,71 +26,16 @@ class graphite {
     template => 'graphite/apache.conf.erb',
   }
 
-  #
-  # Graphite Install
-
-  Exec["pull graphite"] -> Exec["extract graphite"]
-  exec { "pull graphite":
-    command => '/usr/bin/wget "http://launchpad.net/graphite/1.0/0.9.8/+download/graphite-web-0.9.8.tar.gz"',
-    cwd     => "/usr/local/src",
-    creates => "/usr/local/src/graphite-web-0.9.8.tar.gz";
+  file { "/opt/graphite/conf/graphite.wsgi":
+    source    => "/opt/graphite/conf/graphite.wsgi.example",
+    subscribe => Exec["install graphite"],
   }
 
-  exec { "extract graphite":
-    command => '/bin/tar -xzf graphite-web-0.9.8.tar.gz',
-    cwd     => '/usr/local/src',
-    creates => '/usr/local/src/graphite-web-0.9.8',
-  }
-
-  exec { "install graphite":
-    command => '/usr/bin/python setup.py install',
-    cwd     => '/usr/local/src/graphite-web-0.9.8',
-    creates => '/opt/graphite/webapp',
-  }
-
-  #
-  # Carbon Install
-
-  Exec["pull carbon"] -> Exec["extract carbon"]
-  exec { "pull carbon":
-    command => '/usr/bin/wget "http://launchpad.net/graphite/1.0/0.9.8/+download/carbon-0.9.8.tar.gz"',
-    cwd     => "/usr/local/src",
-    creates => "/usr/local/src/carbon-0.9.8.tar.gz";
-  }
-
-  exec { "extract carbon":
-    command => '/bin/tar -xzf carbon-0.9.8.tar.gz',
-    cwd     => '/usr/local/src',
-    creates => '/usr/local/src/carbon-0.9.8',
-  }
-
-  exec { "install carbon":
-    command => '/usr/bin/python setup.py install',
-    cwd     => '/usr/local/src/carbon-0.9.8',
-    creates => '/opt/graphite/lib/carbon',
-  }
-
-  #
-  # Whisper install
-
-  Exec["pull whisper"] -> Exec["extract whisper"] -> Exec["install whisper"]
-  exec { "pull whisper":
-    command => '/usr/bin/wget "http://launchpad.net/graphite/1.0/0.9.8/+download/whisper-0.9.8.tar.gz"',
-    cwd     => "/usr/local/src",
-    creates => "/usr/local/src/whisper-0.9.8.tar.gz";
-  }
-
-  exec { "extract whisper":
-    command => '/bin/tar -xzf whisper-0.9.8.tar.gz',
-    cwd     => '/usr/local/src',
-    creates => '/usr/local/src/whisper-0.9.8',
-  }
-
-  exec { "install whisper":
-    command => '/usr/bin/python setup.py install',
-    cwd     => '/usr/local/src/whisper-0.9.8',
-    creates => '/usr/local/bin/whisper-info.py',
-  }
-
+#  file { "/opt/graphite/storage":
+#    owner     => $graphite::params::web_user,
+#    subscribe => Exec["install-webapp"],
+#    recurse   => inf
+#  }
 
 }
+

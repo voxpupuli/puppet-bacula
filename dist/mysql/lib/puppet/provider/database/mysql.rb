8cbdefc34193a38c59a3dd0a1d33461521f06436
James Turnbull <james@puppetlabs.com>
2011-05-14 20:28:51 -0700

    Test updates to mysql database provider

--- dist/mysql/lib/puppet/provider/database/mysql.rb
+++ dist/mysql/lib/puppet/provider/database/mysql.rb
@@ -9,7 +9,7 @@ Puppet::Type.type(:database).provide(:mysql) do
   commands :mysqlshow  => 'mysqlshow'
 	
   def create
-    mysqladmin('--default-character-set=#{resource[:charset]}', 'create', @resource[:name])
+    mysqladmin("'--default-character-set=#{resource[:charset]}'", 'create', @resource[:name])
   end
 
   def destroy
@@ -18,7 +18,7 @@ Puppet::Type.type(:database).provide(:mysql) do
 
   def exists?
     begin
-      mysql('-NBe', 'show databases').match(/^#{@resource[:name]}$/)
+      mysql('-NBe', "'show databases'").match(/^#{@resource[:name]}$/)
     rescue => e
       debug(e.message)
       return nil
@@ -26,7 +26,7 @@ Puppet::Type.type(:database).provide(:mysql) do
   end
  
   def charset
-    mysql('-NBe', 'show create database #{resource[:name]}').match(/.*?(\S+)\s\*\//)[1]
+    mysql('-NBe', "'show create database #{resource[:name]}'").match(/.*?(\S+)\s\*\//)[1]
   end
 
   def charset=(value)

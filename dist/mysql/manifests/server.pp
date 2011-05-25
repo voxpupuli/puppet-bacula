3cb9cb18752ba9f461c4ce6cd00308dd51c51820
Zach Leslie <zach@puppetlabs.com>
2011-05-25 11:37:46 -0700

    align hashrockets add debian support use params class to set the service names in the service resources

--- dist/mysql/manifests/server.pp
+++ dist/mysql/manifests/server.pp
@@ -17,51 +17,62 @@ class mysql::server {
   if(! $mysql_root_pw) {
     fail('$mysql_root_pw must be set for class mysql::server')
   }
-  package{'mysql-server':
-    name   => 'mysql-server',
-    ensure => installed,
-    notify => Service['mysqld'],
+
+  include mysql::params
+
+  $mysql_service_name = $mysql::params::mysql_service_name
+
+  package{
+    'mysql-server':
+      name   => 'mysql-server',
+      ensure => installed,
+      notify => Service['mysqld'],
   }
-  service { 'mysqld':
-    name => $operatingsystem?{
-      ubuntu  => 'mysql',
-      default => 'mysqld',
-    },
-    ensure => running,
-    enable => true,
+  service { 
+    'mysqld':
+      name   => $mysql_service_name,
+      ensure => running,
+      enable => true,
   }
+
   # this kind of sucks, that I have to specify a difference resource for restart.
   # the reason is that I need the service to be started before mods to the config
   # file which can cause a refresh
-  service{'mysqld-restart':
-    restart => '/usr/sbin/service mysqld restart'
+  service{
+    'mysqld-restart':
+      restart => "/usr/sbin/service ${mysql_service_name} restart"
   }
-  File{
+
+  File {
     owner   => 'mysql',
     group   => 'mysql',
     require => Package['mysql-server'],
   }
+
   # use the previous password for the case where its not configured in /root/.my.cnf
   case $mysql_old_pw {
     '': {$old_pw=''}
-    default: {$old_pw="-p${mysql_old_pw}"}  
+    default: {$old_pw="-p${mysql_old_pw}"}
   }
+
   exec{ 'set_mysql_rootpw':
     command   => "mysqladmin -u root ${old_pw} password ${mysql_root_pw}",
     #logoutput => on_failure,
     logoutput => true,
-    unless   => "mysqladmin -u root -p${mysql_root_pw} status > /dev/null",
+    unless    => "mysqladmin -u root -p${mysql_root_pw} status > /dev/null",
     path      => '/usr/local/sbin:/usr/bin',
     require   => [Package['mysql-server'], Service['mysqld']],
     before    => File['/root/.my.cnf'],
     notify    => Service['mysqld-restart'],
-  } 
+  }
 
   file{['/root/.my.cnf', '/etc/mycnf']:
-    owner => 'root',
-    group => 'root',
-    mode  => '0400',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
     content => template('mysql/my.cnf.erb'),
-    notify => Service['mysqld-restart'],
+    notify  => Service['mysqld-restart'],
   }
+
 }
+

3f9be1b446808b90f9967a6d8bffa941aa86a130
Zach Leslie <zach@puppetlabs.com>
2011-05-31 14:50:30 -0700

    add commenting

--- dist/bacula/manifests/init.pp
+++ dist/bacula/manifests/init.pp
@@ -23,12 +23,9 @@ class bacula (
 
   include bacula::params
   include bacula::nagios
-<<<<<<< HEAD
 
   $bacula_director = $director
   $bacula_password = $password
-=======
->>>>>>> 5538870ed354157917572471197099d7bb151ddb
 
   package { 'bacula-common':
     ensure => present,
@@ -60,14 +57,13 @@ class bacula (
     require => Package[$bacula::params::bacula_client_packages],
   }
 
-<<<<<<< HEAD
   @@concat::fragment {
     "bacula-client-$hostname":
       target  => '/etc/bacula/bacula-dir.conf',
       content => template("bacula/bacula-dir-client.erb")
   }
-=======
+
+  # realize the firewall rules exported from the director
   if defined (Class["firewall"]) { Firewall <<| dport == "9102" |>> }
->>>>>>> 5538870ed354157917572471197099d7bb151ddb
 
 }

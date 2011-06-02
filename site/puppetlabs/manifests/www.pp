4bfc6ce81e24257e915bfe8995e675ebcf30ac2d
Zach Leslie <zach@puppetlabs.com>
2011-06-02 14:31:21 -0700

    make * vhost default instead of ipaddress switching

--- site/puppetlabs/manifests/www.pp
+++ site/puppetlabs/manifests/www.pp
@@ -41,16 +41,13 @@ class puppetlabs::www {
 
   apache::vhost::redirect {
     'blog.puppetlabs.com':
-      vhost_name => $ipaddress,
       port       => '80',
       dest       => 'http://www.puppetlabs.com/blog';
     'puppetcamp.org':
-      vhost_name    => $ipaddress,
       serveraliases => "www.puppetcamp.org",
       port          => '80',
       dest          => 'http://www.puppetlabs.com/community/puppet-camp';
     'reductivelabs.com':
-      vhost_name    => $ipaddress,
       serveraliases => "www.reductivelabs.com",
       port          => '80',
       dest          => 'http://www.puppetlabs.com';

50b654a9dd7200246d269d9626ef507bf93ddcad
Zach Leslie <zach@puppetlabs.com>
2011-06-06 09:52:36 -0700

    add mysql server configuration on bacula01 node

--- site/puppetlabs/manifests/bacula01.pp
+++ site/puppetlabs/manifests/bacula01.pp
@@ -1,5 +1,8 @@
 class puppetlabs::bacula01 {
 
+  $mysql_root_pw = 'y0PM46FbrF72'
+  include mysql::server
+
   class { "bacula::director":
     db_user => 'bacula',
     db_pw   => 'ijdhx8jsd2KJshd',

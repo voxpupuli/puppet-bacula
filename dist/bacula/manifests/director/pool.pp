b890fad61aa573dd027e2d7a43bc7ddb89394d2a
Zach Leslie <zach@puppetlabs.com>
2011-06-06 11:44:55 -0700

    add pool directive for individual configuration file for pools

---
+++ dist/bacula/manifests/director/pool.pp
@@ -0,0 +1,20 @@
+define bacula::director::pool (
+    $type        = "Backup',
+    $recycle     = "Yes"
+    $autoprune   = "Yes",
+    $volret      = "4 months" # Volume Retention
+    $maxvoljobs  = '1',
+    $maxvolbytes = '1000000000',
+    $maxvols     = '30'
+    $label       = ''
+
+  ) {
+
+  concat::fragment {
+    "bacula-director-pool-$name":
+      target => '/etc/bacula/conf.d/pools.conf'
+      concat => tempalte("bacula/pool.conf.erb");
+  }
+
+}
+

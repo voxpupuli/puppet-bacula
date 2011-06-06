121289e0af633daf385276836b241cd25ab67b30
Zach Leslie <zach@puppetlabs.com>
2011-06-06 15:38:23 -0700

    tidy up since I am here

--- dist/nagios/manifests/bacula.pp
+++ dist/nagios/manifests/bacula.pp
@@ -13,39 +13,39 @@
 class nagios::bacula {
 
   @@nagios_service { "check_baculadir_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe!check_proc!1:1 bacula-dir',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe!check_proc!1:1 bacula-dir',
     service_description => "check_baculadir_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_baculasd_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe!check_proc!1:1 bacula-sd',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe!check_proc!1:1 bacula-sd',
     service_description => "check_baculasd_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_baculafd_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe!check_proc!1:1 bacula-fd',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe!check_proc!1:1 bacula-fd',
     service_description => "check_baculafd_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_baculadisk_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe_1arg!check_xvdc',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe_1arg!check_xvdc',
     service_description => "check_bacula_disk_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
 

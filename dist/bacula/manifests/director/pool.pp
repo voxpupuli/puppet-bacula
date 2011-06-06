7d2eee307e329a4df74ad762aa29512c988b54e2
Zach Leslie <zach@puppetlabs.com>
2011-06-06 12:06:55 -0700

    and again, this time with conviction

--- dist/bacula/manifests/director/pool.pp
+++ dist/bacula/manifests/director/pool.pp
@@ -2,17 +2,17 @@ define bacula::director::pool (
     $type        = "Backup',
     $recycle     = "Yes",
     $autoprune   = "Yes",
-    $volret      = "4 months" # Volume Retention
+    $volret      = "4 months", # Volume Retention
     $maxvoljobs  = '1',
     $maxvolbytes = '1000000000',
-    $maxvols     = '30'
+    $maxvols     = '30',
     $label       = ''
 
   ) {
 
   concat::fragment {
     "bacula-director-pool-$name":
-      target => '/etc/bacula/conf.d/pools.conf'
+      target => '/etc/bacula/conf.d/pools.conf',
       concat => tempalte("bacula/pool.conf.erb");
   }
 

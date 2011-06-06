26c150ad0ecc773f456384ccb35bd38e00dfb302
Zach Leslie <zach@puppetlabs.com>
2011-06-06 15:01:09 -0700

    fix bacula director name on deb/ubuntu systems

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,11 +11,11 @@
 # Sample Usage:
 #
 class bacula::params {
-  
+
   case $operatingsystem {
      "ubuntu","debian": {
         $bacula_director_packages = [ "bacula-director-common", "bacula-director-mysql", "bacula-sd-mysql", "bacula-console" ]
-        $bacula_director_services = [ "bacula-dir", "bacula-sd" ]
+        $bacula_director_services = [ "bacula-director", "bacula-sd" ]
         $bacula_client_packages = "bacula-client"
         $bacula_client_services = "bacula-fd"
      }

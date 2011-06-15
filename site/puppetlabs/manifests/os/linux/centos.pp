c522dbb8495d21f1c0ef964190bd474e9e6e377e
Zach Leslie <zach@puppetlabs.com>
2011-06-15 15:00:27 -0700

    escape the $ in basearch for yum repo

--- site/puppetlabs/manifests/os/linux/centos.pp
+++ site/puppetlabs/manifests/os/linux/centos.pp
@@ -1,7 +1,7 @@
 class puppetlabs::os::linux::centos {
 
   yumrepo { 'epel':
-    descr      => "Extra Packages for Enterprise Linux ${lsbmajdistrelease} - $basearch",
+    descr      => "Extra Packages for Enterprise Linux ${lsbmajdistrelease} - \$basearch",
     mirrorlist => "mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-${lsbmajdistrelease}&arch=\$basearch",
     enabled    => 1,
     gpgcheck   => 1,

f90d3ef0371a6dd3836a74d13a10d25ae1d08a36
Zach Leslie <zach@puppetlabs.com>
2011-06-15 15:52:22 -0700

    add updates yum repo and purge all others

--- site/puppetlabs/manifests/os/linux/centos.pp
+++ site/puppetlabs/manifests/os/linux/centos.pp
@@ -1,8 +1,15 @@
 class puppetlabs::os::linux::centos {
 
+
+  resources {
+    "yumrepo":
+      purge => true;
+  }
+
+
   yumrepo { 'epel':
     descr      => "Extra Packages for Enterprise Linux ${lsbmajdistrelease} - \$basearch",
-    mirrorlist => "http://mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-${lsbmajdistrelease}&arch=\$basearch",
+    mirrorlist => "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-${lsbmajdistrelease}&arch=\$basearch",
     enabled    => 1,
     gpgcheck   => 1,
     keepalive  => 1,
@@ -18,6 +25,14 @@ class puppetlabs::os::linux::centos {
         gpgcheck   => 1,
         keepalive  => 1,
       }
+      yumrepo { 'puppetlabs_updates':
+        descr      => "Puppetlabs Internal Updates Mirror Linux ${lsbmajdistrelease} - \$basearch",
+        baseurl    => "http://yo.puppetlabs.lan/cent${lsbmajdistrelease}server-$architecture/RPMS.updates",
+        enabled    => 1,
+        gpgcheck   => 1,
+        keepalive  => 1,
+      }
+    }
     }
     default: {}
   }

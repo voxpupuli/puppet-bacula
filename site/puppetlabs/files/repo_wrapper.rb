75e58b9aa0a5c5fa1b5cbc55912db7edc3d01362
Zach Leslie <zach@puppetlabs.com>
2011-06-18 10:31:05 -0700

    rename repo_wrapper.rb update script and test on burji for signing and pulling in new pacakges

---
+++ site/puppetlabs/files/repo_wrapper.rb
@@ -0,0 +1,39 @@
+#! /usr/bin/env ruby
+
+require 'pp'
+
+apt_base = '/opt/repository/apt'
+keychain_cmd = "keychain -q --agents gpg 4BD6EC30"
+
+# Start Keychain
+puts "Starting keychain..."
+`#{keychain_cmd}`
+
+repos = [
+  '/opt/repository/apt/ubuntu',
+  '/opt/repository/apt/debian',
+  '/opt/repository/apt/ops',
+]
+
+#Dir.glob("#{apt_base}/*").each do |repo|
+repos.each do |repo|
+  puts "Working on #{repo}"
+  incoming = "#{repo}/incoming"
+  if File.directory?("#{incoming}") == true 
+    files = Dir.entries("#{incoming}") - ['.','..']
+    if files.length > 0
+      Dir.chdir(repo)
+      # Do something useful here
+      dists = File.read('conf/distributions').grep(/Codename/).each do |line|
+        dist = line.split(' ').last.chomp
+        cmdlist = []
+        cmdlist << "reprepro -b #{repo} includedeb #{dist} #{incoming}/*.deb"
+        cmdlist << "reprepro -b #{repo} include #{dist} #{incoming}/*.changes"
+        cmdlist.each do |cmd|
+          `#{cmd}`
+        end
+      end
+    end
+  end
+end
+

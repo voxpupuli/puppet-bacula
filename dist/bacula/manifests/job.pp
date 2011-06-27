cc5caa915d4f0d10d2dc00b2ca191be5918cedc2
Zach Leslie <zach@puppetlabs.com>
2011-06-27 11:50:11 -0700

    fixup naming on bacula jobs

--- dist/bacula/manifests/job.pp
+++ dist/bacula/manifests/job.pp
@@ -9,14 +9,14 @@ define bacula::job (
   $director = $bacula::bacula_director
 
   @@concat::fragment {
-    "$name":
+    "bacula-fileset-$name":
       target  => '/etc/bacula/conf.d/fileset.conf',
       content => template("bacula/fileset.conf.erb"),
       tag     => "bacula-$director";
   }
 
   @@concat::fragment {
-    "bacula-job-$hostname":
+    "bacula-job-$name":
       target  => '/etc/bacula/conf.d/job.conf',
       content => template("bacula/job.conf.erb"),
       tag     => "bacula-$director";

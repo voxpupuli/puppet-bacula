3d467ca8b8abb1ddb0f2eb46555f6264ffcc59a0
Zach Leslie <zach@puppetlabs.com>
2011-06-27 11:44:39 -0700

    test a workaround for some scoping issues

--- dist/bacula/manifests/job.pp
+++ dist/bacula/manifests/job.pp
@@ -3,6 +3,11 @@ define bacula::job (
     $excludes = ''
   ) {
 
+  if ! defined(Class["bacula"]) {
+    err("need class bacula for this to be useful")
+  }
+  $director = $bacula::bacula_director
+
   @@concat::fragment {
     "$name":
       target  => '/etc/bacula/conf.d/fileset.conf',

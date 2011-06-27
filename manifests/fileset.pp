47deb39f65957704cc8ff5e399007d62c91ce537
Zach Leslie <zach@puppetlabs.com>
2011-06-27 13:20:11 -0700

    add properly scopped director to fileset

--- manifests/fileset.pp
+++ manifests/fileset.pp
@@ -3,6 +3,11 @@ define bacula::fileset (
     $excludes = ''
   ) {
 
+  if ! defined(Class["bacula"]) {
+    err("need class bacula for this to be useful")
+  }
+  $director = $bacula::bacula_director
+
   @@concat::fragment {
     "bacula-fileset-$name":
       target  => '/etc/bacula/conf.d/fileset.conf',

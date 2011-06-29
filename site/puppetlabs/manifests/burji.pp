a592e2c935997497d3b07fe1d4a5ead31113b1f7
Zach Leslie <zach@puppetlabs.com>
2011-06-29 11:18:34 -0700

    add legba functions to burji for decom of legba

--- site/puppetlabs/manifests/burji.pp
+++ site/puppetlabs/manifests/burji.pp
@@ -1,7 +1,20 @@
 class puppetlabs::burji {
 
-  ssh::allowgroup  { "release": }
-  sudo::allowgroup { "release": }
+  include puppetlabs_ssl
+
+  # User Stuff
+  #
+  ssh::allowgroup  { "release":    }
+  ssh::allowgroup  { "prosvc":     }
+  ssh::allowgroup  { "enterprise": }
+
+  sudo::allowgroup { "release":    }
+
+  # Server stuff
+  #
+  Package <| title == 'mlocate' |>
+
+  $ssl_path = $puppetlabs_ssl::params::ssl_path
 
   include puppetlabs::service::pkgrepo
 
@@ -21,6 +34,73 @@ class puppetlabs::burji {
       options => "None",
       docroot => '/var/www'
   }
+
+  # http://ps.puppetlabs.com
+  #
+  apache::vhost::redirect {
+    'ps.puppetlabs.com':
+      port => '80',
+      dest => 'https://ps.puppetlabs.com',
+  }
+
+  apache::vhost {
+    'ps.puppetlabs.com_ssl':
+      serveraliases => "ps.puppetlabs.com",
+      port          => '443',
+      docroot       => '/opt/prosvc',
+      ssl           => true,
+      auth          => true,
+      priority      => 11,
+      template      => 'puppetlabs/legba.conf.erb';
+  }
+
+  file { "/opt/prosvc":
+    ensure   => directory,
+    owner    => root,
+    group    => prosvc,
+    mode     => 0664,
+    recurse  => true,
+    checksum => none;
+  }
+  file { "/opt/prosvc/.htaccess": ensure => absent; }
+
+  # http://pm.puppetlabs.com
+  #
+  apache::vhost {
+    'pm.puppetlabs.com':
+      serveraliases => "pm.puppetlabs.com",
+      port          => 80,
+      docroot       => '/opt/pm',
+      ssl           => false,
+      redirect_ssl  => true,
+      priority      => 15,
+      template      => 'puppetlabs/legba.conf.erb';
+    'pm.puppetlabs.com_ssl':
+      serveraliases => "pm.puppetlabs.com",
+      port          => 443,
+      docroot       => '/opt/pm',
+      ssl           => true,
+      auth          => false,
+      priority      => 16,
+      template      => 'puppetlabs/legba.conf.erb';
+  }
+
+  file { "/opt/pm":
+    ensure   => directory,
+    owner    => root,
+    group    => enterprise,
+    mode     => 664,
+    recurse  => true,
+    checksum => none;
+  }
+
+  file { "/opt/pm/.htaccess":
+    owner  => root,
+    group  => enterprise,
+    mode   => 644,
+    source => "puppet:///modules/puppetlabs/legba_htaccess";
+  }
+
 
 }
 

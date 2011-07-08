d1c6c04e942b6430555a5c071ec5746fa2f60a31
Zach Leslie <zach@puppetlabs.com>
2011-07-08 09:36:33 -0700

    export pacakges only on linux systems

--- dist/packages/manifests/init.pp
+++ dist/packages/manifests/init.pp
@@ -1,8 +1,10 @@
 # to make it easier to grab the neede package one-offs
 
 class packages {
-  include packages::vcs
-  include packages::shells
-  include packages::archive
+  if $kernel == "Linux" {
+    include packages::vcs
+    include packages::shells
+    include packages::archive
+  }
 
 }

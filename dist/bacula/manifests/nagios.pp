6263f8882b9e9c5533617b22cd11d0ac8a108565
Zach Leslie <zach@puppetlabs.com>
2011-07-11 10:46:17 -0700

    comment out bacula cause I cant figure out where the hell its being called from, grep fails me or stored configs evade me

--- dist/bacula/manifests/nagios.pp
+++ dist/bacula/manifests/nagios.pp
@@ -13,14 +13,14 @@
 class bacula::nagios {
   include bacula::params
 
-  @@nagios_service { "check_bacula_${hostname}":
-    use                 => 'generic-service',
-    host_name           => "$fqdn",
-    check_command       => 'check_bacula',
-    service_description => "check_bacula_${hostname}",
-    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify              => Service[$nagios::params::nagios_service],
-  }
+#  @@nagios_service { "check_bacula_${hostname}":
+#    use                 => 'generic-service',
+#    host_name           => "$fqdn",
+#    check_command       => 'check_bacula',
+#    service_description => "check_bacula_${hostname}",
+#    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+#    notify              => Service[$nagios::params::nagios_service],
+#  }
 
 }
 

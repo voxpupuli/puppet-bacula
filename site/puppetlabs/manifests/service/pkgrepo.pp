61a0752163573dd9a5802661a1fb8c5d44d2592a
Zach Leslie <zach@puppetlabs.com>
2011-07-12 12:19:23 -0700

    add ensure param to pkgrepo class for easy removal

--- site/puppetlabs/manifests/service/pkgrepo.pp
+++ site/puppetlabs/manifests/service/pkgrepo.pp
@@ -1,7 +1,13 @@
-class puppetlabs::service::pkgrepo {
+class puppetlabs::service::pkgrepo (
+  $ensure => present
+) {
+  ###
+  # Package repositories
+  #
 
   file {
     "/usr/local/bin/repo_perms.sh":
+      ensure => $ensure,
       owner  => root,
       group  => root,
       mode   => 0755,
@@ -10,6 +16,7 @@ class puppetlabs::service::pkgrepo {
 
   file {
     "/usr/local/bin/repo_wrapper.rb":
+      ensure => $ensure,
       owner  => root,
       group  => root,
       mode   => 0755,
@@ -18,20 +25,19 @@ class puppetlabs::service::pkgrepo {
 
   cron {
     "/usr/local/bin/repo_perms.sh":
+      ensure  => $ensure,
       user    => root,
       command => "/usr/local/bin/repo_perms.sh",
       minute  => "*/10";
   }
+  if $ensure == present {
+    nagios::website { 'apt.puppetlabs.com': }
+    nagios::website { 'yum.puppetlabs.com': }
+    class { "apt::server::repo": site_name => "apt.puppetlabs.com"; }
+    include yumrepo
 
-  ###
-  # Package repositories
-  #
-  nagios::website { 'apt.puppetlabs.com': }
-  nagios::website { 'yum.puppetlabs.com': }
-  class { "apt::server::repo": site_name => "apt.puppetlabs.com"; }
-  include yumrepo
-
-  Package <| title == "gnupg-agent" |>
+    Package <| title == "gnupg-agent" |>
+  }
 
 }
 

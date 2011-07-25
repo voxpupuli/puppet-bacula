295767f67c3f05b479aab543334f9e24c5408a84
Zach Leslie <zach@puppetlabs.com>
2011-07-25 10:28:24 -0700

    quiet up the notify daemon

--- site/puppetlabs/files/_opt_enterprise_sync.sh
+++ site/puppetlabs/files/_opt_enterprise_sync.sh
@@ -4,7 +4,7 @@ DIR="/opt/enterprise"
 
 [[ -d $DIR ]] || (echo 'FUCK!'; exit 1)
 
-inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
+inotifywait -qq -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' \
 -e close_write -e delete $DIR | while read date time dir file; do
   FILECHANGE=${dir}${file}
   rsync -a  /usr/bin/rsync -a /opt/enterprise/ tb-driver.puppetlabs.lan:/opt/enterprise/ && \

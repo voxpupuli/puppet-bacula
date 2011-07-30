0794eb5f34cd9534048021ee16f53ee8338bb5f4
Adrien Thebo <adrien@puppetlabs.com>
2011-07-29 17:19:35 -0700

    Added base dependencies for gitolite to clippy.

--- site/puppetlabs/manifests/clippy.pp
+++ site/puppetlabs/manifests/clippy.pp
@@ -3,5 +3,13 @@ class puppetlabs::clippy {
   sudo::allowgroup  { "interns": }
   ssh::allowgroup   { "interns": }
 
+  # Resources for gitolite
+
+  Account::User <| title == 'git' |>
+  Group <| title == 'git' |>
+
+  package { "gitolite":
+    ensure => present,
+  }
 }
 

b4724d569b8e8bd3ff3c2262c7b96ee82347e90d
Adrien Thebo <adrien@puppetlabs.com>
2011-08-01 09:31:28 -0700

    Added git user to ssh allowed groups

--- site/puppetlabs/manifests/clippy.pp
+++ site/puppetlabs/manifests/clippy.pp
@@ -2,11 +2,11 @@ class puppetlabs::clippy {
 
   sudo::allowgroup  { "interns": }
   ssh::allowgroup   { "interns": }
+  ssh::allowgroup   { "git": }
 
   # Resources for gitolite
-
   Account::User <| title == 'git' |>
-  Group <| title == 'git' |>
+  Group         <| title == 'git' |>
 
   package { "gitolite":
     ensure => present,

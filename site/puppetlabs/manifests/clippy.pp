512cca1506da073fb4e9fa5f64162dc178653ccc
Adrien Thebo <adrien@puppetlabs.com>
2011-08-01 12:21:00 -0700

    Changed wwwroot for github mirror listener.

--- site/puppetlabs/manifests/clippy.pp
+++ site/puppetlabs/manifests/clippy.pp
@@ -5,9 +5,18 @@ class puppetlabs::clippy {
 
   include git::gitolite
 
+  file { "/var/www/git":
+    ensure => directory,
+    owner  => "root",
+    group  => "root",
+    mode   => "0755",
+  }
+
   class { 'github::params':
+    wwwroot    => "/var/www/git",
     basedir    => "/home/git/repositories",
     vhost_name => "git.puppetlabs.lan",
+    require    => File["/var/www/git"],
   }
 
   github::mirror {

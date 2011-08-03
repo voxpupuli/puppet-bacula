0049b7eedb567786fc5deedec6fd57819dbc947c
Zach Leslie <zach@puppetlabs.com>
2011-08-03 09:17:13 -0700

    add builders to pluto and remove interns

--- site/puppetlabs/manifests/pluto.pp
+++ site/puppetlabs/manifests/pluto.pp
@@ -2,7 +2,7 @@ class puppetlabs::pluto {
 
   ssh::allowgroup { "developers": }
   ssh::allowgroup { "prosvc": }
-  ssh::allowgroup { "interns": }
+  ssh::allowgroup { "builders": }
 
   # Customer Groups
   Account::User <| group == vmware |>

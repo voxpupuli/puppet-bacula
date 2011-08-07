b90f8bd0f0bf212fb3abb0e8e15926a371b59b71
Zach Leslie <zach@puppetlabs.com>
2011-08-07 12:06:09 -0700

    add redirect for docs.mirror0.puppetlabs.com

--- site/puppetlabs/manifests/web01.pp
+++ site/puppetlabs/manifests/web01.pp
@@ -4,6 +4,12 @@ class puppetlabs::web01 {
   include puppetlabs::service::www
   class { "puppetlabs::docs": port => 82; }
 
+  apache::vhost::redirect {
+    "docs.mirror0.puppetlabs.com":
+      port => '80',
+      dest => 'http://docs.puppetlabs.com';
+  }
+
   # Bacula
   $bacula_director = 'baal.puppetlabs.com'
   $bacula_password = '4tc39KValGRv4xqhXhn5X4MsrHB5pQZbMfnzDt'

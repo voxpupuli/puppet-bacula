cb6175323797fbd310e787a18459646ab22b84c8
Zach Leslie <zach@puppetlabs.com>
2011-08-08 10:34:23 -0700

    clean out all old wyrd stuff for reprovisioning

--- site/puppetlabs/manifests/wyrd.pp
+++ site/puppetlabs/manifests/wyrd.pp
@@ -1,19 +1,5 @@
 class puppetlabs::wyrd {
 
-#  class { "apt-cacher": service => false; }
-
-  # keeps http://wyrd.puppetlabs.lan/puppetdoc/ up to date
-  cron {
-    "pull ~zach/src/puppetlabs-modules":
-      command => "source ~/.keychain/wyrd.sh && (cd ~zach/src/puppetlabs-modules/; git pull)",
-      user => zach,  minute => '*/10';
-  }
-
-  apache::vhost {
-    "$fqdn":
-      port    => 80,
-      docroot => '/opt/www'
-  }
 
 }
 

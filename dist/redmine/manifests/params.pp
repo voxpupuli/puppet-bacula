3418cd1496ef30d2a8540d19fffb76dbaf2f8011
Ben Hughes <git@mumble.org.uk>
2011-08-12 15:35:56 -0700

    Make redmine come from our repo, rather than upstream, so we can control changes more if we need to.

--- dist/redmine/manifests/params.pp
+++ dist/redmine/manifests/params.pp
@@ -1,5 +1,5 @@
 class redmine::params {
   $version = '1faf02c9f5ce22ce676e7a3ae710dd0c52a21d45'
-  $source  = 'git://github.com/edavis10/redmine'
-  $rails_version = '2.3.11'
+  $source  = 'git://github.com/puppetlabs/redmine'
+  $rails_version = '2.3.12'
 }

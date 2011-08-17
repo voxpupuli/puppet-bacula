3d6c2e74755b0d6ef760d846f0330c312d500f9e
Zach Leslie <zach@puppetlabs.com>
2011-08-17 15:47:21 -0700

    make bacula::job error conditional useful

--- dist/bacula/manifests/job.pp
+++ dist/bacula/manifests/job.pp
@@ -33,39 +33,39 @@ define bacula::job (
 
   if ! defined(Class["bacula"]) {
     err("need class bacula for this to be useful")
-  }
-  $director = $bacula::bacula_director
-
-  # so if the fileset is not defined, we fall back to one called Common
-  if $fileset == true {
-    if $files == '' { err("you tell me to create a fileset, but not files given") }
-    $fileset_real = "$name"
-    bacula::fileset {
-      "$name":
-        files    => $files,
-        excludes => $excludes
-      }
   } else {
-    $fileset_real = "Common"
-  }
+    $director = $bacula::bacula_director
 
-  @@concat::fragment {
-    "bacula-job-$name":
-      target  => '/etc/bacula/conf.d/job.conf',
-      content => template("bacula/job.conf.erb"),
-      tag     => "bacula-$director";
-  }
+    # so if the fileset is not defined, we fall back to one called Common
+    if $fileset == true {
+      if $files == '' { err("you tell me to create a fileset, but not files given") }
+      $fileset_real = "$name"
+      bacula::fileset {
+        "$name":
+          files    => $files,
+          excludes => $excludes
+        }
+    } else {
+      $fileset_real = "Common"
+    }
 
-  if $bacula::monitor == true {
-    @@nagios_service { "check_bacula_${name}":
-      use                 => 'generic-service',
-      host_name           => "$fqdn",
-      check_command       => "check_bacula!26!${name}",
-      service_description => "check_bacula_${name}",
-      target              => '/etc/nagios3/conf.d/nagios_service.cfg',
-      notify              => Service[$nagios::params::nagios_service],
+    @@concat::fragment {
+      "bacula-job-$name":
+        target  => '/etc/bacula/conf.d/job.conf',
+        content => template("bacula/job.conf.erb"),
+        tag     => "bacula-$director";
+    }
+
+    if $bacula::monitor == true {
+      @@nagios_service { "check_bacula_${name}":
+        use                 => 'generic-service',
+        host_name           => "$fqdn",
+        check_command       => "check_bacula!26!${name}",
+        service_description => "check_bacula_${name}",
+        target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+        notify              => Service[$nagios::params::nagios_service],
+      }
     }
   }
-
 }
 

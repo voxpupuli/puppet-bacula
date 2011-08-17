463f568cccb27393950b036aab0afe81e4edbbab
Ben Hughes <ben@puppetlabs.com>
2011-08-16 18:01:11 -0700

    Add emergency migration Vhosts to web01 while we move dxul in a panic

--- site/puppetlabs/manifests/web01.pp
+++ site/puppetlabs/manifests/web01.pp
@@ -1,6 +1,7 @@
 class puppetlabs::web01 {
 
   include postfix
+  include puppetlabs_ssl
   include puppetlabs::service::www
   class { "puppetlabs::docs": port => 82; }
 
@@ -16,6 +17,34 @@ class puppetlabs::web01 {
   class { "bacula":
     director => $bacula_director,
     password => $bacula_password,
+  }
+
+
+  file{ '/tmp/maint/':
+    ensure => directory,
+    mode   => '0755',
+  }
+  file{ '/tmp/maint/index.html':
+    content => "<html><head><title>PuppetLabs</title></head><body>We're sorry, but we're currently migrating this machine to another host to make it better. Yeah!</body></html>",
+    owner   => 'www-data',
+    mode    => '0644',
+    require => File['/tmp/maint/'],
+  }
+
+  apache::vhost {
+    'maint.puppetlabs.com_ssl':
+      serveraliases => [ 'forge.puppetlabs.com', 'projects.puppetlabs.com' ],
+      port          => 443,
+      priority      => 10,
+      docroot       => '/tmp/maint/',
+      ssl           => true,
+      require       => File['/tmp/maint/index.html'],
+  }
+
+  apache::vhost::redirect {
+    'maint.puppetlabs.com':
+      port => 82,
+      dest => 'https://projects.puppetlabs.com',
   }
 
 }

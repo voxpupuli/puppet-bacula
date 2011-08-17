8bc8afd8df4cd0254921dfbdda53ed7cae96b7e5
Ben Hughes <ben@puppetlabs.com>
2011-08-17 13:55:26 -0700

    remove the maint vhosts from web01 after last night's migration.

--- site/puppetlabs/manifests/web01.pp
+++ site/puppetlabs/manifests/web01.pp
@@ -24,35 +24,5 @@ class puppetlabs::web01 {
     password => $bacula_password,
   }
 
-
-  file{ '/tmp/maint/':
-    ensure => directory,
-    mode   => '0755',
-  }
-  file{ '/tmp/maint/index.html':
-    content => "<html><head><title>PuppetLabs</title></head><body>We're sorry, but we're currently migrating this machine to another host to make it better. Yeah!<br><center><img src='http://anti-goth.com/Bunsen-Beaker-web.jpg'></center></body></html>",
-    owner   => 'www-data',
-    mode    => '0644',
-    require => File['/tmp/maint/'],
-  }
-
-  apache::vhost {
-    'maint.puppetlabs.com_ssl':
-      serveraliases => [ 'forge.puppetlabs.com', 'projects.puppetlabs.com' ],
-      port          => 443,
-      priority      => 10,
-      docroot       => '/tmp/maint/',
-      ssl           => true,
-      template      => 'puppetlabs/ssl_vhost.conf.erb',
-      require       => File['/tmp/maint/index.html'],
-  }
-
-  apache::vhost::redirect {
-    'maint.puppetlabs.com':
-      port => 82,
-      serveraliases => [ 'forge.puppetlabs.com', 'projects.puppetlabs.com' ],
-      dest => 'https://projects.puppetlabs.com',
-  }
-
 }
 

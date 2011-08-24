d282191574c6534b224f1e498ddbe0f456f477c1
Zach Leslie <zach@puppetlabs.com>
2011-08-24 12:18:19 -0700

    add bootserver class and include it on vanir

--- site/puppetlabs/manifests/vanir.pp
+++ site/puppetlabs/manifests/vanir.pp
@@ -1,18 +1,7 @@
 class puppetlabs::vanir {
 
   class { "apt-cacher": }
-
-#  cron {
-#    "pull ~zach/src/puppetlabs-modules":
-#      command => "source ~/.keychain/wyrd.sh && (cd ~zach/src/puppetlabs-modules/; git pull)",
-#      user    => zach,  minute => '*/10';
-#  }
-
-#  apache::vhost {
-#    "$fqdn":
-#      port    => 80,
-#      docroot => '/opt/www'
-#  }
+  include puppetlabs::service::bootserver
 
 }
 

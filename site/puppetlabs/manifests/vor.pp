eb1527a6efede9d9d3c9c7aec1d3137eaff66761
Ben Hughes <ben@puppetlabs.com>
2011-08-26 12:52:40 -0700

    Stab at making vor install postgres from backports.

--- site/puppetlabs/manifests/vor.pp
+++ site/puppetlabs/manifests/vor.pp
@@ -1,5 +1,25 @@
 class puppetlabs::vor {
 
-  
+  File{ owner => 'root', group => 'root', mode => '0644' }
+
+  # Install postgres from backports
+  file{
+    '/etc/preferences.d/postgresql9':
+      ensure   => file,
+      contents => "Package: postgresql\nPin: release a=squeeze-backports\nPin-Priority: 200\n";
+    '/etc/apt/sources.list.d/backports.list':
+      ensure   => file,
+      contents => 'deb http://backports.debian.org/debian-backports squeeze-backports main',
+      requires => File['/etc/preferences.d/postgresql9'];
+  }
+
+  exec{
+    'refresh_apts':
+      command     => '/usr/bin/aptitude --quiet update',
+      refreshonly => 'true',
+      requires    => File['/etc/apt/sources.list.d/backports.list'],
+  }
+
+  package{ 'postgresql-9.0': ensure => present, requires => Exec['refresh_apts'] }
 
 }

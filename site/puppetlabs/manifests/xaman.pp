ae0b6b89abbaf7d98389ad5816eaa04c7d357d0b
Adrien Thebo <adrien@puppetlabs.com>
2011-08-29 14:51:53 -0700

    Add access and sudo groups on xaman

--- site/puppetlabs/manifests/xaman.pp
+++ site/puppetlabs/manifests/xaman.pp
@@ -1,8 +1,17 @@
 class puppetlabs::xaman {
 
   sudo::allowgroup  { "interns": }
+  sudo::allowgroup  { "developers": }
   ssh::allowgroup   { "interns": }
 
-  include puppetlabs_ssl
+  sudo::entry {
+    "randall-okra":
+      entry => "randall ALL = (okra) NOPASSWD: ALL";
+    "pvande-okra":
+      entry => "pvande  ALL = (okra) NOPASSWD: ALL";
+  }
+
+  # yank cert installation while this is in a branch
+  #include puppetlabs_ssl
   include okra::passenger
 }

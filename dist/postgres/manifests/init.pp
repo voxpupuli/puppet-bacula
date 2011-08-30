d7943d41c25c981d84391efbc06e630a89c84118
Ben Hughes <ben@puppetlabs.com>
2011-08-30 13:37:39 -0700

    Update postgres class and move all the debian installation gumph to there!
    Man this is ugly.

--- dist/postgres/manifests/init.pp
+++ dist/postgres/manifests/init.pp
@@ -2,19 +2,49 @@
 # move this around, make it check for debian, as that's all it will
 # work on. Probably map some things.
 
-class postgres {
+class postgres::install::debian {
 
-  postgres::user{ 'superuser':
-    createdb  => true,
-    superuser => true,
+  $pgversion = '9.0' # if you wish, this could move to it's own params
+                     # class.
+
+  case $operatingsystem {
+    default:  { fail "$module_name is Debian only."}
+    debian,ubuntu:  {}
   }
 
-  postgres::hba{ 'superuser':
-    type     => 'user',
-    database => 'ALL',
-    cidr     => '192.168.100.0/24',
-    method   => 'MD5',
+  # Defaults for this class, because I'm lazy.
+  File{ owner => 'root', group => 'root', mode => '0644' }
+  Apt::Pin{ release  => 'squeeze-backports', priority => '1001' }
+
+  # Install postgres from backports
+  apt::pin{
+    [ "postgresql-${pgversion}", "postgresql-client-${pgversion}", 'postgresql-common' ,
+      'postgresql-client-common' , 'libpq5']:
+      before => File['/etc/apt/sources.list.d/backports.list'],
   }
+
+  # Do this, as per http://backports-master.debian.org/Instructions/
+  # so we get backports updates.
+  apt::pin{ '*':
+    release  => 'lenny-backports',
+    priority => '200',
+    filename => 'star'
+  }
+
+  file{
+    '/etc/apt/sources.list.d/backports.list':
+      ensure   => file,
+      content  => 'deb http://backports.debian.org/debian-backports squeeze-backports main',
+  }
+
+  exec{
+    'refresh_apts':
+      command     => '/usr/bin/aptitude --quiet update',
+      refreshonly => 'true',
+      subscribe   => File['/etc/apt/sources.list.d/backports.list'],
+  }
+
+  package{ "postgresql-${pgversion}": ensure => present, require => Exec['refresh_apts'] }
 
 
 }
--- site/puppetlabs/manifests/vor.pp
+++ site/puppetlabs/manifests/vor.pp
@@ -1,43 +1,23 @@
 class puppetlabs::vor {
 
-  # Defaults for this class, because I'm lazy.
-  File{ owner => 'root', group => 'root', mode => '0644' }
-  Apt::Pin{ release  => 'squeeze-backports', priority => '1001' }
-
-  # Install postgres from backports
-  apt::pin{
-    [ 'postgresql-9.0', 'postgresql-client-9.0', 'postgresql-common' ,
-      'postgresql-client-common' , 'libpq5']:
-      before => File['/etc/apt/sources.list.d/backports.list'],
-  }
-
-  # Do this, as per http://backports-master.debian.org/Instructions/
-  # so we get backports updates.
-  apt::pin{ '*':
-    release  => 'lenny-backports',
-    priority => '200',
-    filename => 'star'
-  }
-
-  file{
-    '/etc/apt/sources.list.d/backports.list':
-      ensure   => file,
-      content  => 'deb http://backports.debian.org/debian-backports squeeze-backports main',
-  }
-
-  exec{
-    'refresh_apts':
-      command     => '/usr/bin/aptitude --quiet update',
-      refreshonly => 'true',
-      subscribe   => File['/etc/apt/sources.list.d/backports.list'],
-  }
-
-  package{ 'postgresql-9.0': ensure => present, require => Exec['refresh_apts'] }
-
   # This is dirty and a lot of the above should be moved to this
   # class.
-  class{ 'postgres':
-    require => Package['postgresql-9.0']
+  include postgres::install::debian
+
+  # class{ 'postgres':
+  #   require => Package['postgresql-9.0']
+  # }
+
+  postgres::user{ 'superduperuser':
+    createdb  => true,
+    superuser => true,
+  }
+
+  postgres::hba{ 'superduperuser':
+    type     => 'user',
+    database => 'ALL',
+    cidr     => '192.168.100.0/24',
+    method   => 'MD5',
   }
 
 }

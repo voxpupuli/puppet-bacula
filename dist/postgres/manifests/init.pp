fb9012820c7cc0ff1bc8253c7dbf5e9f2147fd7b
Ben Hughes <ben@puppetlabs.com>
2011-08-30 15:54:53 -0700

    Merge in (not submoduling as I'm changing too much of it) Kris Buytaert's postgres module as it's better than mine. https://github.com/KrisBuytaert/puppet-postgres
    Change vor to try and use that, see what happens.

--- dist/postgres/manifests/init.pp
+++ dist/postgres/manifests/init.pp
@@ -1,104 +1,139 @@
-# This is hack, and just to get something off the ground. I need to
-# move this around, make it check for debian, as that's all it will
-# work on. Probably map some things.
+# Class: postgres
+#
+# This module manages postgres
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage: see postgres/README.markdown
+#
+# [Remember: No empty lines between comments and class definition]
+class postgres {
+  # Common stuff, like ensuring postgres_password defined in site.pp
+  include postgres::common
 
-class postgres::install::debian {
+  # Handle version specified in site.pp (or default to postgresql) 
+  $postgres_client = "postgresql${postgres_version}"
+  $postgres_server = "postgresql${postgres_version}-server"
 
-  $pgversion = '9.0' # if you wish, this could move to it's own params
-                     # class.
-
-  case $operatingsystem {
-    default:  { fail "$module_name is Debian only."}
-    debian,ubuntu:  {}
+  package { [$postgres_client, $postgres_server]: 
+    ensure => installed,
   }
 
-  # Defaults for this class, because I'm lazy.
-  File{ owner => 'root', group => 'root', mode => '0644' }
-  Apt::Pin{ release  => 'squeeze-backports', priority => '1001' }
-
-  # Install postgres from backports
-  apt::pin{
-    [ "postgresql-${pgversion}", "postgresql-client-${pgversion}", 'postgresql-common' ,
-      'postgresql-client-common' , 'libpq5']:
-      before => File['/etc/apt/sources.list.d/backports.list'],
+  user { 'postgres':
+    shell => '/bin/bash',
+    ensure => 'present',
+    comment => 'PostgreSQL Server',
+    uid => '26',
+    gid => '26',
+    home => '/var/lib/pgsql',
+    managehome => true,
+    password => '!!',
   }
 
-  # Do this, as per http://backports-master.debian.org/Instructions/
-  # so we get backports updates.
-  apt::pin{ '*':
-    release  => 'lenny-backports',
-    priority => '200',
-    filename => 'star'
-  }
-
-  file{
-    '/etc/apt/sources.list.d/backports.list':
-      ensure   => file,
-      content  => 'deb http://backports.debian.org/debian-backports squeeze-backports main',
-  }
-
-  exec{
-    'refresh_apts':
-      command     => '/usr/bin/aptitude --quiet update',
-      refreshonly => 'true',
-      subscribe   => File['/etc/apt/sources.list.d/backports.list'],
-  }
-
-  package{ "postgresql-${pgversion}": ensure => present, require => Exec['refresh_apts'] }
-
-
-}
-
-define postgres::user(
-  $user = undef,
-  $password = undef ,
-  $createdb = false,
-  $superuser = false
-) {
-
-  if $user == undef {
-    $username = $name
-  } else {
-    $username = $user
-  }
-
-  if $superuser == true {
-    $su = "-s"
-  } else {
-    $su = "-S"
-  }
-
-  if $createdb == true {
-    $cdb = "-d"
-  } else {
-    $cdb = "-D"
-  }
-
-  exec{ "/usr/bin/createuser ${cdb} ${su} ${username}":
-    user => 'postgres', # run as postgres user.
+  group { 'postgres':
+    ensure => 'present',
+    gid => '26'
   }
 
 }
 
-# TYPE  DATABASE        USER            CIDR-ADDRESS            METHOD
-define postgres::hba(
-  $type = 'user',
-  $database = 'all',
-  $cidr,
-  $method
-) {
-
-  $user = $name
-
-  $pgver = "9.0"
-  $pghbafile = "/etc/postgresql/${pgver}/main/pg_hba.conf"
-
-  $linetoadd = "$type\t$database\t$user\t$cidr\t$method\n"
-
-  exec{
-    'mod_pghba':
-      command => "/usr/bin/printf \"$linetoadd\" >>$pghbafile && /usr/sbin/invoke-rc.d postgresql reload",
-      unless  => "/bin/grep -q \"$linetoadd\" $pghbafile",
+# Initialize the database with the postgres_password password.
+define postgres::initdb() {
+  if $postgres_password == "" {
+    exec {
+        "InitDB":
+          command => "/bin/chown postgres.postgres /var/lib/pgsql && /bin/su  postgres -c \"/usr/bin/initdb /var/lib/pgsql/data -E UTF8\"",
+          require =>  [User['postgres'],Package["postgresql${postgres_version}-server"]],
+          unless => "/usr/bin/test -e /var/lib/pgsql/data/PG_VERSION",
+    }
+  } else {
+    exec {
+        "InitDB":
+          command => "/bin/chown postgres.postgres /var/lib/pgsql && echo \"${postgres_password}\" > /tmp/ps && /bin/su  postgres -c \"/usr/bin/initdb /var/lib/pgsql/data --auth='password' --pwfile=/tmp/ps -E UTF8 \" && rm -rf /tmp/ps",
+          require =>  [User['postgres'],Package["postgresql${postgres_version}-server"]],
+          unless => "/usr/bin/test -e /var/lib/pgsql/data/PG_VERSION ",
+    }
   }
+}
 
+# Start the service if not running
+define postgres::enable {
+  service { postgresql:
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Exec["InitDB"],
+  }
+}
+
+
+# Postgres host based authentication 
+define postgres::hba ($postgres_password="",$allowedrules){
+  file { "/var/lib/pgsql/data/pg_hba.conf":
+    content => template("postgres/pg_hba.conf.erb"),	
+    owner  => "root",
+    group  => "root",
+    notify => Service["postgresql"],
+ #   require => File["/var/lib/pgsql/.order"],
+    require => Exec["InitDB"],
+  }
+}
+
+define postgres::config ($listen="localhost")  {
+  file {"/var/lib/pgsql/data/postgresql.conf":
+    content => template("postgres/postgresql.conf.erb"),
+    owner => postgres,
+    group => postgres,
+    notify => Service["postgresql"],
+  #  require => File["/var/lib/pgsql/.order"],
+    require => Exec["InitDB"],
+  }
+}
+
+# Base SQL exec
+define sqlexec($username, $password, $database, $sql, $sqlcheck) {
+  if $postgres_password == "" {
+    exec{ "psql -h localhost --username=${username} $database -c \"${sql}\" >> /var/lib/puppet/log/postgresql.sql.log 2>&1 && /bin/sleep 5":
+      path        => $path,
+      timeout     => 600,
+      unless      => "psql -U $username $database -c $sqlcheck",
+      require =>  [User['postgres'],Service[postgresql]],
+    }
+  } else {
+    exec{ "psql -h localhost --username=${username} $database -c \"${sql}\" >> /var/lib/puppet/log/postgresql.sql.log 2>&1 && /bin/sleep 5":
+      environment => "PGPASSWORD=${postgres_password}",
+      path        => $path,
+      timeout     => 600,
+      unless      => "psql -U $username $database -c $sqlcheck",
+      require =>  [User['postgres'],Service[postgresql]],
+    }
+  }
+}
+
+# Create a Postgres user
+define postgres::createuser($passwd) {
+  sqlexec{ createuser:
+    password => $postgres_password, 
+    username => "postgres",
+    database => "postgres",
+    sql      => "CREATE ROLE ${name} WITH LOGIN PASSWORD '${passwd}';",
+    sqlcheck => "\"SELECT usename FROM pg_user WHERE usename = '${name}'\" | grep ${name}",
+    require  =>  Service[postgresql],
+  }
+}
+
+# Create a Postgres db
+define postgres::createdb($owner) {
+  sqlexec{ $name:
+    password => $postgres_password, 
+    username => "postgres",
+    database => "postgres",
+    sql => "CREATE DATABASE $name WITH OWNER = $owner ENCODING = 'UTF8';",
+    sqlcheck => "\"SELECT datname FROM pg_database WHERE datname ='$name'\" | grep $name",
+    require => Service[postgresql],
+  }
 }
--- site/puppetlabs/manifests/vor.pp
+++ site/puppetlabs/manifests/vor.pp
@@ -4,21 +4,24 @@ class puppetlabs::vor {
   # class.
   include postgres::install::debian
 
+  postgres::config{ "host": listen => "*", }
+  postgres::hba { "host": allowedrules => [ "host    DATABASE all    192.168.100.0/24  md5", ], }
+  postgres::enable{ "host": }
+
   # class{ 'postgres':
   #   require => Package['postgresql-9.0']
   # }
-
-  postgres::user{ 'superduperuser':
-    createdb  => true,
-    superuser => true,
-  }
-
-  postgres::hba{ 'superduperuser':
-    type     => 'user',
-    database => 'ALL',
-    cidr     => '192.168.100.0/24',
-    method   => 'MD5',
-  }
-
+  #   postgres::user{ 'superduperuser':
+  #     createdb  => true,
+  #     superuser => true,
+  #   }
+  # 
+  #   postgres::hba{ 'superduperuser':
+  #     type     => 'user',
+  #     database => 'ALL',
+  #     cidr     => '192.168.100.0/24',
+  #     method   => 'MD5',
+  #   }
+  # 
+  # }
 }
-

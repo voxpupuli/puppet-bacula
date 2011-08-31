f86abe82f97478e6881eceb4e993ca4018946444
Zach Leslie <zach@puppetlabs.com>
2011-08-31 13:52:09 -0700

    Make nagios code neater make nagios check all disks Increase ping time from 100 to 150 ms

--- dist/nagios/manifests/init.pp
+++ dist/nagios/manifests/init.pp
@@ -29,12 +29,13 @@ class nagios (
     require => Package[$nagios::params::nrpe_packages],
   }
 
+  # File that contains some nrpd command "aliases"
   file { $nagios::params::nrpe_configuration:
-    ensure => present,
-    owner => nagios,
-    group => nagios,
+    ensure  => present,
+    owner   => nagios,
+    group   => nagios,
     content => template('nagios/nrpe.cfg.erb'),
-    notify => Service[$nagios::params::nrpe_service],
+    notify  => Service[$nagios::params::nrpe_service],
     require => File['/etc/nagios'],
   }
 
@@ -58,7 +59,7 @@ class nagios (
 
   @@nagios_service { "check_ping_${hostname}":
     use                 => 'generic-service',
-    check_command       => 'check_ping!100.0,20%!500.0,60%',
+    check_command       => 'check_ping!150.0,20%!500.0,60%',
     host_name           => "$fqdn",
     service_description => "check_ping_${hostname}",
     target              => '/etc/nagios3/conf.d/nagios_service.cfg',
@@ -66,61 +67,61 @@ class nagios (
   }
 
   @@nagios_service { "check_ssh_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_ssh', 
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_ssh',
     service_description => "check_ssh_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_dns_${hostname}":
-    ensure => absent,
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_dns',
+    ensure              => absent,
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_dns',
     service_description => "check_dns_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_disk_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe_1arg!check_xvda',
-    service_description => "check_disk_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe_1arg!check_alldisks',
+    service_description => "check_alldisks_${hostname}",
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_load_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe_1arg!check_load',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe_1arg!check_load',
     service_description => "check_load_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_zombie_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe_1arg!check_zombie_procs',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe_1arg!check_zombie_procs',
     service_description => "check_zombie_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
   @@nagios_service { "check_munin-node_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe!check_proc!1:1 munin-node',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe!check_proc!1:1 munin-node',
     service_description => "check_munin-node_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
-  @firewall { 
+  @firewall {
     '0120-INPUT ACCEPT 5666':
       jump   => 'ACCEPT',
       dport  => '5666',

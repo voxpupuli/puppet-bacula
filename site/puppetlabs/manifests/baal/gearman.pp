96734d849b1564aa38919f0c7e6ae39f7113cf42
Zach Leslie <zach@puppetlabs.com>
2011-09-02 13:48:14 -0700

    add packages for gearman

--- site/puppetlabs/manifests/baal/gearman.pp
+++ site/puppetlabs/manifests/baal/gearman.pp
@@ -1,4 +1,22 @@
 class puppetlabs::baal::gearman {
 
+  packages = [
+    "autoconf",
+    "automake",
+    "make",
+    "gcc",
+    "g++",
+    "wget",
+    "tar",
+    "file",
+    "netcat",
+    "uuid-dev",
+    "libltdl3-dev",
+    "libncurses5-dev",
+    "libevent-dev",
+  ]
+
+  package { $packages: ensure => installed; }
 
 }
+

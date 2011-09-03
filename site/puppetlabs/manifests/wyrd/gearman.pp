8314bf078367696307f04dc5d17d63c1882caca5
Zach Leslie <zach@puppetlabs.com>
2011-09-03 15:55:37 -0700

    add file for worker, service for woker, and stop replacing the file when I make edits

--- site/puppetlabs/manifests/wyrd/gearman.pp
+++ site/puppetlabs/manifests/wyrd/gearman.pp
@@ -16,6 +16,18 @@ class puppetlabs::wyrd::gearman {
     shell => "/bin/bash",
   }
 
+  service { "mod-gearman-worker":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+  }
+
+  $key = 'FpIHcrKjZrZy2DYzhEMog9OLwAD4KuV'
+  file { "/etc/mod-gearman/worker.conf":
+    replace => false,
+    content => template("nagios/worker.conf.erb"),
+    notify  => Service["mod-gearman-worker"],
+  }
 
 }
 

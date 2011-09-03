4c8931148d2792257e8372e6b9fe9cf0d1e0bfd8
Zach Leslie <zach@puppetlabs.com>
2011-09-03 16:02:07 -0700

    Make xaman allow its own ip address for local checks

--- dist/pdns/manifests/nagios.pp
+++ dist/pdns/manifests/nagios.pp
@@ -13,12 +13,12 @@
 class pdns::nagios {
 
   @@nagios_service { "check_pdns_${hostname}":
-    use => 'generic-service',
-    host_name => "$fqdn",
-    check_command => 'check_nrpe!check_proc!1:5 pdns_server',
+    use                 => 'generic-service',
+    host_name           => "$fqdn",
+    check_command       => 'check_nrpe!check_proc!1:5 pdns_server',
     service_description => "check_pdns_${hostname}",
-    target => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify => Service[$nagios::params::nagios_service],
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
   }
 
 }

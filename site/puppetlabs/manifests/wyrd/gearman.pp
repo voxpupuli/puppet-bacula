deddde883f9340d5407a827bf256024cd7e535e9
Zach Leslie <zach@puppetlabs.com>
2011-09-03 15:42:37 -0700

    setup apt source for wheezy Add packages for mod-gearman on wyrd set shell on nagios user for gearman to work add gearman service and config file

--- site/puppetlabs/manifests/wyrd/gearman.pp
+++ site/puppetlabs/manifests/wyrd/gearman.pp
@@ -1,8 +1,33 @@
 class puppetlabs::wyrd::gearman {
 
-  include nagios::params
+  file {
+    "/etc/apt/sources.list.d/wheezy.list":
+      content => "deb http://ftp.us.debian.org/debian/ wheezy main",
+      notify  => Exec["apt-get update"]
+  }
 
-  package { "$nagios_plugin_packages": ensure => installed; }
+  $packages = [
+    "mod-gearman-worker"
+  ]
 
+  package { $packages: ensure => installed; }
+
+  user { "nagios":
+    shell => "/bin/bash",
+  }
+
+  service { "mod-gearman-worker":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+  }
+
+  $key = 'FpIHcrKjZrZy2DYzhEMog9OLwAD4KuV'
+  file { "/etc/mod-gearman/worker.conf":
+    replace => false,
+    content => template("nagios/worker.conf.erb"),
+    notify  => Service["mod-gearman-worker"],
+  }
 
 }
+

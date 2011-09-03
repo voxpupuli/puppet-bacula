0fdae59b2737d7b1e5e6d112b60199b763d6a546
Zach Leslie <zach@puppetlabs.com>
2011-09-03 15:53:33 -0700

    add actual packages, and nagios-plugins already gets installed

--- site/puppetlabs/manifests/wyrd/gearman.pp
+++ site/puppetlabs/manifests/wyrd/gearman.pp
@@ -1,8 +1,26 @@
 class puppetlabs::wyrd::gearman {
 
-  include nagios::params
+  file {
+    "/etc/apt/sources.list.d/wheezy.list":
+      content => "deb http://ftp.us.debian.org/debian/ wheezy main",
+      notify  => Exec["apt-get update"]
+  }
 
-  package { $::nagios::params::nagios_plugin_packages: ensure => installed; }
+  $packages = [
+    "gearman",
+    "gearman-tools",
+    "mod-gearman-doc",
+    "mod-gearman-module",
+    "mod-gearman-tools",
+    "mod-gearman-worker"
+  ]
+
+  package { $packages: ensure => installed; }
+
+  user { "nagios":
+    shell => "/bin/bash",
+  }
+
 
 }
 

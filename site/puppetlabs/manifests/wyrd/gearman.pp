551140151d57b2162705e6adef4442a8007a4684
Zach Leslie <zach@puppetlabs.com>
2011-09-03 15:50:06 -0700

    test quoting

--- site/puppetlabs/manifests/wyrd/gearman.pp
+++ site/puppetlabs/manifests/wyrd/gearman.pp
@@ -2,7 +2,7 @@ class puppetlabs::wyrd::gearman {
 
   include nagios::params
 
-  package { "$::nagios::params::nagios_plugin_packages": ensure => installed; }
-
+  package { $::nagios::params::nagios_plugin_packages: ensure => installed; }
 
 }
+

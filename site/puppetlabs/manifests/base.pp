158aec3a60e08625e7c449161f8b4792e0c40ecb
Zach Leslie <zach@puppetlabs.com>
2011-09-14 13:24:54 -0700

    move remaining puppetlabs classes into the proper role

--- site/puppetlabs/manifests/base.pp
+++ site/puppetlabs/manifests/base.pp
@@ -43,7 +43,24 @@ class puppetlabs::base {
 
   case $role {
     workstation: { }
-    server: { }
+    server: {
+      # SSH
+      include ssh::server
+      ssh::allowgroup  { "sysadmin": }
+
+      # Sudo
+      include sudo
+      sudo::allowgroup { "sysadmin": }
+
+      # Accounts
+      # This should probably be more selective on certain hosts/distros/oses
+      include virtual::users
+      Account::User <| tag == 'allstaff' |>
+      Group         <| tag == 'allstaff' |>
+
+      # Firewall
+      if defined(Class["firewall"]) { Firewall <||> }
+    }
     unknown: { }
     default: { }
   }

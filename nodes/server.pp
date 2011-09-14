18213d50a035662cc7ac1439474c48e91a8e67a1
Zach Leslie <zach@puppetlabs.com>
2011-09-14 16:52:25 -0700

    add ndoes directory and the contents of puppetlabs class and puppetlabs::base class

---
+++ nodes/server.pp
@@ -0,0 +1,23 @@
+node server inherits basenode {
+
+  class { "nagios": nrpe_server  => hiera("nrpe_server");  }
+  class { 'munin':  munin_server => hiera("munin_server"); }
+
+  # SSH
+  include ssh::server
+  ssh::allowgroup  { "sysadmin": }
+
+  # Sudo
+  include sudo
+  sudo::allowgroup { "sysadmin": }
+
+  # Accounts
+  # This should probably be more selective on certain hosts/distros/oses
+  include virtual::users
+  Account::User <| tag == 'allstaff' |>
+  Group         <| tag == 'allstaff' |>
+
+  # Firewall
+  if defined(Class["firewall"]) { Firewall <||> }
+
+}

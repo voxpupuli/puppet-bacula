a112a8fb82c126f524551045475fe28b2eeea7e8
Adrien Thebo <adrien@puppetlabs.com>
2011-09-14 10:00:23 -0700

    Add puppet doc to things. And formatting. And things.

--- dist/apt/manifests/backports.pp
+++ dist/apt/manifests/backports.pp
@@ -1,7 +1,26 @@
-# including this will get a backports for whatever version of
-# debian/ubuntu you're running. You can set it if you feel you must,
-# but don't.
-
+# This adds the necessary components to get backports for ubuntu and debian
+#
+# == Parameters
+#
+# [*release*]
+#   The ubuntu/debian release name. Defaults to $lsbdistcodename. Setting this
+#   manually can cause undefined behavior. (Read: universe exploding)
+#
+# == Examples
+#
+#   include apt::backports
+#
+#   class { 'apt::backports':
+#     release => 'natty',
+#   }
+#
+# == Authors
+#
+# Ben Hughes, I think. At least blame him if this goes wrong. I just added puppet doc.
+#
+# == Copyright
+#
+# Copyright 2011 Puppet Labs Inc, unless otherwise noted.
 class apt::backports( $release=undef ) {
 
   if release == undef {
@@ -10,43 +29,39 @@ class apt::backports( $release=undef ) {
     $releasename = $release
   }
 
-  if $operatingsystem == 'debian' {
-    $repourl = 'http://backports.debian.org/debian-backports'
-    $repocomponent = 'main'
+  case $operatingsystem {
+    'Debian': {
+      $repourl = 'http://backports.debian.org/debian-backports'
+      $repocomponent = 'main'
+    }
+    'Ubuntu': {
+      $repourl = 'http://archive.ubuntu.com/ubuntu'
+      $repocomponent = 'universe multiverse restricted'
+    }
+    default: {
+      fail( "$module_name is Debian & Umbongo only." )
+    }
   }
-
-  if $operatingsystem == 'ubuntu' {
-    $repourl = 'http://archive.ubuntu.com/ubuntu'
-    $repocomponent = 'universe multiverse restricted'
-  }
-
-  if $repourl == undef {
-    fail( "$module_name is Debian & Umbongo only." )
-  }
-
-
-  # Defaults for this class, because I'm lazy.
-  File{ owner => 'root', group => 'root', mode => '0644' }
 
   # Do this, as per http://backports-master.debian.org/Instructions/
   # so we get backports updates.
-  apt::pin{ '*':
+  apt::pin { '*':
     release  => "${releasename}-backports",
     priority => '200',
     filename => 'star'
   }
 
-  file{
-    '/etc/apt/sources.list.d/backports.list':
-      ensure   => file,
-      content  => "deb ${repourl} ${releasename}-backports ${repocomponent}",
+  file { '/etc/apt/sources.list.d/backports.list':
+    ensure   => file,
+    content  => "deb ${repourl} ${releasename}-backports ${repocomponent}",
+    owner    => 'root',
+    group    => 'root',
+    mode     => '0644',
   }
 
-  exec{
-    'refresh_apts_pre_postgres_install':
-      command     => '/usr/bin/aptitude --quiet update',
-      refreshonly => 'true',
-      subscribe   => File['/etc/apt/sources.list.d/backports.list'],
+  exec { 'Refresh apt cache after adding backports':
+    command     => '/usr/bin/aptitude --quiet update',
+    refreshonly => 'true',
+    subscribe   => File['/etc/apt/sources.list.d/backports.list'],
   }
-
 }

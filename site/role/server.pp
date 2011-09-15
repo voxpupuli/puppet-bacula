5a7a379291fbc0405687a05fb02d998df7c9dad1
Zach Leslie <zach@puppetlabs.com>
2011-09-15 16:52:54 -0700

    add role::server class

---
+++ site/role/server.pp
@@ -0,0 +1,24 @@
+class role::server {
+  include role::base
+
+  class { "nagios": nrpe_server  => hiera("nrpe_server");  }
+  class { 'munin':  munin_server => hiera("munin_server"); }
+  class { "ntp":    server       => hiera("ntpserver"); }
+
+  # SSH
+  include ssh::server
+  ssh::allowgroup  { "sysadmin": }
+
+  # Sudo
+  include sudo
+  sudo::allowgroup { "sysadmin": }
+
+  # Accounts
+  # This should probably be more selective on certain hosts/distros/oses
+  include virtual::users
+  Account::User <| tag == 'allstaff' |>
+  Group         <| tag == 'allstaff' |>
+
+  # Firewall
+  if defined(Class["firewall"]) { Firewall <||> }
+}

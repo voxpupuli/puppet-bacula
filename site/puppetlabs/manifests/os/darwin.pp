3e112a36cce6db40b8cbe3488c654cccfde9188e
Zach Leslie <zach@puppetlabs.com>
2011-09-21 18:38:37 -0700

    add cronjob to update macports

--- site/puppetlabs/manifests/os/darwin.pp
+++ site/puppetlabs/manifests/os/darwin.pp
@@ -1,4 +1,10 @@
 class puppetlabs::os::darwin {
 
+  cron { "update macports":
+    command => "/opt/local/bin/port -q selfupdate",
+    minute  => 0,
+    hour    => 0,
+    weekday => 0,
+  }
 
 }

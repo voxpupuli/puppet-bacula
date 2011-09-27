82c4dc3cc4d00cd46128511ee55c7cffc4fa0e5e
Ben Hughes <ben@puppetlabs.com>
2011-09-27 16:17:35 -0700

    Move dxul back to pointing at a commit for forge

--- nodes/dxul.pp
+++ nodes/dxul.pp
@@ -15,8 +15,7 @@ node dxul {
   # Puppet Forge
   class { 'forge':
     vhost        => 'forge.puppetlabs.com',
-    # git_revision => 'c007fde8d3de345bfb31adf295b78fdb68c33541',
-    git_revision => 'HEAD',
+    git_revision => 'c007fde8d3de345bfb31adf295b78fdb68c33541',
   }
 
   # Backup

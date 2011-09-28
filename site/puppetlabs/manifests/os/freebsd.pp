99e8528d8c31607e4f4a6e0723db04c0dcf415e2
Ben Hughes <ben@puppetlabs.com>
2011-09-28 16:41:32 -0700

    try alternate freebsd package provider

--- site/puppetlabs/manifests/os/freebsd.pp
+++ site/puppetlabs/manifests/os/freebsd.pp
@@ -3,7 +3,7 @@ class puppetlabs::os::freebsd {
   # Install some basic packages. Nothing too spicy.
   package{ [ 'tmux', 'pv', 'netcat', 'lsof', 'vim-lite']:
     ensure   => present,
-    provider => freebsd,
+    # provider => freebsd,
   }
 
 }

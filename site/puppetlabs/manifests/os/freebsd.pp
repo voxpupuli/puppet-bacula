7b533ab43dc3f180a14b55e6f597e83afd6c3463
Ben Hughes <ben@puppetlabs.com>
2011-09-28 16:47:07 -0700

    use "full" filenames for packages to install on freebsd

--- site/puppetlabs/manifests/os/freebsd.pp
+++ site/puppetlabs/manifests/os/freebsd.pp
@@ -1,7 +1,13 @@
 class puppetlabs::os::freebsd {
 
+  $packages_to_install = [  'sysutils/tmux',
+                            'sysutils/pv',
+                            'net/netcat',
+                            'sysutils/lsof',
+                            'editors/vim-lite' ]
+
   # Install some basic packages. Nothing too spicy.
-  package{ [ 'tmux', 'pv', 'netcat', 'lsof', 'vim-lite']:
+  package{ $packages_to_install:
     ensure   => present,
     # provider => freebsd,
   }

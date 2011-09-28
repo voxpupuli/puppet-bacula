59ae851017b802e69cdd45de496830d3768263b8
Ben Hughes <ben@puppetlabs.com>
2011-09-28 16:58:32 -0700

    install sane shells (zsh, bash) by default on FreeBSD

--- site/puppetlabs/manifests/os/freebsd.pp
+++ site/puppetlabs/manifests/os/freebsd.pp
@@ -2,6 +2,8 @@ class puppetlabs::os::freebsd {
 
   $packages_to_install = [  'sysutils/tmux',
                             'sysutils/pv',
+                            'shells/bash',
+                            'shells/zsh',
                             'net/netcat',
                             'sysutils/lsof',
                             'editors/vim-lite' ]
@@ -14,8 +16,9 @@ class puppetlabs::os::freebsd {
 
   # This is horrible, but it stops a lot of things breaking (concat for example)
   file{ '/bin/bash':
-    ensure => link,
-    target => '/usr/local/bin/bash',
+    ensure   => link,
+    target   => '/usr/local/bin/bash',
+    requires => Package['shells/bash'],
   }
 
 }

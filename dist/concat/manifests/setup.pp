e9fb75e452bf235fc90d52440a1a8e0b1a750a74
Ben Hughes <ben@puppetlabs.com>
2011-09-28 16:50:11 -0700

    make concat FreeBSD path aware

--- dist/concat/manifests/setup.pp
+++ dist/concat/manifests/setup.pp
@@ -5,7 +5,12 @@
 #
 # It also copies out the concatfragments.sh file to /usr/local/bin
 class concat::setup {
-    $concatdir = "/var/lib/puppet/concat"
+
+    $concatdir = $operatingsystem ? {
+      FreeBSD => "/var/puppet/concat",
+      default => "/var/lib/puppet/concat",
+    }
+
     $sortpath = $operatingsystem ? {
         Darwin  => "/usr/bin/sort",
         Ubuntu  => "/usr/bin/sort",

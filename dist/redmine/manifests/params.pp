b0d82e7b3f9b58a1ba4a0c371a4ece5519daee30
Ben Hughes <ben@puppetlabs.com>
2011-09-29 15:00:43 -0700

    Bump the version of redmine to the one we're using out of our own repo

--- dist/redmine/manifests/params.pp
+++ dist/redmine/manifests/params.pp
@@ -1,5 +1,5 @@
 class redmine::params {
-  $version = '1faf02c9f5ce22ce676e7a3ae710dd0c52a21d45'
+  $version = 'd0427b732c9e9d1d1aa927e1dfd67bd166f58b17'
   $source  = 'git://github.com/puppetlabs/redmine'
   $rails_version = '2.3.12'
 }

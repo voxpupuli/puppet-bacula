b40178b363dac1f1d8a1bf1353f22c2aebef5735
Ben Hughes <ben@puppetlabs.com>
2011-09-28 17:42:52 -0700

    make shells module slightly more OS agnostic

--- dist/packages/manifests/shells.pp
+++ dist/packages/manifests/shells.pp
@@ -1,8 +1,29 @@
 # to make it easier to grab the neede package one-offs
+# Make them OS indepedant.
 
 class packages::shells {
 
-  @package { "zsh":  ensure => installed; }
-  @package { "bash": ensure => installed; }
+  $bash = $operatingsystem ? {
+    'freebsd' => 'shells/bash',
+    default   => 'bash'
+  }
+
+  $zsh = $operatingsystem ? {
+    'freebsd' => 'shells/zsh',
+    default   => 'zsh'
+  }
+
+
+  # Now do a virtual package for each shell.
+  @package { $zsh:
+    alias => 'zsh',
+    ensure => installed,
+  }
+
+  @package { $bash:
+    alias => 'bash',
+    ensure => installed,
+  }
+
 
 }

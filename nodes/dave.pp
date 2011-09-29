2391437dec0b0fe2e012e659dde085033d139148
Ben Hughes <ben@puppetlabs.com>
2011-09-28 18:59:40 -0700

    Throw sudoers on dave

--- nodes/dave.pp
+++ nodes/dave.pp
@@ -7,8 +7,9 @@ node 'dave.dc1.puppetlabs.net' {
 
   # Accounts
   # This should probably be more selective on certain hosts/distros/oses
-  include packages::shells
   include virtual::users
   Account::User <| groups == 'sysadmin' |>
   Group         <| tag == 'allstaff' |>
+
+  include sudo
 }

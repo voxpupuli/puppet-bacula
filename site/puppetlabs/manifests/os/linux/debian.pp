ee649c9a649d77b0e047814c7247a07c2606ab99
Zach Leslie <zach@puppetlabs.com>
2011-10-05 20:49:23 -0700

    de-cruft puppetlabs::os::linux::debian

--- site/puppetlabs/manifests/os/linux/debian.pp
+++ site/puppetlabs/manifests/os/linux/debian.pp
@@ -3,13 +3,6 @@ class puppetlabs::os::linux::debian  {
 
   include harden
 
-  # This doesn't help at all, since the provider is actually adduser, not useradd
-  # should be removed, class and module included
-  #class { "useradd::settings":
-  #  last_uid => '1099',
-  #  last_gid => '1099',
-  #}
-
   case $domain {
     "puppetlabs.lan": {
       # Setup apt settings specific to the lan
@@ -41,6 +34,7 @@ class puppetlabs::os::linux::debian  {
     "/etc/apt/sources.list.d/ops.list":
       ensure   => absent,
   }
+
   file {
     "/etc/apt/sources.list.d/puppetlabs.list":
       content => "deb http://apt.puppetlabs.com/debian squeeze main\n",

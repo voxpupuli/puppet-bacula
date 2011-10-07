c40239aa640b5306ba64c3ebc182c995dc2fc6fd
Zach Leslie <zach@puppetlabs.com>
2011-10-07 13:01:05 -0700

    implement ssl in docs make sure vhost supports ssl make sure that the https listening port is configurable add apache::vhost for ssl to puppetlabs::docs

--- site/puppetlabs/manifests/docs.pp
+++ site/puppetlabs/manifests/docs.pp
@@ -1,5 +1,6 @@
 class puppetlabs::docs (
-  $port='80'
+  $http_port  ='80',
+  $https_port ='443'
   ) {
   #
   # This is temp so as to not purge the puppetlabs.com vhost.
@@ -8,7 +9,7 @@ class puppetlabs::docs (
 
   apache::vhost::redirect {
     'docs.reductivelabs.com':
-      port       => $port,
+      port       => $http_port,
       dest       => 'http://docs.puppetlabs.com',
       status     => '301'
   }
@@ -16,12 +17,24 @@ class puppetlabs::docs (
   # This should be port 82 when running on web01
   apache::vhost {
     'docs.puppetlabs.com':
-      port          => $port,
+      port          => $http_port,
       docroot       => $docroot,
       template      => 'puppetlabs/docs_vhost.erb',
       ssl           => false,
       priority      => 20,
   }
+
+  # HTTPS added for #9976
+  apache::vhost {
+    'docs.puppetlabs.com':
+      port          => $https_port,
+      docroot       => $docroot,
+      template      => 'puppetlabs/docs_vhost.erb',
+      ssl           => true,
+      priority      => 20,
+  }
+
+
   #
   # Since this sourced from git we should probably use VCS repo and a variable for the tag to make up date these files.
   #

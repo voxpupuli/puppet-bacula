0aa8caa46ebcca34856cf8ba3f0209eb8bfdbd79
Ben Hughes <ben@puppetlabs.com>
2011-10-10 17:35:06 -0700

    Update postfix to support centos (5.6 at current, but should work on later versions). Make the postfix main.cf pull in the ca-certfiles from params file now too.

--- dist/postfix/manifests/params.pp
+++ dist/postfix/manifests/params.pp
@@ -8,6 +8,7 @@ class postfix::params {
       $postfix_newaliases  = '/usr/bin/newaliases'
       $postfix_maincf = '/etc/postfix/main.cf'
       $postfix_maincf_erb = 'postfix/main.cf.debian.erb'
+      $ssl_certs = '/etc/ssl/certs/ca-certificates.crt'
     }
     'freebsd': {
       $postfix_package = 'mail/postfix'
@@ -16,6 +17,16 @@ class postfix::params {
       $postfix_newaliases  = '/usr/local/bin/newaliases'
       $postfix_maincf = '/usr/local/etc/postfix/main.cf'
       $postfix_maincf_erb = 'postfix/main.cf.freebsd.erb'
+      $ssl_certs = '/usr/local/share/certs/ca-root-nss.crt'
+    }
+    'centos': {
+      $postfix_package = 'postfix'
+      $postfix_service = 'postfix'
+      $postfix_alias_files = '/etc/postfix/aliases'
+      $postfix_newaliases  = '/usr/bin/newaliases'
+      $postfix_maincf = '/etc/postfix/main.cf'
+      $postfix_maincf_erb = 'postfix/main.cf.centos.erb'
+      $ssl_certs = '/etc/pki/tls/certs/ca-bundle.crt'
     }
     default: {
       fail("Please check ${module_name} for this OS.")

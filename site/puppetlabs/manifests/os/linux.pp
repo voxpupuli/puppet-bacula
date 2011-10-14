99a3c15d165702caf21110408216314a9f642f43
Zach Leslie <zach@puppetlabs.com>
2011-10-14 11:36:23 -0700

    begin adding scripts to linux

--- site/puppetlabs/manifests/os/linux.pp
+++ site/puppetlabs/manifests/os/linux.pp
@@ -5,6 +5,12 @@ class puppetlabs::os::linux {
   include virtual::packages
   include packages
 
+  File {
+    owner => root,
+    group => root,
+    mode  => 644
+  }
+
   file {
     "/etc/timezone":  owner => root, group => root, mode => 644, content => "America/Vancouver";
     "/etc/localtime": owner => root, group => root, mode => 644, source  => "/usr/share/zoneinfo/America/Vancouver";
@@ -15,6 +21,12 @@ class puppetlabs::os::linux {
       command => "/usr/bin/find /tmp -mtime +3 -print0 | xargs --no-run-if-empty -0 rm",
       ensure  => present,
       user => root,  minute => 10,  hour => 20, weekday => 0;
+  }
+
+  file {
+    "/usr/local/bin/weigh":
+      mode   => 755,
+      source => "puppet:///modules/puppetlabs/usr/local/bin/weigh",
   }
 
 }

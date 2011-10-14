baeccb6353d64c8a0035edc33986e81b3e2e3c85
Zach Leslie <zach@puppetlabs.com>
2011-10-14 14:03:56 -0700

    update denyhosts to use template for config file for use with passing ADMIN_EMAIL and soon others add service for reloading configuration

--- dist/denyhosts/manifests/init.pp
+++ dist/denyhosts/manifests/init.pp
@@ -1,5 +1,22 @@
-class denyhosts {
+class denyhosts (
+  $adminemail = "root@localhost"
+  ) {
 
   package { "denyhosts": ensure => installed; }
+
+  file { "/etc/denyhosts.conf":
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    content => template("denyhosts/denyhosts.conf.erb"),
+    notify  => Service["denyhosts"],
+  }
+
+  service {
+    "denyhosts":
+      ensure  => running,
+      enable  => true,
+      pattern => "denyhosts",
+  }
 
 }

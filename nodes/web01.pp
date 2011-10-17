f49ae8581eb4be49d6a5bb8a719d7a2236ca035b
Ben Hughes <ben@puppetlabs.com>
2011-10-16 21:14:02 -0700

    Hopefully do all the git/wordpress/syncing crap in one go this time.

--- nodes/web01.pp
+++ nodes/web01.pp
@@ -41,12 +41,20 @@ node web01 {
   file{ '/home/wordpresssync': ensure => directory, owner => 'wordpresssync', group => 'wordpresssync', } ->
   ssh::allowgroup { 'wordpresssync': } ->
   sudo::entry{ "wordpresssync": entry => "wordpresssync ALL=(ALL) NOPASSWD: /usr/local/bin/wordpress_db_dumper.sh\n", } ->
+  sudo::entry{ "wordpresssync": entry => "wordpresssync ALL=(ALL) NOPASSWD: /usr/bin/rsync --server --sender -logDtprCe.iLsf . /var/www/puppetlabs.com/", } ->
   ssh_authorized_key{ 'wordpresssync@wordpresssync':
     key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6Y9S5IxfMwDwiy8gMwYJ3dy5aAidOzRQJSTC84TsJ8GCtBabpyRlvmPi04q9h7qf6YY8ltY+1P3VyFvGNXzKrYvBRXs0atu2fr7RjsZqNIjpIAqLRadp+THx1jPdN8Dkk5ZRIC/3chOsY0GZ5ayu7N9JrUR8OBg0TbMvq7YAeG/cFWmNTQP02OVskF4nl9MDtEtigT3eLsDq5HjBc36koiiB85lptvds3uqDgiYG0D9AccgLD0zAu1aijWf7Xk9R5X7hwjokzKkaOF5S236T9GXFB2mrBHF/E1amrC59qd6eRTtJRrlmH+VXJSgIe0AAqv9af+F4JRcs8SVbf2gNF',
     type    => 'ssh-rsa',
     ensure  => 'present',
     user    => 'wordpresssync',
-    options => $ssh_options,
+    options => $ssh_options;
+  'wordpressrsync@wordpresssync':
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDWtNmx5XnCrP+8jpBurQ6r9xdx9pPz4bS7lOmdwXpPfpz4mZvtO6nzsBWHh/THsJtHRHCA580UzK/4Y+7vBoIOxYUVbbiHssVcudLpZcTrhOCYr3skTwzANi0vc5449/GNdqAeHsT4xI59/XmJrlB9lhU/jLUpf1HESAZYt+5HvJnJZh84yb4JJD9CTqqDsBxFlA9DcMZ0VOJmGjjowvatpdMlgzy4N+pIRYPPuCisaEpho4sRsasjCvLmQTbRBd58Eeg4J5QVBtmx/ieq1uj09AJOCxiFN8MmJRrMWhY5xPPDDz5Yf79mhR7cDCRtO5ijgsLkBAdPNMj983lDD9Zl',
+    type    => 'ssh-rsa',
+    ensure  => 'present',
+    user    => 'wordpresssync',
+    options => [ 'command="sudo rsync --server --sender -logDtprCe.iLsf . /var/www/puppetlabs.com/"' ];
   }
+
 
 }

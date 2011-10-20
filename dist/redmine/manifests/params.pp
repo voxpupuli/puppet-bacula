9e4d7dbf520a1166e60a12247658a65f0646cef7
Ben Hughes <ben@puppetlabs.com>
2011-10-19 17:02:49 -0700

    Bump revision SHA1 for redmine so it stays at the correct version

--- dist/redmine/manifests/params.pp
+++ dist/redmine/manifests/params.pp
@@ -1,5 +1,5 @@
 class redmine::params {
-  $version = 'd0427b732c9e9d1d1aa927e1dfd67bd166f58b17'
+  $version = '847ba6fb0acb642ce9d058b24536f9403ba6c7f8'
   $source  = 'git://github.com/puppetlabs/redmine'
   $rails_version = '2.3.12'
 }

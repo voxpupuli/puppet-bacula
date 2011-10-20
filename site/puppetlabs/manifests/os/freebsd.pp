0e860c8e20a7444b8841afa8e67a4423166360c3
Zach Leslie <zach@puppetlabs.com>
2011-10-20 10:49:51 -0700

    add some port management tools on freebsd hosts

--- site/puppetlabs/manifests/os/freebsd.pp
+++ site/puppetlabs/manifests/os/freebsd.pp
@@ -3,6 +3,8 @@ class puppetlabs::os::freebsd {
   $packages_to_install = [  'sysutils/tmux',
                             'sysutils/pv',
                             'sysutils/screen',
+                            'ports-mgmt/portmaster',
+                            'ports-mgmt/portupgrade',
                             'net/netcat',
                             'security/ca_root_nss',
                             'sysutils/lsof',
@@ -13,6 +15,18 @@ class puppetlabs::os::freebsd {
   package{ $packages_to_install:
     ensure   => present,
     # provider => freebsd,
+  }
+
+  package {
+    "ports-mgmt/portaudit":
+      ensure => installed,
+      notify => Exec["/usr/local/sbin/portaudit -Fda"];
+  }
+
+  exec {
+    "/usr/local/sbin/portaudit -Fda":
+      user        => root,
+      refreshonly => true;
   }
 
   # This is horrible, but it stops a lot of things breaking (concat for example)

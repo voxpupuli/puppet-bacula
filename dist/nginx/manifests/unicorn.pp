920809553b6db2b6dc33ecfbcfd383ee06f6f911
Ben Hughes <ben@puppetlabs.com>
2011-10-22 13:48:16 -0700

    Unicorn nginx define added

---
+++ dist/nginx/manifests/unicorn.pp
@@ -0,0 +1,48 @@
+# Define: nginx::server::vhost
+#
+#   nginx vhost. For serving web traffic as you would with apache.
+#
+# Parameters:
+#
+# Requires:
+#   include nginx::server
+#
+define nginx::unicorn(
+  $port,
+  #$dest,
+  $priority   = '10',
+  $template   = 'nginx/vhost-unicorn.conf.erb',
+  $servername = '',
+  $magic      = ''
+  ) {
+
+  include nginx
+
+  if $servername == '' {
+    $srvname = $name
+  } else {
+    $srvname = $servername
+  }
+
+  file {
+    "${nginx::params::vdir}/${priority}-${name}":
+      content => template($template),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '755',
+      require => Package['nginx'],
+      notify  => Service['nginx'],
+  }
+
+  # liberally borrowed from apache module.
+  if ! defined(Firewall["0100-INPUT ACCEPT $port"]) {
+    @firewall {
+      "0100-INPUT ACCEPT $port":
+        jump  => 'ACCEPT',
+        dport => "$port",
+        proto => 'tcp'
+    }
+  }
+
+}
+# EOF

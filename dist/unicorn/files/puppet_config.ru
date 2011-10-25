41550bb88fb7254aeae492bd78681e3f71be70e1
Ben Hughes <ben@puppetlabs.com>
2011-10-24 19:03:27 -0700

    Update the unicorn::app module to allow supplying a bunch more info, depending on the type of unicorn it is.
    Now you can (and some you must) specify:
    
    unicorn_pidfile - pid file for unicorn
    unicorn_socket  - socket location (no tcp support yet)
    backlog			- backlog size for socket, *
    unicorn_worker_processes - number of workers. *
    unicorn_user	- user to change to after fork *
    unicorn_group	- group to change to after fork *
    rack_file		- config.ru file.
    config_{file,template} - the unicorn config file.
    
    (* denotes optional.)
    
    This is so it's all explicit and you don't have to copy files around
    manually, just manually to the git repo. Which apparently is better.
    
    Also give the config file name as an option to unicorn::app too, so we can
    specify for non defaults (puppetmaster for instance.)

---
+++ dist/unicorn/files/puppet_config.ru
@@ -0,0 +1,18 @@
+# a config.ru, for use with every rack-compatible webserver.
+# SSL needs to be handled outside this, though.
+
+# if puppet is not in your RUBYLIB:
+# $:.unshift('/opt/puppet/lib')
+
+$0 = "master"
+#require 'puppet'
+
+# if you want debugging:
+# ARGV << "--debug"
+
+ARGV << "--rack"
+require 'puppet/application/master'
+# we're usually running inside a Rack::Builder.new {} block,
+# therefore we need to call run *here*.
+run Puppet::Application[:master].run
+
--- dist/unicorn/manifests/app.pp
+++ dist/unicorn/manifests/app.pp
@@ -1,11 +1,26 @@
 define unicorn::app (
     $approot,
-    $config     ='config/unicorn.config.rb',
-    $initscript = "unicorn/initscript_newer.erb" #default template location
+    $unicorn_pidfile,
+    $unicorn_socket,
+    $rack_file,
+    $config_template          = 'unicorn/config_unicorn.config.rb.erb',
+    $initscript               = "unicorn/initscript_newer.erb", #default template location
+    $unicorn_backlog          = '2048',
+    $unicorn_worker_processes = '4',
+    $unicorn_user             = '',
+    $unicorn_group            = '',
+    $config_file              = ''
   ) {
 
   # get the common stuff, like the unicon package(s)
   include unicorn
+
+  # If we have been given a config path, use it, if not, make one up.
+  if $config_file == '' {
+    $config = "${approot}/config/unicorn.config.rb"
+  } else {
+    $config = $config_file
+  }
 
   service {
     "unicorn_${name}":
@@ -19,11 +34,23 @@ define unicorn::app (
 
   file {
     "/etc/init.d/unicorn_${name}":
-      owner   => root,
-      group   => root,
-      mode    => 755,
-      content => template("${initscript}"),
-      notify  => Service["unicorn_${name}"],
+        owner   => root,
+        group   => root,
+        mode    => 755,
+        content => template("${initscript}"),
+        notify  => Service["unicorn_${name}"];
+    $config:
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        content => template( $config_template ),
+        notify  => Service["unicorn_${name}"];
+    "${approot}/config.ru":
+        owner  => root,
+        group  => root,
+        mode   => 644,
+        source => $rack_file,
+        notify  => Service["unicorn_${name}"],
   }
 
 }

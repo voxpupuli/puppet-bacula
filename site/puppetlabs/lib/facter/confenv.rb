71ce8c3c6b4739a127ec91d1fc45b778116ff4da
Ben Hughes <ben@puppetlabs.com>
2011-10-24 20:38:38 -0700

    Make confenv more platform agnostic.

--- site/puppetlabs/lib/facter/confenv.rb
+++ site/puppetlabs/lib/facter/confenv.rb
@@ -1,11 +1,25 @@
 Facter.add("confenv") do
   setcode do
-    kernel = Facter.value('kernel')
-    if kernel.match(/FreeBSD/)
-      path = '/usr/local/bin/puppet'
-    elsif
-      path = '/usr/bin/puppet'
+
+    path = nil
+    [ '/usr/bin/puppet', '/usr/local/bin/puppet', '/opt/puppet/bin/puppet'].each do |puppet|
+      if File.exists?( puppet )
+        path = puppet
+        break
+      end
     end
-    %x{#{path} config print environment}.chomp if File.exists?(path)
+
+    return "problem with environment" if path.nil?
+
+    # Split it, in case it is PE. You can compare strings too, it's a little
+    # dirty.
+    if puppetversion.spit( ' ' )[0] > "2.7"
+      env = %x{#{path} config print environment}.chomp
+    else
+      env = %x{#{path} agent --configprint environment}.chomp
+    end
+
+    env
+
   end
 end

b392b1d4ab600732c432dafa49063a1f1610fa4f
Zach Leslie <zach@puppetlabs.com>
2011-10-25 17:37:58 -0700

    add dns to wyrd for a second lan nameserver

--- nodes/wyrd.pp
+++ nodes/wyrd.pp
@@ -12,5 +12,32 @@ node wyrd {
       key => hiera("gearman_key")
   }
 
+  # DNS resolution to internal hosts
+  class {
+    "unbound":
+      interface => ["::0","0.0.0.0"],
+      access    => ["192.168.100.0/24","10.0.0.0/16"],
+  }
+
+  unbound::stub { "puppetlabs.lan":
+    address  => '192.168.100.1',
+    insecure => true,
+  }
+
+  unbound::stub { "100.168.192.in-addr.arpa.":
+    address  => '192.168.100.1',
+    insecure => true,
+  }
+
+  unbound::stub { "puppetlabs.net":
+    address  => '10.0.42.42',
+    insecure => true,
+  }
+
+  unbound::stub { "42.0.10.in-addr.arpa.":
+    address  => '10.0.42.42',
+    insecure => true,
+  }
+
 }
 

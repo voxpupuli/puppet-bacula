38a75b184e1fd2282cdd8c884e0a70c6d5163ab5
Zach Leslie <zach@puppetlabs.com>
2011-10-28 17:26:06 -0700

    alignment

--- dist/nagios/manifests/website.pp
+++ dist/nagios/manifests/website.pp
@@ -16,30 +16,32 @@
 #   nagios::website { "f.q.d.n": }
 #   nagios::website { "f.q.d.n": auth => 'user:password' }
 #
-define nagios::website($auth=undef) {
+define nagios::website(
+  $auth = undef
+  ) {
 
   include nagios::params
 
   if $auth {
     @@nagios_service { "$name":
-      use => 'generic-service',
-      check_command => "check_http_site_auth!$name!$auth",
-      host_name => "$fqdn",
+      use                 => 'generic-service',
+      check_command       => "check_http_site_auth!$name!$auth",
+      host_name           => "$fqdn",
       service_description => "$name",
-      target => '/etc/nagios3/conf.d/nagios_service.cfg',
-      require => Nagios_command['check_http_site'],
-      notify => Service[$nagios::params::nagios_service],
+      target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+      require             => Nagios_command['check_http_site'],
+      notify              => Service[$nagios::params::nagios_service],
     }
   }
   else {
     @@nagios_service { "$name":
-      use => 'generic-service',
-      check_command => "check_http_site!$name",
-      host_name => "$fqdn",
+      use                 => 'generic-service',
+      check_command       => "check_http_site!$name",
+      host_name           => "$fqdn",
       service_description => "$name",
-      target => '/etc/nagios3/conf.d/nagios_service.cfg',
-      require => Nagios_command['check_http_site_auth'],
-      notify => Service[$nagios::params::nagios_service],
+      target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+      require             => Nagios_command['check_http_site_auth'],
+      notify              => Service[$nagios::params::nagios_service],
     }
   }
 }

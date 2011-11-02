2704138492e9ee2b5dd8c854a673e1bbf337b5ee
Zach Leslie <zach@puppetlabs.com>
2011-11-02 11:08:08 -0700

    update local subnets for dave

--- nodes/dave.pp
+++ nodes/dave.pp
@@ -5,11 +5,10 @@
 node 'dave.dc1.puppetlabs.net' {
   include role::server
 
-
   class{ 'ipsec':
     my_ip         => $::ipaddress,
     their_ip      => '74.85.255.4',
-    local_subnet  => '10.0.42.0/24',
+    local_subnet  => '10.0.0.0/16',
     remote_subnet => '192.168.100.0/24',
     local_router  => '10.0.42.1',
     remote_router => '192.168.100.1',

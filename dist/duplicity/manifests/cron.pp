c28657df434ac6bdfa334d7a2f463d7330244e46
Adrien Thebo <adrien@puppetlabs.com>
2011-11-09 14:51:08 -0800

    Add environment manipulation of duplicity cron

--- dist/duplicity/manifests/cron.pp
+++ dist/duplicity/manifests/cron.pp
@@ -2,12 +2,15 @@ define duplicity::cron(
   $ensure = "present",
   $user,
   $target,
+  $gpg_agent_info,
   $options = []
 ) {
 
   include duplicity::client
 
-  $command = inline_template("/usr/bin/duplicity <%= options.join(' ') %> ${name} ${target}")
+  $duplicity_command = inline_template("/usr/bin/duplicity <%= options.join(' ') %> ${name} ${target}")
+  $environment_command = "source ${gpg_agent_info}; export GPG_AGENT_INFO;"
+  $command = "/bin/bash -c '${environment_command} ${duplicity_command}'"
 
   cron { "Duplicity backup of ${name}":
     ensure  => $ensure,

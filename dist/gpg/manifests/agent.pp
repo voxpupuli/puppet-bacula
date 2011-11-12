2edaa08b2c57d54df349feae5c270e3951c52b02
Adrien Thebo <adrien@puppetlabs.com>
2011-11-11 17:00:48 -0800

    Looser regex for gpg::agent checking

--- dist/gpg/manifests/agent.pp
+++ dist/gpg/manifests/agent.pp
@@ -9,13 +9,13 @@
 #   ensure => present,
 # }
 #
-define gpg::agent ($ensure='present', $outfile = '', $options = []) {
+define gpg::agent ($ensure='present', $outfile = 'UNSET', $options = []) {
 
-  if $outfile == '' {
-    $gpg_agent_info = "/home/${name}/.gpg-agent-info"
-  }
-  else {
-    $gpg_agent_info = $outfile
+
+  $gpg_agent_info = $outfile ? {
+    'UNSET' => "--write-env-file /home/${name}/.gpg-agent-info",
+    undef   => "",
+    default => "--write-env-file $outfile",
   }
 
   $command = inline_template("gpg-agent --allow-preset-passphrase --write-env-file ${gpg_agent_info} --daemon <%= options.join(' ') %>")
@@ -26,14 +26,14 @@ define gpg::agent ($ensure='present', $outfile = '', $options = []) {
         user    => $name,
         path    => "/usr/bin:/bin:/usr/sbin:/sbin",
         command => $command,
-        unless  => "ps -U ${name} -o args | grep -v grep | grep \"${command}\"",
+        unless  => "ps -U ${name} -o args | grep -v grep | grep gpg-agent",
       }
     }
     absent: {
       exec { "kill gpg-agent":
         user    => $name,
         path    => "/usr/bin:/bin:/usr/sbin:/sbin",
-        command => "ps -U ${name} -eo pid,args | grep -v grep | grep gpg-agent | xargs kill",
+        command => "ps -U ${name} -o pid,args | grep -v grep | grep gpg-agent | awk '{print $1}' | xargs kill",
         onlyif  => "ps -U ${name} -o args | grep -v grep | grep gpg-agent",
       }
     }

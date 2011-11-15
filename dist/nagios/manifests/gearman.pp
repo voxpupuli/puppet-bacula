c99427b69cf877ee0f1b7ae5d4cb1ea53fdd2eec
Zach Leslie <zach@puppetlabs.com>
2011-11-15 13:23:29 -0800

    Move gearman checks out of baal node definition and into geaman class.
    Add nagios_server variable to gearman class and create hiera data for
    lookup .  This is to facilitate adding the chesk on the nagios server
    rather than on the worker.  Make all node use this lookup.
    
    Add nagios check commands to differentiate between sever and worker.
    Each worker needs a check, and each server needs a check.  There is only
    one server per site currently.

--- dist/nagios/manifests/gearman.pp
+++ dist/nagios/manifests/gearman.pp
@@ -1,6 +1,7 @@
 class nagios::gearman (
     $server = false,
-    $key
+    $key,
+    $nagios_server
   ){
 
   $nrpe_server = hiera("nrpe_server")
@@ -51,7 +52,16 @@ class nagios::gearman (
     require => Package[$packages],
   }
 
-  # Server only stuff, only need one per site
+  @@nagios_service { "check_gearman_worker_${hostname}":
+    use                 => 'generic-service',
+    check_command       => 'check_gearman_worker!localhost!worker_${hostname}',
+    host_name           => "${nagios_server}",
+    service_description => "check_gearman_worker_${hostname}",
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+    notify              => Service[$nagios::params::nagios_service],
+  }
+
+  # Server only stuff, only need one per site, atm
   if $server == true {
     service { "gearman-job-server":
       ensure    => running,
@@ -65,6 +75,16 @@ class nagios::gearman (
       content => template("nagios/gearman.conf.erb"),
       notify  => [Service["mod-gearman-worker"],Service["$::nagios::params::nagios_service"]],
     }
+
+    @@nagios_service { "check_gearman_server":
+      use                 => 'generic-service',
+      check_command       => 'check_gearman_server!localhost',
+      host_name           => "${nagios_server}",
+      service_description => "check_gearman_server",
+      target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+      notify              => Service[$nagios::params::nagios_service],
+    }
+
   }
 
 }

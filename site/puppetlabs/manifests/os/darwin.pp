17c3c556d74037c361b4b62912d6a77950afe5da
Ben Hughes <ben@puppetlabs.com>
2011-11-21 11:38:30 -0800

    Make macports updates from cron silent on Darwin.

--- site/puppetlabs/manifests/os/darwin.pp
+++ site/puppetlabs/manifests/os/darwin.pp
@@ -7,7 +7,7 @@ class puppetlabs::os::darwin {
   # keep macports up to date
   cron {
     "update macports":
-      command => "/opt/local/bin/port -q selfupdate",
+      command => "/opt/local/bin/port -q selfupdate >/dev/null",
       minute  => 0,
       hour    => 0,
       weekday => 1,

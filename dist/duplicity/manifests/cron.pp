21d68652a7d3e0c509508a55ade7aad661f0b53f
Adrien Thebo <adrien@puppetlabs.com>
2011-11-23 10:59:01 -0800

    Remove old reference to cron squelching.

--- dist/duplicity/manifests/cron.pp
+++ dist/duplicity/manifests/cron.pp
@@ -9,7 +9,7 @@ define duplicity::cron(
 
   include duplicity::client
 
-  $duplicity_command = inline_template("/usr/bin/duplicity <%= options.join(' ') %> ${name} ${target} <% quiet == 'true' and puts '1>/dev/null 2>/dev/null' %> ")
+  $duplicity_command = inline_template("/usr/bin/duplicity <%= options.join(' ') %> ${name} ${target}")
   $environment_command = "source ${gpg_agent_info}; export GPG_AGENT_INFO;"
   $command = "/bin/bash -c '${environment_command} ${duplicity_command}'"
 

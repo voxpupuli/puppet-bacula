54d4cc1bf89505db0e542097c80a8e212ac4a4f9
Adrien Thebo <adrien@puppetlabs.com>
2011-12-20 13:47:15 -0800

    (#11486) Setup 'deploy' user on rpm-builder

--- nodes/rpm-builder.pp
+++ nodes/rpm-builder.pp
@@ -10,5 +10,11 @@ node rpm-builder {
   # Surprise jenkin users that was one-off'd
   # Putting this in puppet to avoid breakage.
   Account::User <| tag == 'jenkins' |>
+
+  # (#11486) Setup 'deploy' user on rpm-builder
+  Account::User <| tag == deploy |>
+  Ssh_authorized_key <| tag == deploy |>
+  ssh::allowgroup { "www-data": }
+  sudo::entry { "deploy": entry => "deploy ALL=(ALL) NOPASSWD: ALL\n" }
 }
 

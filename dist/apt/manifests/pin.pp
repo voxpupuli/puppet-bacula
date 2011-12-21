cad1d18a15be35a1ee47300c1229218ca3d32ecc
Ben Hughes <ben@puppetlabs.com>
2011-12-21 15:28:25 -0800

    Make apt::pin able to have more than one wildcard style pin.
    So you can pin multiple entire releases, for when you need both backports
    installed and testing. Uses wildcard => true, then does the magic for you.

--- dist/apt/manifests/pin.pp
+++ dist/apt/manifests/pin.pp
@@ -5,14 +5,19 @@
 #        release  => 'lenny-backports',
 #        priority => '1001', }
 #
-# apt::pin{ '*':
+# set wildcard => true for wildcards, it then kinda ignores the
+# namevar so you can have duplicates without issue.
+#
+# apt::pin{ 'lenny_backports_wildcard':
 #  release  => 'lenny-backports',
 #  priority => '200',
-#  filename => 'star'
+#  filename => 'star',
+#  wildcard => true
 #  }
-##
+#
+#
 
-define apt::pin( $release, $priority, $filename = undef, $ensure = 'present' ) {
+define apt::pin( $release, $priority, $filename = undef, $ensure = 'present', $wildcard = false ) {
   # get around naming things such as '*'
   if $filename == undef {
     $fname = $name
@@ -20,10 +25,17 @@ define apt::pin( $release, $priority, $filename = undef, $ensure = 'present' ) {
     $fname = $filename
   }
 
+  # So, if we want to have more than one '*' entry.
+  if $wildcard == true {
+    $packagename = '*'
+  } else {
+    $packagename = $name
+  }
+
   file{
     "/etc/apt/preferences.d/${fname}.pref":
       ensure   => $ensure,
-      content  => "Package: $name\nPin: release a=$release\nPin-Priority: $priority\n",
+      content  => "Package: ${packagename}\nPin: release a=${release}\nPin-Priority: ${priority}\n",
       owner => 'root',
       group => 'root',
       mode => '0644'

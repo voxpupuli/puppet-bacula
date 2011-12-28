ae480ee7b49bcea202db4865ef5e3328490da9bf
Zach Leslie <zach@puppetlabs.com>
2011-12-28 14:41:16 -0800

    add support to the apt::pin define for pinning on things other than release, like origin

--- dist/apt/manifests/pin.pp
+++ dist/apt/manifests/pin.pp
@@ -17,7 +17,14 @@
 #
 #
 
-define apt::pin( $release, $priority, $filename = undef, $ensure = 'present', $wildcard = false ) {
+define apt::pin( 
+    $release  = '',
+    $origin   = '',
+    $priority,
+    $filename = undef,
+    $ensure   = 'present',
+    $wildcard = false
+  ) {
   # get around naming things such as '*'
   if $filename == undef {
     $fname = $name
@@ -32,12 +39,21 @@ define apt::pin( $release, $priority, $filename = undef, $ensure = 'present', $w
     $packagename = $name
   }
 
+  # to pin on something other than release, like origin.  See apt_preferences(5)
+  if $release != '' {
+    $pin = "release a=${release}"
+  } elsif $origin != '' {
+    $pin = "origin \"${origin}\""
+  } else {
+    err("Apt::Pin needs either $release or $origin, jerk")
+  }
+
   file{
     "/etc/apt/preferences.d/${fname}.pref":
-      ensure   => $ensure,
-      content  => "Package: ${packagename}\nPin: release a=${release}\nPin-Priority: ${priority}\n",
-      owner => 'root',
-      group => 'root',
-      mode => '0644'
+      ensure  => $ensure,
+      content => "Package: ${packagename}\nPin: ${pin}\nPin-Priority: ${priority}\n",
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644'
   }
 }

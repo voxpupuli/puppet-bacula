6f09055e723b71e5cd3a04784b0873c47b6b71e6
Adrien Thebo <adrien@puppetlabs.com>
2011-12-30 13:22:10 -0800

    (#11518) add customer dropbox accounts

--- nodes/odin.pp
+++ nodes/odin.pp
@@ -1,3 +1,22 @@
 node odin {
   include role::server
+
+  # Customer Groups
+  Account::User <| tag == customer |>
+  Group <| tag == customer |>
+
+  $ssh_customer = [
+    "vmware",
+    "motorola",
+    "nokia",
+    "blackrock",
+    "secureworks",
+    "bioware",
+    "wealthfront",
+    "scea",        #10214
+    "advance"
+  ]
+
+  ssh::allowgroup { "prosvc": }
+  ssh::allowgroup { $ssh_customer: chroot => true; }
 }

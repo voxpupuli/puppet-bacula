58566a19cd04f42cab19b698e6b37f1ef6ced4d2
Zach Leslie <zach@puppetlabs.com>
2012-01-18 10:47:11 -0800

    its not a module?

--- site/puppetlabs/lib/puppet/parser/functions/genpass.rb
+++ site/puppetlabs/lib/puppet/parser/functions/genpass.rb
@@ -1,35 +1,29 @@
 require 'pp'
 require 'yaml'
 
-module Puppet::Parser::Functions
-  newfunction(:genpass, :type => :rvalue) do |args|
-    store   = lookupvar('hostname')
-    search  = args[0]
-    datadir = '/etc/puppet/Pdubs'
-    datastore = "#{datadir}/#{store}.yaml"
+newfunction(:genpass, :type => :rvalue) do |args|
+  store   = lookupvar('hostname')
+  search  = args[0]
+  datadir = '/etc/puppet/Pdubs'
+  datastore = "#{datadir}/#{store}.yaml"
 
-    unless File.directory?(datadir)
-      Puppet::info "Creating datadir #{datadir}"
-      Dir.mkdir(datadir)
-    end
-
-    data = {}
-    if File.exists?(datastore)
-      data = YAML::load(File.open(datastore))
-    else
-      o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten;
-      string  =  (0..50).map{ o[rand(o.length)]  }.join;
-      data["#{search}"] = string
-      File.open(datastore, "w") do |f|
-        f.write(data.to_yaml)
-      end
-    end
-    Puppet.info data["#{search}"]
-    data["#{search}"]
+  unless File.directory?(datadir)
+    Puppet::info "Creating datadir #{datadir}"
+    Dir.mkdir(datadir)
   end
+
+  data = {}
+  if File.exists?(datastore)
+    data = YAML::load(File.open(datastore))
+  else
+    o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten;
+    string  =  (0..50).map{ o[rand(o.length)]  }.join;
+    data["#{search}"] = string
+    File.open(datastore, "w") do |f|
+      f.write(data.to_yaml)
+    end
+  end
+  Puppet.info data["#{search}"]
+  data["#{search}"]
 end
-
-
-
-
 

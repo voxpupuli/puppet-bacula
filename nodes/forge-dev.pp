c2f9b58e0bec3bcaf00cabdf96a0bdf6098a3b52
Ben Hughes <ben@puppetlabs.com>
2012-01-23 15:48:44 -0800

    Make forge-dev run the same code as idun for forge.

--- nodes/forge-dev.pp
+++ nodes/forge-dev.pp
@@ -6,13 +6,13 @@ node forge-dev {
 
   # Puppet Forge
   class { 'forge':
-      vhost       => 'forge-dev.puppetlabs.com',
-      ssl         => false,
-      newrelic    => false,
-      do_ssh_keys => true,
-      appserver   => 'unicorn',
-      github_url  => 'git@github.com:puppetlabs/puppet-forge.git',
-      git_revision => 'latest',
+      vhost        => 'forge-dev.puppetlabs.com',
+      ssl          => false,
+      newrelic     => false,
+      do_ssh_keys  => true,
+      appserver    => 'unicorn',
+      github_url   => 'git@github.com:puppetlabs/puppet-forge.git',
+      git_revision => '6dcedf02c5671c71fea32cc71e81f2753f0363dd',
   }
 
 }

c47a3e24b8b5a123844412398429cf29f4e71119
Adrien Thebo <adrien@puppetlabs.com>
2012-01-27 11:57:11 -0800

    Avoid spamming the ever living hell out of puppet-dev

--- nodes/jotunn.pp
+++ nodes/jotunn.pp
@@ -11,7 +11,7 @@ node jotunn {
   # https://projects.puppetlabs.com/issues/7849
   # github pull request robot
   # THIS DOESN'T WORK, VCSREPO IS UTTER MONKEY SHIT.
-  class{ 'githubrobotpuller':
-    version => 'bd4ea8f52b66556a1d45c03f9ff975e09f6b16e2',
-  }
+#  class{ 'githubrobotpuller':
+#    version => 'bd4ea8f52b66556a1d45c03f9ff975e09f6b16e2',
+#  }
 }

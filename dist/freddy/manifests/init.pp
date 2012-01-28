7da0eb9ba0e185f388eb99cc728660fc6e5b78bd
Adrien Thebo <adrien@puppetlabs.com>
2012-01-27 16:57:23 -0800

    Support files for freddy

--- dist/freddy/manifests/init.pp
+++ dist/freddy/manifests/init.pp
@@ -6,4 +6,13 @@ class freddy($root = '/opt/freddy') {
     source   => 'https://github.com/stepchange/freddy',
     revision => 'd30a0e3dd9588a0fdbc312139c544d2223c07485',
   }
+
+  file { "${root}/public":
+    ensure => directory,
+  }
+
+  file { "${root}/config.ru":
+    ensure => present,
+    source => "puppet:///modules/freddy/config.ru",
+  }
 }

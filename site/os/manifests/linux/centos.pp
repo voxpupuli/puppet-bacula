0cbb50bcdbdd3b553da47ad0edef011b5c20bbe3
Adrien Thebo <adrien@puppetlabs.com>
2012-01-31 13:02:29 -0800

    Point internal centos mirrors at real directories

--- site/os/manifests/linux/centos.pp
+++ site/os/manifests/linux/centos.pp
@@ -33,14 +33,14 @@ class os::linux::centos {
     "puppetlabs.lan": {
       yumrepo { 'puppetlabs_os':
         descr      => "Puppetlabs Internal OS Mirror Linux ${lsbmajdistrelease} - \$basearch",
-        baseurl    => "http://yo.puppetlabs.lan/cent${lsbmajdistrelease}server-$architecture/RPMS.os",
+        baseurl    => "http://yo.puppetlabs.lan/cent${lsbmajdistrelease}latestserver-$architecture/RPMS.os",
         enabled    => 1,
         gpgcheck   => 1,
         keepalive  => 1,
       }
       yumrepo { 'puppetlabs_updates':
         descr      => "Puppetlabs Internal Updates Mirror Linux ${lsbmajdistrelease} - \$basearch",
-        baseurl    => "http://yo.puppetlabs.lan/cent${lsbmajdistrelease}server-$architecture/RPMS.updates",
+        baseurl    => "http://yo.puppetlabs.lan/cent${lsbmajdistrelease}latestserver-$architecture/RPMS.updates",
         enabled    => 1,
         gpgcheck   => 1,
         keepalive  => 1,

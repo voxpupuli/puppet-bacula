181ec841101997c8834d380fab75e00ffc112d35
Zach Leslie <zach@puppetlabs.com>
2012-02-09 15:24:13 -0800

    stop using packages::vcs and start just including git on linux

--- dist/packages/manifests/vcs.pp
+++
@@ -1,7 +0,0 @@
-# to make it easier to grab the neede package one-offs
-
-class packages::vcs {
-	#@package { "git-core": ensure => installed; }
-	@package { "subversion": ensure => installed; }
-
-}

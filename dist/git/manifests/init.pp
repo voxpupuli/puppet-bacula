77d4111469000987cadbc5899b683580288ef24d
Adrien Thebo <adrien@puppetlabs.com>
2012-02-09 17:06:44 -0800

    Fixed git package name for ubuntu.
    Haet.

--- dist/git/manifests/init.pp
+++ dist/git/manifests/init.pp
@@ -1,6 +1,14 @@
 # install everything required to be a git client
 class git {
-  package { 'git':
+
+  case $operatingsystem {
+    'Debian': { $package_name = 'git' }
+    'CentOS': { $package_name = 'git' }
+    'Ubuntu': { $package_name = 'git-core }
+    default: { fail("No git package known for operating system ${operatingsystem}") }
+  }
+
+  package { $package_name:
     ensure => installed,
   }
 }

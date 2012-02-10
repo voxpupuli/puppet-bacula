25d59404611c4d6a55872074e34134975f1dbbc1
Adrien Thebo <adrien@puppetlabs.com>
2012-02-09 17:21:23 -0800

    Remember kids, quoted strings need quotes on *both* ends.

--- dist/git/manifests/init.pp
+++ dist/git/manifests/init.pp
@@ -4,7 +4,7 @@ class git {
   case $operatingsystem {
     'Debian': { $package_name = 'git' }
     'CentOS': { $package_name = 'git' }
-    'Ubuntu': { $package_name = 'git-core }
+    'Ubuntu': { $package_name = 'git-core' }
     default: { fail("No git package known for operating system ${operatingsystem}") }
   }
 

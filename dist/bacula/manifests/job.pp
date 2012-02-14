eaa213314c732b3939beffa7a0ec83d2d8680bab
Zach Leslie <zach@puppetlabs.com>
2012-02-14 13:49:39 -0800

    make bacula directory include a restore job by default and remove it from the bacula-dir.conf template

--- dist/bacula/manifests/job.pp
+++ dist/bacula/manifests/job.pp
@@ -35,7 +35,7 @@ define bacula::job (
   if defined(Class["bacula"]) {
     # if the fileset is not defined, we fall back to one called "Common"
     if $fileset == true {
-      if $files == '' { err("you tell me to create a fileset, but not files given") }
+      if $files == '' { err("you tell me to create a fileset, but no files given") }
       $fileset_real = $name
       bacula::fileset {
         $name:

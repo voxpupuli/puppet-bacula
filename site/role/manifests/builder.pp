e75f61e8dcf8d4dbf72cc95c269427339e005c5e
Zach Leslie <zach@puppetlabs.com>
2012-02-17 16:00:32 -0800

    add role::builder

---
+++ site/role/manifests/builder.pp
@@ -0,0 +1,11 @@
+class role::builder {
+
+  class { "role::server": nagios => false; }
+
+  Account::User      <| title == 'jenkins' |>
+  Ssh_authorized_key <| user == 'jenkins' |>
+
+  ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
+  ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
+
+}

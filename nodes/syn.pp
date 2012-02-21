b4300574b49e18c9de8ccf9dd742b05f8a23e1f8
Michael Kincaid <michael@puppetlabs.com>
2012-02-21 15:17:16 -0800

    Add another gather script dependency gem.

--- nodes/syn.pp
+++ nodes/syn.pp
@@ -13,7 +13,7 @@ node syn {
   ssh::allowgroup  { "interns": }
   sudo::allowgroup { "interns": }
 
-  package { ["rest-client","mime-types","httparty"]: provider => gem, ensure => present, }
+  package { ["rest-client","mime-types","httparty","nokogiri"]: provider => gem, ensure => present, }
   package { "ii": ensure => present, }
 
   file { "/var/lib/gather":

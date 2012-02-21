b09ecda13da9edd090d8b1fcdf430b4fae67e1c7
Ben Hughes <ben@puppetlabs.com>
2012-02-20 17:33:54 -0800

    Make ZFS monitoring automatic with munin.

---
+++ dist/zfs/manifests/munin.pp
@@ -0,0 +1,35 @@
+class zfs::munin {
+
+  include munin::params
+
+  # install the files, then use them.
+  file{ "${munin::params::plugin_source}/zfs":
+    ensure => directory,
+    recuse => true,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///module/zfs/muninplugins/',
+  }
+
+	$plugins = [  'zfs-filesystem-graph',
+                'zfs-statistics-for-freebsd',
+                'zfs-stats-for-freebsd-arc-efficiency',
+                'zfs-stats-for-freebsd-arc-utilization',
+                'zfs-stats-for-freebsd-cache-hits-by-cache-list',
+                'zfs-stats-for-freebsd-cache-hits-by-data-type',
+                'zfs-stats-for-freebsd-dmu-prefetch',
+                'zfs_arc_cache_hits_by_cache_list',
+                'zfs_arc_cache_hits_by_data_type',
+                'zfs_arc_efficiency',
+                'zfs_arc_utilization',
+                'zfs_dmu_prefetch',
+                'zlist',
+                'zpool_iostat' ]
+
+  munin::pluginer{ $plugins: ,
+    pluginpath => "${munin::params::plugin_source}/zfs",
+    require    => File["${munin::params::plugin_source}/zfs"],
+  }
+
+}

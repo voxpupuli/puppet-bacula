83d0a7c6c17c15f06d772ed37e2e3b837bd957c2
Zach Leslie <zach@puppetlabs.com>
2012-02-22 17:42:15 -0800

    update dhcp paths for munin::dhcp

--- dist/munin/manifests/dhcp.pp
+++ dist/munin/manifests/dhcp.pp
@@ -9,8 +9,8 @@ munin::dhcp {
       confname => 'dhcpd3',
       confs => {
         "user"           => "root",
-        "env.leasefile"  => "/var/lib/dhcp3/dhcpd.leases",
-        "env.configfile" => "/etc/dhcp3/dhcpd.conf",
+        "env.leasefile"  => "/var/lib/dhcp/dhcpd.leases",
+        "env.configfile" => "/etc/dhcp/dhcpd.conf",
         #"env.filter"    => "^10\.140\.",
         "env.critical"   => "0.95",
         "env.warning"    => "0.9",

9abd3531919cf6f0c04105ae63bef838561e72ed
Adrien Thebo <adrien@puppetlabs.com>
2012-02-24 12:54:11 -0800

    Update docs for service::gitolite, aggregate resources

--- nodes/clippy.pp
+++ nodes/clippy.pp
@@ -17,23 +17,4 @@ node clippy {
   sudo::allowgroup  { "techops": }
   ssh::allowgroup   { "techops": }
 
-  gpg::agent { "git":
-    options => [
-      "--default-cache-ttl 999999999",
-      "--max-cache-ttl     999999999",
-      "--use-standard-socket",
-    ],
-  }
-
-  duplicity::cron { "/home/git":
-    user           => "git",
-    target         => "ssh://gitbackups@bacula01.puppetlabs.lan:22//bacula/duplicity/git.puppetlabs.net",
-    home           => "/home/git",
-    mailto         => '',
-    options        => [
-      "--encrypt-key 409D0688",
-      "--sign-key 409D0688",
-      "--use-agent",
-    ],
-  }
 }
--- site/service/manifests/gitolite.pp
+++ site/service/manifests/gitolite.pp
@@ -1,6 +1,10 @@
-# puppetlabs::service::gitmirror
+# = Class: service::gito
 #
-# Installs and configures gitolite, and sets up github mirrors
+# == Purpose
+#
+#  - Installs and configures gitolite
+#  - Sets up mirrors of github repositories
+#  - Sets up and runs secure backups with duplicity
 
 class service::gitolite {
   include git::gitolite
@@ -40,5 +44,25 @@ class service::gitolite {
     "puppetlabs/puppetlabs-sysadmin-docs":
       private => true,
       ensure  => present;
+  }
+
+  gpg::agent { "git":
+    options => [
+      "--default-cache-ttl 999999999",
+      "--max-cache-ttl     999999999",
+      "--use-standard-socket",
+    ],
+  }
+
+  duplicity::cron { "/home/git":
+    user           => "git",
+    target         => "ssh://gitbackups@bacula01.puppetlabs.lan:22//bacula/duplicity/git.puppetlabs.net",
+    home           => "/home/git",
+    mailto         => '',
+    options        => [
+      "--encrypt-key 409D0688",
+      "--sign-key 409D0688",
+      "--use-agent",
+    ],
   }
 }

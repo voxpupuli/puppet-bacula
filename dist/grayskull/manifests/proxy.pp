4b64e75fb427f7e5ea93a57be913d8e3024c43de
Ben Hughes <ben@puppetlabs.com>
2012-02-24 16:25:41 -0800

    Add in Grayskull module to demo it for lols.

---
+++ dist/grayskull/manifests/proxy.pp
@@ -0,0 +1,23 @@
+class grayskull::proxy ($servername = $fqdn, $port = 8080) {
+  include nginx
+
+  nginx::unicorn {
+    'grayskull.puppetlabs.com':
+      priority       => 8,
+      unicorn_socket => 'http://localhost:8081',
+      path           => '/dev/null',
+      ssl            => true,
+      sslonly        => true,
+      isdefaultvhost => true, # default for SSL.
+      magic          => inline_template( "ssl_verify_client = on;\nssl_client_cert   = '${grayskull::installdir}/ssl/ca_crt.pem'\nssl_cert          = '${grayskull::installdir}/ssl/crt.pem'\nssl_key           = '${grayskull::installdir}/ssl/key.pem'\n" ),
+  }
+
+
+  file { "${grayskull::installdir}/ssl":
+    ensure  => directory,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => 0600,
+    recurse => true;
+  }
+}

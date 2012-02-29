744195922828c70f7378129bb729e97d6647b18e
Zach Leslie <zach@puppetlabs.com>
2012-02-29 14:52:39 -0800

    deb-builder is now role::builder only

--- site/role/manifests/builder.pp
+++ site/role/manifests/builder.pp
@@ -3,7 +3,7 @@ class role::builder {
   class { "role::server": nagios => false; }
 
   Account::User      <| title == 'jenkins' |>
-  Ssh_authorized_key <| user == 'jenkins' |>
+  Ssh_authorized_key <| user  == 'jenkins' |>
 
   ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
   ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }

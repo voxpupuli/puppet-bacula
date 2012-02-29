81e2f87bdc906f1203abfb0909a9fbc67c1704c6
Zach Leslie <zach@puppetlabs.com>
2012-02-29 15:31:22 -0800

    stop monitoring nagios on builders, but we should still ping them

--- site/role/manifests/builder.pp
+++ site/role/manifests/builder.pp
@@ -8,4 +8,7 @@ class role::builder {
   ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
   ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
 
+  @@nagios_host { $fqdn: alias => $hostname, address =>$ipaddress, hostgroups => hiera('location'); }
+  @@nagios_service { "check_ping_${hostname}": host_name => $fqdn, service_description => "check_ping_${fqdn}"; }
+
 }

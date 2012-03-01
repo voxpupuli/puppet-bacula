13231f38678e1fffdfd7820d5daf9f1111f1a8b8
Zach Leslie <zach@puppetlabs.com>
2012-03-01 11:05:01 -0800

    (#12787) Disable Nagios monitoring on builders

--- site/role/manifests/builder.pp
+++ site/role/manifests/builder.pp
@@ -1,14 +1,26 @@
 class role::builder {
 
-  class { "role::server": nagios => false; }
+  class { "role::server": nagios => false, bacula_monitor => false; }
 
   Account::User      <| title == 'jenkins' |>
   Ssh_authorized_key <| user  == 'jenkins' |>
 
-  ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
-  ssh::allowgroup  { ['release', 'builder', 'jenkins' ]: }
+  ssh::allowgroup   { ['release', 'builder', 'jenkins' ]: }
+  sudo::allowgroup  { ['release', 'builder', 'jenkins' ]: }
 
-  @@nagios_host { $fqdn: alias => $hostname, address =>$ipaddress, hostgroups => hiera('location'); }
-  @@nagios_service { "check_ping_${hostname}": host_name => $fqdn, service_description => "check_ping_${fqdn}"; }
+  @@nagios_host    { $fqdn:
+    alias      => $hostname,
+    address    => $ipaddress,
+    hostgroups => hiera('location'),
+    use        => 'generic-host',
+    target     => '/etc/nagios3/conf.d/nagios_host.cfg',
+  }
+  @@nagios_service { "check_ping_${hostname}":
+    use                 => 'generic-service',
+    host_name           => $fqdn,
+    check_command       => 'check_ping!150.0,20%!500.0,60%',
+    service_description => "check_ping_${fqdn}",
+    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+  }
 
 }

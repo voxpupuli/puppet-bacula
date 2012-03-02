8e005abef060289598a67a2541874b4e03377f49
Zach Leslie <zach@puppetlabs.com>
2012-03-02 15:29:11 -0800

    set port to 8080 on nginx::unicorn vhost for grayskull

--- dist/grayskull/manifests/proxy.pp
+++ dist/grayskull/manifests/proxy.pp
@@ -7,6 +7,7 @@ class grayskull::proxy ($servername = $fqdn, $port = 8080) {
       unicorn_socket => 'http://localhost:8081',
       path           => '/dev/null',
       ssl            => true,
+      port           => 8080,
       sslonly        => true,
       isdefaultvhost => true, # default for SSL.
       magic          => inline_template( "ssl_verify_client = on;\nssl_client_cert   = '${grayskull::installdir}/ssl/ca_crt.pem'\nssl_cert          = '${grayskull::installdir}/ssl/crt.pem'\nssl_key           = '${grayskull::installdir}/ssl/key.pem'\n" ),

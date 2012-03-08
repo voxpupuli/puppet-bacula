f6e9c8cbefbcca19fabf9e27856b24e4aa8d6dfd
Zach Leslie <zach@puppetlabs.com>
2012-03-08 14:58:43 -0800

    unbound is not being used on wyrd.  There is no point in running it.

--- nodes/wyrd.pp
+++ nodes/wyrd.pp
@@ -1,35 +1,5 @@
 node wyrd {
-
   include role::server
   include role::gearman
-
-
-  # DNS resolution to internal hosts
-  class {
-    "unbound":
-      interface => ["::0","0.0.0.0"],
-      access    => ["192.168.100.0/23","10.0.0.0/16"],
-  }
-
-  unbound::stub { "puppetlabs.lan":
-    address  => '192.168.100.8',
-    insecure => true,
-  }
-
-  unbound::stub { "100.168.192.in-addr.arpa.":
-    address  => '192.168.100.8',
-    insecure => true,
-  }
-
-  unbound::stub { "dc1.puppetlabs.net":
-    address  => '10.0.1.20',
-    insecure => true,
-  }
-
-  unbound::stub { "42.0.10.in-addr.arpa.":
-    address  => '10.0.1.20',
-    insecure => true,
-  }
-
 }
 

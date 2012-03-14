ee04b965540886a6504843a1fa361008a8456334
Adrien Thebo <adrien@puppetlabs.com>
2012-03-14 15:52:26 -0700

    Document rack::app

--- dist/rack/manifests/app.pp
+++ dist/rack/manifests/app.pp
@@ -1,6 +1,17 @@
 #
 # Defines a simple wrapper for installing a rack app at a sub uri.
 #
+# == Parameters
+#
+# [*doc_root*]
+#
+# The location of the webserver document root. A symlink in this root will be
+# made to the rack app.
+#
+# [*app_root*]
+#
+# The location of the rack application.
+#
 # == Example
 #
 # rack::app { "jsonp":

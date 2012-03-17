2c3afac80763d4fc193c8c1fa10313e9ef1f9f1c
Adrien Thebo <adrien@puppetlabs.com>
2012-03-16 21:59:31 -0700

    Add centos as a redhat variant
    But I liekd the $osfamily fact ;_;

--- dist/vim/manifests/init.pp
+++ dist/vim/manifests/init.pp
@@ -1,7 +1,7 @@
 class vim{
   case $operatingsystem {
     Debian, Ubuntu: { include vim::debian }
-    Redhat: { include vim::redhat }
+    Redhat, CentOS: { include vim::redhat }
     Suse: { include vim::suse }
     default: { fail("unspecified operatingsystem ${operatingsystem}") }
   }

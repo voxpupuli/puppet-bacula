48e304cfe829d43a4bb0244a8bfd87f65111e12b
Adrien Thebo <adrien@puppetlabs.com>
2012-03-17 10:56:41 -0700

    Correct sles vim package definition

--- dist/vim/manifests/init.pp
+++ dist/vim/manifests/init.pp
@@ -2,7 +2,7 @@ class vim{
   case $operatingsystem {
     Debian, Ubuntu: { include vim::debian }
     Redhat, CentOS, Fedora: { include vim::redhat }
-    Suse: { include vim::suse }
+    Suse, SLES: { include vim::suse }
     default: { fail("unspecified operatingsystem ${operatingsystem}") }
   }
 }

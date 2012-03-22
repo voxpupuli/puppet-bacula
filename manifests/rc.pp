b5d989a5e39ae1296b8b2bee9a3b0559b26fa9ee
Adrien Thebo <adrien@puppetlabs.com>
2012-03-21 20:26:44 -0700

    Add tunable share dir for gitolite

--- manifests/rc.pp
+++ manifests/rc.pp
@@ -20,16 +20,21 @@ class gitolite::rc (
   $gl_wildrepos_perm_cats = hiera('gitolite_rc_gl_wildrepos_perm_cats'),
   $projects_list          = hiera('gitolite_rc_projects_list'),
   $repo_umask             = hiera('gitolite_rc_repo_umask'),
+  $gl_adc_path            = hiera('gitolite_rc_gl_adc_path', 'UNSET'),
+  $gl_htpasswd_file       = hiera('gitolite_rc_gl_htpasswd_file', 'UNSET')
 ) {
 
   require gitolite::instance
 
-  $gl_adc_path      = hiera('gitolite_rc_gl_adc_path', 'UNSET')
-  $gl_htpasswd_file = hiera('gitolite_rc_gl_htpasswd_file', 'UNSET')
 
   $user  = hiera('gitolite_instance_user')
   $group = hiera('gitolite_instance_group')
   $home  = hiera('gitolite_instance_home')
+
+  $gl_share_dir = hiera('gitolite_install_source') ? {
+    'git'   => "${home}/share",
+    default => "/usr/share",
+  }
 
   file { "${home}/.gitolite.rc":
     ensure  => present,

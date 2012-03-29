5aebc7fb89f876fe4c9391060561961a615b7306
Zach Leslie <zach@puppetlabs.com>
2012-03-29 10:11:12 -0700

    attempt to fix type

--- dist/sysctl/lib/puppet/type/sysctl.rb
+++ dist/sysctl/lib/puppet/type/sysctl.rb
@@ -1,42 +1,40 @@
-module Puppet
-    newtype(:sysctl) do
+Puppet::Type.newtype(:sysctl) do
 
-        @doc = "Manages kernel parameters in /etc/sysctl.conf.  By default this will
-                only edit the configuration file, and not change any of the runtime
-                values.  If you wish changes to be activated right away, you can do
-                so with an exec like so:
+    @doc = "Manages kernel parameters in /etc/sysctl.conf.  By default this will
+            only edit the configuration file, and not change any of the runtime
+            values.  If you wish changes to be activated right away, you can do
+            so with an exec like so:
 
-                        exec { load-sysctl:
-                            command => \"/sbin/sysctl -p /etc/sysctl.conf\",
-                            refreshonly => true
-                        }
+                    exec { load-sysctl:
+                        command => \"/sbin/sysctl -p /etc/sysctl.conf\",
+                        refreshonly => true
+                    }
 
-                Set any changes you want to happen right away to notify this command,
-                or you can set it as the default:
+            Set any changes you want to happen right away to notify this command,
+            or you can set it as the default:
 
-                        Sysctl {
-                            notify => Exec[load-sysctl]
-                        }"
+                    Sysctl {
+                        notify => Exec[load-sysctl]
+                    }"
 
-        ensurable
+    ensurable
 
-        newparam(:name, :namevar => true) do
-            desc "Name of the parameter"
-            isnamevar
-        end
-        
-        newproperty(:val) do
-            desc "Value the parameter should be set to"
-        end
+    newparam(:name, :namevar => true) do
+        desc "Name of the parameter"
+        isnamevar
+    end
 
-        newproperty(:target) do
-            desc "Name of the file to store parameters in"
-            defaultto { if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::ParsedFile)
-                            @resource.class.defaultprovider.default_target
-                        else
-                            nil
-                        end
-            }
-        end
+    newproperty(:val) do
+        desc "Value the parameter should be set to"
+    end
+
+    newproperty(:target) do
+        desc "Name of the file to store parameters in"
+        defaultto { if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::ParsedFile)
+                        @resource.class.defaultprovider.default_target
+                    else
+                        nil
+                    end
+        }
     end
 end

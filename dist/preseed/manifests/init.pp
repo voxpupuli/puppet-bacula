0c6a72e4ba064d0cb042537f02b65ab2aa8dc9a7
Zach Leslie <zach@puppetlabs.com>
2012-03-30 16:15:57 -0700

    begin making default options for preseed and populating template

--- dist/preseed/manifests/init.pp
+++ dist/preseed/manifests/init.pp
@@ -1,6 +1,9 @@
 define preseed (
-    $template = 'preseed/debian_base.cfg.erb',
-    $proxy    = ''
+    $template         = 'preseed/debian_base.cfg.erb',
+    $locale           = 'en_US',
+    $keymap           = 'us',
+    $netcfg_interface = 'auto',
+    $proxy            = ''
 ) {
 
   file { $name:

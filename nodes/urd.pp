b54bc32d98c5395f75f5f54aef95d1a4295a52a0
Zach Leslie <zach@puppetlabs.com>
2012-03-30 16:32:07 -0700

    try to add some labels to note that things are not regular

--- nodes/urd.pp
+++ nodes/urd.pp
@@ -15,4 +15,19 @@ node urd {
   preseed { "/var/www/${fqdn}/d-i/debian_base.cfg": }
   preseed { "/var/www/${fqdn}/d-i/debian_ops.cfg": }
 
+  $debian = {
+    "arch" => ["amd64"],
+    "ver"  => ["squeeze","wheezy"],
+    "os"   => "debian"
+  }
+
+  $debian_common = {
+    "file"    => "os_<%= os %>",
+    "kernel"  => "images/<%= os %>/<%= ver %>/<%= arch %>/linux",
+    "append"  => "vga=normal initrd=images/<%= os %>/<%= ver %>/<%= arch %>/initrd.gz auto locale=en_US console-keymaps-at/keymap=us hostname=<%= os %> domain=unknown url=http://<%= fqdn %>/d-i/debian_base.cfg text",
+    "label"   => "Random Ops Shit <%= os %> <%= ver %> <= arch %>",
+  }
+
+  resource_permute('pxe::bootstrap', $debian, $debian_common)
+
 }

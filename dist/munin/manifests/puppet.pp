c45511ba5c2b11bd8681d6026b4469b5091deca4
Ben Hughes <ben@puppetlabs.com>
2012-03-29 18:20:07 -0700

    Fix puppet munin plugin so it works, and works on non linux (well install wise).

--- dist/munin/manifests/puppet.pp
+++ dist/munin/manifests/puppet.pp
@@ -13,32 +13,21 @@
 #
 class munin::puppet {
   include munin::params
-  include munin
-
-  file { '/etc/munin/plugin-conf.d/puppet_runs':
-    source => 'puppet:///modules/munin/puppet_runs',
-    ensure => present,
-  }
-
-  file { '/usr/share/munin/plugins/puppet_runs':
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => 'puppet:///modules/munin/puppet_runs_plugin',
-    ensure => present,
-  }
-
-  munin::plugin { 'puppet_runs':
-    require => [ File['/usr/share/munin/plugins/puppet_runs'], File['/etc/munin/plugin-conf.d/puppet_runs'] ],
-  }
-
 
   # Replaced, as the old plugin stopped working with.. ooh 2.6.x
-  $plugins = [ 'puppet_agent_memory']
+  $plugins = [ 'puppet_agent_memory', 'puppet_runs' ]
 
-  munin::plugins::install{ puppet: }
+  @munin::pluginconf { 'puppet_runs':
+      confname => 'puppet_runs',
+      confs => {
+        'user'    => 'root',
+        'command' => 'ruby %c',
+      }
+  }
 
-  munin::plugin{ $plugins: ,
+  @munin::plugins::install{ puppet: }
+
+  @munin::plugin{ $plugins: ,
     pluginpath => "${munin::params::plugin_source}/puppet",
     require    => Munin::Plugins::Install['puppet'],
   }

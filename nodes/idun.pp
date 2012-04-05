b1989e142de176f42dab226b2727580477e875e1
Ben Hughes <ben@puppetlabs.com>
2012-04-05 16:18:07 -0700

    Stop using newforge on forge.

--- nodes/idun.pp
+++ nodes/idun.pp
@@ -7,7 +7,6 @@ node idun {
   # Puppet Forge
   class { 'forge':
     vhost         => 'forge.puppetlabs.com',
-    serveraliases => 'newforge.puppetlabs.com', # remove me at some point.
     github_url    => 'git@github.com:puppetlabs/puppet-forge.git',
     git_revision  => '0.3.1',
     appserver     => 'unicorn',
@@ -18,4 +17,11 @@ node idun {
   include munin
   include munin::puppet
 
+  nginx::vhost::redirect {
+    'newforge_to_forge':
+      servername => 'newforge.puppetlabs.com',
+      priority   => 77,
+      port       => 80,
+      dest       => 'http://forge.puppetlabs.com/',
+  }
 }

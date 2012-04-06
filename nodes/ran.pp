6dcc3397ee102ce76e1aaa6c48a44a7d7ddfd54c
Zach Leslie <zach@puppetlabs.com>
2012-04-06 10:00:10 -0700

    add operations pxe options to dc

--- nodes/ran.pp
+++ nodes/ran.pp
@@ -9,8 +9,23 @@ node ran {
       port => 80,
   }
 
-  file { "/var/www/ran.dc1.puppetlabs.net/d-i/debian_base.cfg":
-    source => "puppet:///puppetlabs/debian_base.cfg",
+  preseed { "/var/www/${fqdn}/d-i/debian_base.cfg": }
+  preseed { "/var/www/${fqdn}/d-i/debian_ops.cfg": 
   }
+
+  $debian = {
+    "arch" => ["amd64"],
+    "ver"  => ["squeeze","wheezy"],
+    "os"   => "debian"
+  }
+
+  $debian_common = {
+    "file"      => "os_<%= os %>",
+    "kernel"    => "images/<%= os %>/<%= ver %>/<%= arch %>/linux",
+    "append"    => "initrd=images/<%= os %>/<%= ver %>/<%= arch %>/initrd.gz auto locale=en_US console-keymaps-at/keymap=us hostname=<%= os %> domain=unknown url=http://<%= fqdn %>/d-i/debian_ops.cfg text suite=<%= ver %>",
+    "menutitle" => "Operations auto-deployment <%= os %> <%= ver %> <%= arch %>",
+  }
+
+  resource_permute('pxe::menu::installentry', $debian, $debian_common)
 
 }

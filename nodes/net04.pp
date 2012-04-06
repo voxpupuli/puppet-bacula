4fbd2ce3f60f2a2bd0d0777eefce7a9dc3db44f6
Zach Leslie <zach@puppetlabs.com>
2012-04-06 11:30:30 -0700

    add new net03 and net04 nodes for doing slave transfers for now

---
+++ nodes/net04.pp
@@ -0,0 +1,33 @@
+node net04 {
+
+  include role::server
+
+  # ----------
+  # Bind
+  # ----------
+  class { 'bind':
+    customoptions => "check-names master ignore;\nallow-recursion {192.168.100.0/23; 10.0.0.0/16; };\n",
+  }
+
+  bind::zone {
+    'puppetlabs.lan':
+      type         => 'slave',
+      masters      => '192.168.100.8';
+    '100.168.192.in-addr.arpa':
+      type         => 'slave',
+      masters      => '192.168.100.8';
+    'dc1.puppetlabs.net':
+      type         => 'slave',
+      masters      => '10.0.1.20';
+    '1.0.10.in-addr.arpa':
+      type         => 'slave',
+      masters      => '10.0.1.20';
+    '5.0.10.in-addr.arpa':
+      type         => 'slave',
+      masters      => '10.0.1.20';
+    '42.0.10.in-addr.arpa':
+      type         => 'slave',
+      masters      => '10.0.1.20';
+  }
+
+}

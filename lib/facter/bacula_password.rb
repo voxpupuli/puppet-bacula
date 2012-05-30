8e19bbb8f5d97a2fcc1538a210c574650534be89
Adrien Thebo <adrien@puppetlabs.com>
2012-05-29 21:58:04 -0700

    Remove fallback bacula_password fact
    For the sake of safety and sanity, remove the randomly generated
    password. Using a default and fallback password could have unpredictable
    effects.
    
    This password should be generated from somewhere else.

--- lib/facter/bacula_password.rb
+++ lib/facter/bacula_password.rb
@@ -9,9 +9,6 @@
 client_config = '/etc/bacula/bacula-fd.conf'
 
 Facter.add(:bacula_password) do
-  # If a bacula client config exists, extract the password from it. This takes
-  # precedence since it uses the on-disk password.
-  has_weight 100
   confine :id => 'root'
 
   setcode do
@@ -28,16 +25,5 @@ Facter.add(:bacula_password) do
     rescue Errno::ENOENT => e
       Facter.debug e
     end
-  end
-end
-
-# This code was ripped off from Zach's genpass function
-Facter.add(:bacula_password) do
-  has_weight 0
-  confine :id => 'root'
-
-  setcode do
-    o =  [('a'..'z'),('A'..'Z')].map(&:to_a).flatten
-    (0..50).map{ o[rand(o.length)]  }.join
   end
 end

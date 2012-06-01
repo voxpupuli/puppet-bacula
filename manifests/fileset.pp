6920f57b2bf78a2072e10409f1bb89824ec0560f
Adrien Thebo <adrien@puppetlabs.com>
2012-06-01 10:26:13 -0700

    STOP DOING STUPID SHIT

--- manifests/fileset.pp
+++ manifests/fileset.pp
@@ -8,6 +8,6 @@ define bacula::fileset (
   @@concat::fragment { "bacula-fileset-$name":
     target  => '/etc/bacula/conf.d/fileset.conf',
     content => template("bacula/fileset.conf.erb"),
-    tag     => "bacula-$${bacula::params::bacula_director}";
+    tag     => "bacula-${bacula::params::bacula_director}";
   }
 }

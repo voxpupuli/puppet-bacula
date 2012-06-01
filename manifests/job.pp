073fee3c53bd3064798d1d1e8023b73d6f0008ff
Adrien Thebo <adrien@puppetlabs.com>
2012-06-01 10:06:30 -0700

    Drag in bacula class instead of checking for presence

--- manifests/job.pp
+++ manifests/job.pp
@@ -34,42 +34,40 @@ define bacula::job (
     $storage  = hiera('bacula_storage')
   ) {
 
-  if defined(Class["bacula"]) {
-    # if the fileset is not defined, we fall back to one called "Common"
-    if $fileset == true {
-      if $files == '' { err("you tell me to create a fileset, but no files given") }
-      $fileset_real = $name
-      bacula::fileset {
-        $name:
-          files    => $files,
-          excludes => $excludes
-        }
-    } else {
-      $fileset_real = "Common"
-    }
+  require bacula
 
-    @@concat::fragment {
-      "bacula-job-${name}":
-        target  => '/etc/bacula/conf.d/job.conf',
-        content => template("${template}"),
-        tag     => "bacula-${::bacula::params::bacula_director}";
-    }
+  # if the fileset is not defined, we fall back to one called "Common"
+  if $fileset == true {
+    if $files == '' { err("you tell me to create a fileset, but no files given") }
+    $fileset_real = $name
+    bacula::fileset {
+      $name:
+        files    => $files,
+        excludes => $excludes
+      }
+  } else {
+    $fileset_real = "Common"
+  }
 
-    if $bacula::monitor == true {
-      if $jobtype == 'Backup' {
-        @@nagios_service { "check_bacula_${name}":
-          use                      => 'generic-service',
-          host_name                => $::bacula::params::bacula_director,
-          check_command            => "check_nrpe!check_bacula!30 ${name}",
-          service_description      => "check_bacula_${name}",
-          target                   => '/etc/nagios3/conf.d/nagios_service.cfg',
-          notify                   => Service[$nagios::params::nagios_service],
-          first_notification_delay => '120',
-        }
+  @@concat::fragment {
+    "bacula-job-${name}":
+      target  => '/etc/bacula/conf.d/job.conf',
+      content => template("${template}"),
+      tag     => "bacula-${::bacula::params::bacula_director}";
+  }
+
+  if $bacula::monitor == true {
+    if $jobtype == 'Backup' {
+      @@nagios_service { "check_bacula_${name}":
+        use                      => 'generic-service',
+        host_name                => $::bacula::params::bacula_director,
+        check_command            => "check_nrpe!check_bacula!30 ${name}",
+        service_description      => "check_bacula_${name}",
+        target                   => '/etc/nagios3/conf.d/nagios_service.cfg',
+        notify                   => Service[$nagios::params::nagios_service],
+        first_notification_delay => '120',
       }
     }
-  } else {
-    err("need Class['bacula'] for this to be useful")
   }
 }
 

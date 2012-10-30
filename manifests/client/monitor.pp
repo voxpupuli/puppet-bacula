fac6bf4ca040f6f57f6063b727eb3278efa92520
Zach Leslie <zach@puppetlabs.com>
2012-10-29 17:24:36 -0700

    fix

--- manifests/client/monitor.pp
+++ manifests/client/monitor.pp
@@ -5,7 +5,7 @@ class bacula::client::monitor {
   icinga::service { "check_ssh_${hostname}":
     check_command      => 'check_nrpe!check_proc!1:1 bacula-fd',
     escalate           => true,
-    notification_delay => '120'
+    notification_delay => '120',
     first_escalation   => '3',
     require            => Service[$bacula::params::bacula_client_services],
   }

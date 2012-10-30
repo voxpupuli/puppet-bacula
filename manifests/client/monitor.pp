0828681785d8b239cf1508dd99e762caa7494241
Zach Leslie <zach@puppetlabs.com>
2012-10-29 17:23:35 -0700

    reuse code

--- manifests/client/monitor.pp
+++ manifests/client/monitor.pp
@@ -2,26 +2,12 @@ class bacula::client::monitor {
   include bacula::params
   include nagios::params
 
-  @@nagios_service { "check_baculafd_${hostname}":
-    service_description      => "Bacula File Daemon for ${hostname}",
-    use                      => 'generic-service',
-    host_name                => "$fqdn",
-    check_command            => 'check_nrpe!check_proc!1:1 bacula-fd',
-    service_description      => "check_baculafd_${hostname}",
-    target                   => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify                   => Service[$nagios::params::nagios_service],
-    require                  => Service[$bacula::params::bacula_client_services],
-    first_notification_delay => '120',
+  icinga::service { "check_ssh_${hostname}":
+    check_command      => 'check_nrpe!check_proc!1:1 bacula-fd',
+    escalate           => true,
+    notification_delay => '120'
+    first_escalation   => '3',
+    require            => Service[$bacula::params::bacula_client_services],
   }
 
-  @@nagios_servicedependency {"check_baculafd_${hostname}":
-    host_name                      => "$fqdn",
-    service_description            => "check_ping_${hostname}",
-    dependent_host_name            => "$fqdn",
-    dependent_service_description  => "check_baculafd_${hostname}",
-    execution_failure_criteria     => "n",
-    notification_failure_criteria  => "w,u,c",
-    ensure                         => present,
-    target                         => '/etc/nagios3/conf.d/nagios_servicedep.cfg',
-  }
 }

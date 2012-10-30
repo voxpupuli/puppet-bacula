1b4b7e34e051d90d8f0bef759c8b608ca011accc
Zach Leslie <zach@puppetlabs.com>
2012-10-29 17:26:30 -0700

    again with the fix

--- manifests/client/monitor.pp
+++ manifests/client/monitor.pp
@@ -2,7 +2,7 @@ class bacula::client::monitor {
   include bacula::params
   include nagios::params
 
-  icinga::service { "check_ssh_${hostname}":
+  icinga::service { "check_baculafd_${hostname}":
     check_command      => 'check_nrpe!check_proc!1:1 bacula-fd',
     escalate           => true,
     notification_delay => '120',

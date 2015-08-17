b15cd86be48257426f72fed00851c9b146ee2663
Manfred Pusch <manfred@pusch.cc>
2015-08-17 18:25:52 +0200

    added tests for the RHEL packages

--- spec/classes/director_spec.rb
+++ spec/classes/director_spec.rb
@@ -1,15 +1,51 @@
 require 'spec_helper' 
 
 describe 'bacula::director' do
-  let(:facts) {
-    {
-      :osfamily => 'Debian',
-      :operatingsystem => 'Debian',
-      :operatingsystemrelease => '7.0',
-      :concat_basedir => '/dne'
+  context 'Debian' do 
+    let(:facts) {
+      {
+        :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :operatingsystemrelease => '7.0',
+        :concat_basedir => '/dne'
+      }
     }
-  }
-  let(:params) { {:storage => 'mystorage.lan'} }
-  it { should contain_class('bacula::director') }
+    let(:params) { {:storage => 'mystorage.lan'} }
+    it { should contain_class('bacula::director') }
+  end
+  context 'RedHat' do
+    let(:facts) {
+      {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'RedHat',
+        :operatingsystemrelease => '7.0',
+        :operatingsystemmajrelease => '7',
+        :concat_basedir => '/dne'
+      }
+    }
+    let(:params) { {:storage => 'mystorage.lan'} }
+    it { should contain_class('bacula::director') }
+    context 'New packages' do
+      it { should contain_package('bacula-director').with(
+          'ensure' => 'present',
+        )
+      }
+      it { should_not contain_package('bacula-director-common') }
+    end
+    context 'Old packages' do
+      let(:facts) do
+        super().merge(
+          {
+            :operatingsystemrelease => '6.0',
+            :operatingsystemmajrelease => '6',
+          }
+        )
+      end
+      it { should contain_package('bacula-director-common').with(
+          'ensure' => 'present',
+        )
+      }
+      it { should_not contain_package('bacula-director') }
+    end
+  end
 end
-
--- spec/classes/storage_spec.rb
+++ spec/classes/storage_spec.rb
@@ -1,13 +1,49 @@
 require 'spec_helper'
 
 describe 'bacula::storage' do
-  let(:facts) {
-    {
-      :operatingsystem => 'Debian',
-      :concat_basedir => '/dne'
+  context 'Debian' do
+    let(:facts) {
+      {
+        :operatingsystem => 'Debian',
+        :concat_basedir => '/dne'
+      }
     }
-  }
-  let(:params) { {:director => 'mydirector.lan'} }
-  it { should contain_class('bacula::storage') }
+    let(:params) { {:director => 'mydirector.lan'} }
+    it { should contain_class('bacula::storage') }
+  end
+  context 'RedHat' do
+    let(:facts) {
+      {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'RedHat',
+        :operatingsystemrelease => '7.0',
+        :operatingsystemmajrelease => '7',
+        :concat_basedir => '/dne'
+      }
+    }
+    let(:params) { {:director => 'mydirector.lan'} }
+    it { should contain_class('bacula::storage') }
+    context 'New packages' do
+      it { should contain_package('bacula-storage').with(
+          'ensure' => 'present',
+        )
+      }
+      it { should_not contain_package('bacula-storage-common') }
+    end
+    context 'Old packages' do
+      let(:facts) do
+        super().merge(
+          {
+            :operatingsystemrelease => '6.0',
+            :operatingsystemmajrelease => '6',
+          }
+        )
+      end
+      it { should contain_package('bacula-storage-common').with(
+          'ensure' => 'present',
+        )
+      }
+      it { should_not contain_package('bacula-storage') }
+    end
+  end
 end
-

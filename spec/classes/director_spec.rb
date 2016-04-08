4e234ee4d9cd9710e30cac616f929af9dbea11c7
Zach Leslie <xaque208@gmail.com>
2016-04-08 16:13:10 -0700

    Begin migration to exclusive puppet4 support
    This work drops support for puppet 3.x, using native type validation and
    data in modules, updates build matrix to drop older Puppet builds, uses
    rspec-puppet-facts to improve testing.

--- spec/classes/director_spec.rb
+++ spec/classes/director_spec.rb
@@ -1,52 +1,15 @@
 require 'spec_helper'
 
 describe 'bacula::director' do
-  require 'hiera'
-  context 'Debian' do
-    let(:facts) {
-      {
-        :osfamily => 'Debian',
-        :operatingsystem => 'Debian',
-        :operatingsystemrelease => '7.0',
-        :concat_basedir => '/dne',
-        :ipaddress => '10.0.0.1'
-      }
-    }
-    it { should contain_class('bacula::director') }
-  end
-  context 'RedHat' do
-    let(:facts) {
-      {
-        :osfamily => 'RedHat',
-        :operatingsystem => 'RedHat',
-        :operatingsystemrelease => '7.0',
-        :operatingsystemmajrelease => '7',
-        :concat_basedir => '/dne',
-        :ipaddress => '10.0.0.1'
-      }
-    }
-    it { should contain_class('bacula::director') }
-    context 'New packages' do
-      it { should contain_package('bacula-director').with(
-          'ensure' => 'present',
-        )
-      }
-      it { should_not contain_package('bacula-director-common') }
-    end
-    context 'Old packages' do
-      let(:facts) do
-        super().merge(
-          {
-            :operatingsystemrelease => '6.0',
-            :operatingsystemmajrelease => '6',
-          }
-        )
+  on_supported_os.each do |os, facts|
+    let(:facts) { facts }
+    context "on #{os}" do
+
+      case facts[:osfamily]
+      when 'Debian'
+        it { should contain_class('bacula::director') }
       end
-      it { should contain_package('bacula-director-common').with(
-          'ensure' => 'present',
-        )
-      }
-      it { should_not contain_package('bacula-director') }
+
     end
   end
 end

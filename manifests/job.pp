c59857144b9f4396b4b96c035b4a7d43787f1b0a
Romain Tartière <romain@blogreen.org>
2018-08-06 16:04:02 +0200

    Synchronise type of $priory between job and jodefs
    jodefs defines $priory as an Integer.  Accordingly, expect an
    Optional[Integer] for job.

--- manifests/job.pp
+++ manifests/job.pp
@@ -68,7 +68,7 @@ define bacula::job (
   Optional[String]  $messages            = undef,
   Optional[String]  $restoredir          = '/tmp/bacula-restores',
   Optional[String]  $sched               = undef,
-  Optional[Variant[Integer,String]]  $priority   = undef,
+  Optional[Integer] $priority            = undef,
   Optional[String] $job_tag        = undef,
   Optional[String]  $selection_type      = undef,
   Optional[String]  $selection_pattern   = undef,
